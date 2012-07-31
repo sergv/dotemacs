ce9e1048eb4f43c901832340a3813c44914623a9
Sergey Vinokurov <serg.foo@gmail.com>
2012-07-31 23:14:01 +0300

    combine ibuffer and tabbar buffer groups definitions into +buffer-groups+
    This way the duplication of effort to maintain the same group definitions
    in two separate files is eliminated.

--- src/tabbar-buffer-groups.el
+++ src/tabbar-buffer-groups.el
@@ -7,256 +7,43 @@
 ;; Description:
 
 (require 'more-scheme)
+(require 'buffer-groups)
 
 (defun tabbar-buffer-groups+ (buffer)
   "Return the list of group names BUFFER belongs to.
 Return only one group for each buffer."
   (with-current-buffer (get-buffer buffer)
-    (let ((starred-buffer-re (rx bol "*" (+ nonl) "*" eol))
-          (bufname (buffer-name)))
-      (cond
-        ((and (memq major-mode
-                    '(cl-mode
-                      lisp-mode
-                      common-lisp-mode))
-              (not (string-match-pure? starred-buffer-re bufname)))
-         '("Lisp"))
-
-        ;; count repl as both lisp and slime mode
-        ((eq major-mode 'slime-repl-mode)
-         '("Lisp" "SLIME"))
-
-        ((and (memq major-mode
-                    '(emacs-lisp-mode
-                      inferior-emacs-lisp-mode))
-              (not (member bufname
-                           '("*scratch*"
-                             "*Pp Eval Output*"))))
-         '("Emacs Lisp"))
-
-        ((string-match-pure? (rx "*"
-                                 (or (seq (or "slime-repl"
-                                              "sldb")
-                                          (+ " ")
-                                          (or "sbcl"
-                                              "sbcl-full"
-                                              "cmucl"
-                                              "clisp"
-                                              "ccl"
-                                              "ecl"
-                                              "clozure"
-                                              "lisp"
-                                              "scheme"
-                                              "chicken"
-                                              "bigloo"
-                                              "scheme48"
-                                              "guile"
-                                              "gambit"
-                                              "gauche"
-                                              "mit")
-                                          (? "/"
-                                             (+ digit)))
-                                     (or "slime-events"
-                                         "slime-description"
-                                         "slime-trace"
-                                         "slime-compilation"
-                                         "slime-xref"
-                                         "slime-apropos"
-                                         "slime-inspector"
-                                         "slime-macroexpansion"
-                                         "inferior-lisp"
-                                         "lisp-interaction"
-                                         "fuzzy completions"))
-                                 "*"
-                                 ;; (? "<"
-                                 ;;    (+ digit)
-                                 ;;    ">")
-                                 )
-                             (downcase bufname))
-         (memq major-mode
-               ;; count relp here too, just in case
-               '(slime-repl-mode
-                 sldb-mode))
-         '("SLIME"))
-
-        ((or (eq major-mode 'scheme-mode)
-             (string-match-pure? (rx (or (seq "*"
-                                                  (? (or "chicken"
-                                                         "bigloo"
-                                                         "scheme48"
-                                                         "guile"
-                                                         "gambit"
-                                                         "gauche"
-                                                         "mit")
-                                                     "-")
-                                                  "scheme*")
-                                             "* Guile REPL *")
-                                         (? "<"
-                                            (+ digit)
-                                            ">"))
-                                 bufname))
-         '("Scheme"))
-
-        ((memq major-mode
-               '(c-mode
-                 c++-mode))
-         '("C/C++"))
-
-        ((or (memq major-mode
-                   '(haskell-mode
-                     inferior-haskell-mode
-                     inferior-hugs-mode
-                     haskell-hugs-mode
-                     ghc-core-mode
-                     hugs-mode))
-             (string-match-pure? (rx "*haskell*"
-                                     (? "<"
-                                        (+ digit)
-                                        ">"))
-                                 bufname))
-         '("Haskell"))
-
-        ((or (memq major-mode
-                   '(prolog-mode))
-             (string-match-pure? (rx "*prolog*"
-                                     (? "<"
-                                        (+ digit)
-                                        ">"))
-                                 bufname))
-         '("Prolog"))
-
-        ((or (memq major-mode
-                   '(octave-mode
-                     inferior-octave-mode))
-             (string-match-pure? (rx "*Octave*"
-                                     (? "<"
-                                        (+ digit)
-                                        ">"))
-                                 bufname))
-         '("Octave"))
-
-        ((or (memq major-mode
-                   '(python-mode
-                     python-repl-mode
-                     inferior-python-mode
-                     python-run-mode))
-             (string-match-pure? (rx (or "*Python*"
-                                         "*IPython*"
-                                         "*Python Output*")
-                                     (? "<"
-                                        (+ digit)
-                                        ">"))
-                                 bufname))
-         '("Python"))
-
-        ((memq major-mode
-               '(cython-mode
-                 cython-compilation-mode))
-         '("Cython"))
-
-        ((or (memq major-mode
-                   '(maxima-mode
-                     maxima-noweb-mode
-                     inferior-maxima-mode))
-             (string-match-pure? (rx (or "*maxima*"
-                                         "*imaxima*")
-                                     (? "<"
-                                        (+ digit)
-                                        ">"))
-                                 bufname))
-         '("Maxima"))
-
-        ((or (memq major-mode
-                   '(org-mode
-                     org-agenda-mode
-                     diary-mode
-                     calendar-mode)))
-         '("Org"))
-
-        ((or (memq major-mode '(LaTeX-mode latex-mode tex-mode)))
-         '("LaTeX"))
-
-        ((or (memq major-mode '(html-mode
-                                sgml-mode
-                                nxhtml-mode
-                                nxhtml-muamo-mode
-                                nxml-mode
-                                css-mode
-                                js-mode
-                                django-nxhtml-mumamo-mode
-                                django-html-mumamo-mode
-                                rnc-mode)))
-         '("Web"))
-
-        ((or (memq major-mode
-                   '(magit-mode
-                     magit-commit-mode
-                     magit-diff-mode
-                     magit-key-mode
-                     magit-log-edit-mode
-                     magit-log-mode
-                     magit-reflog-mode
-                     magit-show-branches-mode
-                     magit-stash-mode
-                     magit-status-mode
-                     magit-wazzup-mode
-                     gitignore-mode))
-             (string-match-pure? (rx bol "*magit" (* nonl) "*" eol) bufname))
-         '("Version Control"))
-
-        ((or (member bufname
-                     '("*Tags List*"
-                       "makefile"
-                       "Makefile"
-                       "GNUMakefile"))
-             (get-buffer-process (current-buffer))
-             (memq major-mode
-                   '(comint-mode
-                     compilation-mode
-                     grep-mode
-                     latex-compilation-mode
-                     haskell-compilation-mode
-                     hs-lint-mode
-                     hs-scan-mode
-                     gnuplot-run-mode
-                     eshell-mode
-                     shell-mode)))
-         '("Utility"))
-
-
-        ((memq major-mode
-               '(rmail-mode
-                 rmail-edit-mode
-                 vm-summary-mode
-                 vm-mode
-                 mail-mode
-                 mh-letter-mode
-                 mh-show-mode
-                 mh-folder-mode
-                 gnus-summary-mode
-                 message-mode
-                 gnus-group-mode
-                 gnus-article-mode
-                 score-mode
-                 gnus-browse-killed-mode))
-         '("Mail"))
-
-        ;; put *scratch* buffer in "Common" group
-        ((or (string-match-pure? starred-buffer-re bufname)
-             (member bufname
-                     '("*scratch*"
-                       "*Messages*"
-                       "*Pp Eval Output*"))
-             (memq major-mode
-                   '(dired-mode
-                     help-mode
-                     apropos-mode
-                     Info-mode
-                     Man-mode
-                     ibuffer-mode)))
-         '("Common"))
-
-        (t '("Text"))))))
+    (cond ((eq? major-mode 'slime-repl-mode)
+           '("lisp" "slime"))
+          ;; "interpret" +buffer-groups+
+          (else
+           (let ((bufname (buffer-name)))
+             (labels
+               ((matches-definition? (definition)
+                  (cond ((null? definition)
+                         nil)
+                        ((eq? (car definition) 'or)
+                         (matches-definition? (cdr definition)))
+                        ((eq? (caar definition) 'predicate)
+                         ;; eval predicate body
+                         (or (eval (cdar definition))
+                             (matches-definition? (cdr definition))))
+                        ((eq? (caar definition) 'mode)
+                         (or (eq? major-mode (cdar definition))
+                             (matches-definition? (cdr definition))))
+                        ((eq? (caar definition) 'name)
+                         (or (string-match-pure? (cdar definition) bufname)
+                             (matches-definition? (cdr definition))))))
+                (find-group (groups)
+                  (if (null? groups)
+                    '("text")
+                    (let ((group (caar groups))
+                          (definition (cadar groups)))
+                      (if (matches-definition? definition)
+                        ;; tabbar expects group to be a list
+                        (list group)
+                        (find-group (cdr groups)))))))
+               (find-group +buffer-groups+)))))))
 
 
 (provide 'tabbar-buffer-groups)
