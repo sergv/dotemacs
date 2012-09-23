d67dcff2f66e44876849862f93fff09aa180bb6a
Sergey Vinokurov <serg.foo@gmail.com>
2012-09-23 15:08:07 +0300

    clean up fortunes, set up environment variables properly by quering bash

--- set-up-environment-variables.el
+++ set-up-environment-variables.el
@@ -1,4 +1,4 @@
-;; set-up-environment-variables.el ---
+;; set-up-environment-variables.el --- -*- lexical-binding: t -*-
 
 ;; Copyright (C) Sergey Vinokurov
 ;;
@@ -9,32 +9,18 @@
 (require 'more-scheme)
 (require 'cl)
 
-(for-each
- (lambda (entry)
-   (destructuring-bind (variable value)
-       entry
-     (setenv variable value)))
- '(("PATH"
-    "/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin/:/home/sergey/.cabal/bin:/home/sergey/projects/lisp/local/bin:/home/sergey/projects/python/pypy/bin:/home/sergey/projects/scheme/local/bin")
-   ("LD_LIBRARY_PATH"
-    "/home/sergey/projects/python/webcam/collect-data/local/lib/:/usr/local/lib:/usr/lib:/home/sergey/projects/lisp/local/lib:/home/sergey/projects/scheme/local/lib")
-   ("PYTHONPATH"
-    "/home/sergey/projects/python/modules/:/home/sergey/projects/python/webcam/collect-data/local/lib/python2.7/site-packages:/home/sergey/projects/python/theano/local/lib/python2.7/site-packages/")
-   ("INFOPATH"
-    "/usr/share/info:/usr/local/share/info:/home/sergey/projects/lisp/local/share/info:/home/sergey/projects/scheme/local/share/info")
-   ("MANPATH"
-    "/usr/share/man:/usr/local/share/man:/home/sergey/projects/lisp/local/share/man:/home/sergey/projects/scheme/local/share/man")
-   ("PKG_CONFIG_PATH"
-    "/usr/lib/pkgconfig:/usr/share/pkgconfig:/usr/local/lib/pkgconfig:/usr/local/share/pkgconfig:/home/sergey/projects/scheme/local/lib/pkgconfig/:/home/sergey/projects/scheme/local/share/pkgconfig")
-   ("EDITOR"
-    "emacs")))
+(when (file-exists-p "~/.bash_env")
+  (let* ((variables "PATH LD_LIBRARY_PATH PYTHONPATH EDITOR INFOPATH MANPATH PKG_CONFIG_PATH")
+         (values (shell-command-to-string
+                  (format ". ~/.bash_env; printenv %s;" variables))))
+    (loop
+      for var in (split-string variables " ")
+      for value in (split-string values "\n")
+      do (setenv var value))))
 
 (defun env-var-into-list (env-var list)
-  (for-each
-   (lambda (dir)
-     (add-to-list list dir))
-   (split-string (getenv env-var)
-                 ":")))
+  (for-each (lambda (dir) (add-to-list list dir))
+   (split-string (getenv env-var) ":")))
 
 (env-var-into-list "PATH" 'exec-path)
 
@@ -42,7 +28,6 @@
 (provide 'set-up-environment-variables)
 
 ;; Local Variables:
-;; lexical-binding: t
 ;; End:
 
 ;; set-up-environment-variables.el ends here
