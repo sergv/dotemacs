3c4b675e1b7a3a897d7768ac363a6bf79324d0d4
Sergey Vinokurov <serg.foo@gmail.com>
2012-09-29 01:35:39 +0300

    setup for telnet-mode; change warning policy of elisp-compile-and-move

---
+++ src/telnet-mode-setup.el
@@ -0,0 +1,19 @@
+;; telnet-mode-setup.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: Saturday, 29 September 2012
+;; Description:
+
+(defun telnet-mode-setup ()
+  (init-common :use-yasnippet nil :use-comment nil))
+
+(add-hook 'telnet-mode-hook #'telnet-mode-setup)
+
+(provide 'telnet-mode-setup)
+
+;; Local Variables:
+;; End:
+
+;; telnet-setup.el ends here
