9450dd3d587f6a399ebf1c3f65e9255f9778cf86
Sergey Vinokurov <serg.foo@gmail.com>
2012-10-09 02:09:46 +0300

    improve C++ support, move c++-related files into c directory; introduce c-like-setup

---
+++ src/c/c++-abbrev+.el
@@ -0,0 +1,35 @@
+;; c++-abbrev+.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: Monday,  8 October 2012
+;; Description:
+
+(require 'common)
+
+(defun c++-abbrev+-setup ()
+  (setf abbrev+-skip-syntax '("w" "w_" "^ >")
+        abbrev+-abbreviations
+        (list
+         (list "\\<pr\\(?:i\\(?:nt?\\)?\\)?f?\\>"
+               (list
+                (lambda ()
+                  (yas/expand-snippet "printf(\"$1\\n\"$2);$0")))
+               (lambda () (not (point-inside-string-or-comment?))))
+         (list "\\<info\\>"
+               (list
+                (lambda ()
+                  (yas/expand-snippet
+                   "std::cout << \"$1: \" << $1 << std::endl;$2")))
+               (lambda () (not (point-inside-string-or-comment?))))))
+
+  (def-keys-for-map vim:insert-mode-local-keymap
+    ("SPC" abbrev+-insert-space-or-expand-abbrev)))
+
+(provide 'c++-abbrev+)
+
+;; Local Variables:
+;; End:
+
+;; c++-abbrev+.el ends here
--- src/c/c-setup.el
+++ src/c/c-setup.el
@@ -6,110 +6,11 @@
 ;; Created: long ago
 ;; Description:
 
-
-;; this whole file will be loaded on demand using c-c++-setup.el
-
-(require 'custom)
-
-(require 'c-eldoc)
-(require 'ctypes)
-(require 'find-file)
+(require 'c-like-setup)
 (require 'c-abbrev+)
 
-;; (defun c-c++-get-dual-file (file)
-;;   (let* ((header-suffix-re "[Hh]\\(?:[Hh]\\|\\+\\+\\|[Pp][Pp]\\|[Xx][Xx]\\)?")
-;;          (source-suffix-re "[Cc]\\(?:[Cc]\\|\\+\\+\\|[Pp][Pp]\\|[Xx][Xx]\\)?")
-;;          (fname (file-name-sans-extension
-;;                  (file-name-nondirectory file)))
-;;          (ext   (file-name-extension file))
-;;          (files (remove-if #'file-directory-p
-;;                            (directory-files
-;;                             (file-name-directory file))))
-;;          suffix)
-;;     (setq suffix
-;;           (cond
-;;             ((string-match-p (concat "^" header-suffix-re "$")
-;;                              ext)
-;;              source-suffix-re)
-;;             ((string-match-p (concat "^" source-suffix-re "$")
-;;                              ext)
-;;              header-suffix-re)
-;;             (t (error
-;;                 "Error: current file is not header nor C/C++ source file: %S"
-;;                 file))))
-;;     (find-if (lambda (x) (string-match-p (concat "^" fname "." suffix "$")
-;;                                     x))
-;;              files)))
-;;
-;; (defvar-loc c-c++-is-dual-file-for)
-;;
-;; (defun c-c++-switch-header-and-source ()
-;;   "Switch between header .*.(h|hh|H|HH|h++|H++|HXX) and source
-;; .*.(c|cc|cpp|c++|C|CC|CPP|C++|cxx|CXX) file."
-;;   (interactive)
-;;   ;; use simple cache
-;;   (if (and c-c++-is-dual-file-for
-;;            ;; only if these files have the same name sans extension
-;;            ;; if that's not the case then these files are not consieder
-;;            ;; dual
-;;            (string= (file-name-sans-extension
-;;                      (file-name-nondirectory (buffer-file-name)))
-;;                     (file-name-sans-extension
-;;                      (file-name-nondirectory c-c++-is-dual-file-for))))
-;;     (find-file c-c++-is-dual-file-for)
-;;
-;;     (let ((other-file (c-c++-get-dual-file (buffer-file-name))))
-;;       (unless other-file
-;;         (error (concat "Error: dual for file %S "
-;;                        "doesn't exists in current directory %S")
-;;                buffer-file-name
-;;                (file-name-directory buffer-file-name)))
-;;       (setq c-c++-is-dual-file-for other-file)
-;;       (find-file other-file))))
-;;
-
-(eval-after-load
- "c-eldoc"
- '(progn
-   ;; make cache buffer names start with a space to make them invisible
-   (redefun c-eldoc-get-buffer (function-name)
-     "Call the preprocessor on the current file"
-     ;; run the first time for macros
-     (let ((output-buffer (cache-gethash (current-buffer) c-eldoc-buffers)))
-       (if output-buffer output-buffer
-         (let* ((this-name (concat " *" buffer-file-name "-preprocessed*"))
-                (preprocessor-command (concat c-eldoc-cpp-command " "
-                                              c-eldoc-cpp-macro-arguments " "
-                                              c-eldoc-includes " "
-                                              buffer-file-name))
-                (cur-buffer (current-buffer))
-                (output-buffer (generate-new-buffer this-name)))
-           (call-process-shell-command preprocessor-command nil output-buffer nil)
-           ;; run the second time for normal functions
-           (setq preprocessor-command (concat c-eldoc-cpp-command " "
-                                              c-eldoc-cpp-normal-arguments " "
-                                              c-eldoc-includes " "
-                                              buffer-file-name))
-           (call-process-shell-command preprocessor-command nil output-buffer nil)
-           (cache-puthash cur-buffer output-buffer c-eldoc-buffers)
-           output-buffer))))))
-
 (defun c-setup ()
-  (init-common :use-render-formula t)
-  (autopair-mode 1)
-
-  (hs-minor-mode 1)
-  (c-turn-on-eldoc-mode)
-
-  (modify-syntax-entry ?_ "_")
-
-  (setf whitespace-line-column 80
-        whitespace-style '(tabs lines-tail)
-        ;; affects only tab display
-        tab-width 4)
-
-  (set (make-variable-buffer-local 'vim:shift-width) 8)
-  (set (make-variable-buffer-local 'eldoc-idle-delay) 0.2)
+  (c-like-setup)
 
   (if-buffer-has-file
    (set (make-local-variable 'compile-command)
@@ -131,36 +32,6 @@
    (set (make-local-variable 'compile-command)
         (concat "make " (file-name-sans-extension
                          (file-name-nondirectory buffer-file-name)))))
-
-  (which-function-mode -1)
-
-  (setq c-tab-always-indent t)
-  (c-toggle-hungry-state 1)
-  (c-toggle-electric-state -1)
-  (c-toggle-auto-newline -1)
-  ;; (subword-mode t)
-
-  (setq vim:normal-mode-local-keymap           (make-keymap)
-        vim:insert-mode-local-keymap           (make-sparse-keymap)
-        vim:visual-mode-local-keymap           (make-sparse-keymap)
-        vim:operator-pending-mode-local-keymap (make-sparse-keymap))
-
-  (def-keys-for-map vim:normal-mode-local-keymap
-    ("SPC SPC" ff-find-related-file)
-    ("g TAB"   c-indent-defun)
-
-    ("g t"     c-end-of-defun)
-    ("g n"     c-beginning-of-defun)
-
-    ("z o"     hs-show-block)
-    ("z v"     hs-show-block)
-    ("z c"     hs-hide-block)
-    ("z C"     hs-hide-all)
-    ("z O"     hs-show-all))
-
-  (def-keys-for-map vim:visual-mode-local-keymap
-    ("g t" c-end-of-defun)
-    ("g n" c-beginning-of-defun))
 
   (c-abbrev+-setup))
 
