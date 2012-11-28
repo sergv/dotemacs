752d1d92e9703755389098e194b7dfadb9af2631
Sergey Vinokurov <serg.foo@gmail.com>
2012-11-28 21:22:33 +0200

    move ibuffer custom vars to setup; redefine ibuffer-jump-to-buffer to use case-insensetive completion

--- src/ibuffer-setup.el
+++ src/ibuffer-setup.el
@@ -13,6 +13,11 @@
 
 (defalias 'list-buffers 'ibuffer)
 
+(setf ibuffer-formats '((mark modified read-only " " (name 32 32 :left :elide) " " (mode 16 16 :left :elide) " " filename-and-process)
+                        (mark " " (name 32 -1) " " filename-and-process))
+      ibuffer-jump-offer-only-visible-buffers t
+      ibuffer-show-empty-filter-groups nil)
+
 (eval-after-load
  "ibuffer"
  '(progn
@@ -79,6 +84,64 @@
      (if by-regexp
        (call-interactively #'ibuffer-mark-by-mode-regexp)
        (call-interactively #'ibuffer-mark-by-mode)))
+
+   ;; ignore case when prompting for buffer name
+   (redefun ibuffer-jump-to-buffer (name)
+     "Move point to the buffer whose name is NAME.
+
+If called interactively, prompt for a buffer name and go to the
+corresponding line in the Ibuffer buffer.  If said buffer is in a
+hidden group filter, open it.
+
+If `ibuffer-jump-offer-only-visible-buffers' is non-nil, only offer
+visible buffers in the completion list.  Calling the command with
+a prefix argument reverses the meaning of that variable."
+     (interactive (list
+                   (let ((only-visible ibuffer-jump-offer-only-visible-buffers))
+                     (when current-prefix-arg
+                       (setq only-visible (not only-visible)))
+                     (if only-visible
+                       (let ((table (mapcar #'(lambda (x)
+                                                (buffer-name (car x)))
+                                            (ibuffer-current-state-list)))
+                             (completion-ignore-case t))
+                         (when (null table)
+                           (error "No buffers!"))
+                         (completing-read "Jump to buffer: "
+                                          table nil t))
+                       (read-buffer "Jump to buffer: " nil t)))))
+     (when (not (string= "" name))
+       (let (buf-point)
+         ;; Blindly search for our buffer: it is very likely that it is
+         ;; not in a hidden filter group.
+         (ibuffer-map-lines #'(lambda (buf _marks)
+                                (when (string= (buffer-name buf) name)
+                                  (setq buf-point (point))
+                                  nil))
+                            t nil)
+         (when (and
+                (null buf-point)
+                (not (null ibuffer-hidden-filter-groups)))
+           ;; We did not find our buffer.  It must be in a hidden filter
+           ;; group, so go through all hidden filter groups to find it.
+           (catch 'found
+             (dolist (group ibuffer-hidden-filter-groups)
+               (ibuffer-jump-to-filter-group group)
+               (ibuffer-toggle-filter-group)
+               (ibuffer-map-lines #'(lambda (buf _marks)
+                                      (when (string= (buffer-name buf) name)
+                                        (setq buf-point (point))
+                                        nil))
+                                  t group)
+               (if buf-point
+                 (throw 'found nil)
+                 (ibuffer-toggle-filter-group)))))
+         (if (null buf-point)
+           ;; Still not found even though we expanded all hidden filter
+           ;; groups: that must be because it's hidden by predicate:
+           ;; we won't bother trying to display it.
+           (error "No buffer with name %s" name)
+           (goto-char buf-point)))))
 
    ;; (def-keys-for-map ibuffer-mode-map +vi-essential-keys+)
    (def-keys-for-map ibuffer-mode-map
