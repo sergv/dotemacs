211d06c2ab2164f386a319a92dc44c2d2a505042
Sergey Vinokurov <serg.foo@gmail.com>
2012-12-08 20:07:32 +0200

    improve Python setup, use new python.el, throw away old python-mode

--- src/python/python-setup.el
+++ src/python/python-setup.el
@@ -16,63 +16,131 @@
 (require 'outline-headers)
 (require 'compile)
 
+(require 'python-highlight)
+(require 'python)
+
+(font-lock-add-keywords 'python-mode
+                        *python-font-lock-keywords*
+                        'set)
+(font-lock-add-keywords 'inferior-python-mode
+                        *python-font-lock-keywords*
+                        'set)
 
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
+;; (setf python-indent-offset 4
+;;       ;; ipython setup
+;;       python-shell-buffer-name "python repl"
+;;       python-shell-interpreter "python"
+;;       python-shell-internal-buffer-name " python-repl-internal"
+;;       python-shell-interpreter-args "-i"
+;;
+;;       ;; python-shell-prompt-regexp "> "
+;;       ;; python-shell-prompt-block-regexp ">> "
+;;       ;; python-shell-prompt-output-regexp ""
+;;
+;;       python-shell-enable-font-lock t
+;;
+;;       ;;  python-shell-completion-setup-code
+;;       ;;    "from IPython.core.completerlib import module_completion"
+;;       ;;  python-shell-completion-module-string-code
+;;       ;;    "';'.join(module_completion('''%s'''))\n"
+;;       ;;  python-shell-completion-string-code
+;;       ;;    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+;;
+;;       )
 
-;; (setf py-temp-directory (concat +prog-data-path+ "/python-tmp"))
-;;
-;; (autoload 'python-mode "python-mode" "Pythom mode." t)
-;;
-;; (autoload 'ipython "ipython" "Ipython repl." t)
-;;
-;; (defun python-initialize-vars ()
-;;   (setf py-use-number-face-p nil
-;;         py-underscore-word-syntax-p nil)
-;;
-;;   (setf py-install-directory (concat +emacs-config-path+
-;;                                      "/third-party/python/python-mode")
-;;
-;;         python-python-command "ipython"
-;;         python-python-command-args
-;;         (append python-python-command
-;;                 '("--pprint"
-;;                   "--color-info"
-;;                   "--colors"
-;;                   "Linux"
-;;                   "--nosep"
-;;                   "--no-confirm-exit"
-;;                   "--deep-reload")))
-;;
-;;   (setenv "PYTHONPATH"
-;;           (mapconcat
-;;            #'identity
-;;            (list
-;;             "/home/sergey/projects/python/modules/"
-;;             "/home/sergey/projects/python/webcam/collect-data/local/lib/python2.7/site-packages")
-;;            ":"))
-;;   (setenv "IPYTHONDIR" (concat +prog-data-path+ "/ipython"))
-;;
-;;   ;; pymacs
-;;   ;; (setf pymacs-load-path (list ;; (concat
-;;   ;;                         ;;  py-install-directory
-;;   ;;                         ;;  "/Pymacs")
-;;   ;;                         (concat +emacs-config-path+
-;;   ;;                                 "/src/python/modules")))
-;;   ;; (add-to-list 'load-path (concat +emacs-config-path+
-;;   ;;                                 "/src/python/pymacs"))
-;;   ;; (setenv "PYMACS_PYTHON" "python2.7")
-;;   ;; (setenv "PYMACS_INSTALL_DIR" py-install-directory)
-;;   )
-;;
-;; (python-initialize-vars)
-;;
-;; (eval-after-load
-;;  "python-mode"
-;;  '(progn
-;;    (load-library "python-highlight")
-;;    (load-library "python-customizations")))
+(setf python-indent-offset 4
+      ;; ipython setup
+      python-shell-buffer-name "python repl"
+      python-shell-interpreter "ipython"
+      python-shell-internal-buffer-name " ipython-repl-internal"
+      python-shell-interpreter-args "--pprint --color-info --colors Linux --nosep --no-confirm-exit --deep-reload"
+
+      python-shell-prompt-regexp "> "
+      python-shell-prompt-block-regexp ">> "
+      python-shell-prompt-output-regexp ""
+
+      python-shell-enable-font-lock t
+
+      python-shell-completion-setup-code
+      "from IPython.core.completerlib import module_completion"
+      python-shell-completion-module-string-code
+      "';'.join(module_completion('''%s'''))\n"
+      python-shell-completion-string-code
+      ;; "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"
+      "';'.join(get_ipython().Completer.complete('''%s''')[1]) #PYTHON-MODE SILENT\n"
+      )
+
+(defun python-complete ()
+  "Try to complete the python symbol before point. Only knows about the stuff
+in the current *Python* session."
+  (interactive)
+  (let ((completion-accum nil))
+    (let* ((sep ";")
+           (python-process (or (get-buffer-process (current-buffer))
+                               (get-buffer-process (get-buffer (concat "*" python-shell-buffer-name "*")))
+                               (error "Python REPL not found")
+                               ;;XXX hack for .py buffers
+                               ;; (get-process py-which-bufname)
+                               ))
+           (prompt-beg (overlay-end comint-last-prompt-overlay))
+           ;; XXX currently we go backwards to find the beginning of an
+           ;; expression part; a more powerful approach in the future might be
+           ;; to let ipython have the complete line, so that context can be used
+           ;; to do things like filename completion etc.
+           (beg (save-excursion (skip-chars-backward "a-z0-9A-Z_./" (point-at-bol))
+                                (point)))
+           (end (point))
+           (pattern (buffer-substring-no-properties prompt-beg end))
+           (completions nil)
+           completion
+           (comint-preoutput-filter-functions
+             (append comint-preoutput-filter-functions
+                     `(ansi-color-filter-apply
+                       ,(lambda (string)
+                          (setq completion-accum (concat completion-accum string))
+                          ""))))
+           (completion-command-template
+             "';'.join(get_ipython().Completer.complete('''%s''')[1]) #PYTHON-MODE SILENT\n"))
+      (process-send-string python-process
+                           (format completion-command-template pattern))
+      (accept-process-output python-process)
+      (setq completions
+            (split-string (substring completion-accum
+                                     0
+                                     (position ?\n completion-accum))
+                          sep))
+
+      (cond
+        ((null completions)
+         (error "Can't find completion for \"%s\"" pattern))
+        ((null? (cdr completions))
+         ;; got only one completion
+         (setf completion (car completions)))
+        (t
+         (setf completion (completing-read-vanilla ""
+                                                   completions
+                                                   nil
+                                                   nil
+                                                   pattern))))
+      (if (or (string= pattern completion)
+              (= 0 (length completion)))
+        (error "No completions found for \"%s\"" pattern)
+        (progn
+          (delete-region beg end)
+          (insert completion))))))
+
+
+(setenv "PYTHONPATH"
+        (mapconcat
+         #'identity
+         (list
+          "/home/sergey/projects/python/modules/"
+          "/home/sergey/projects/python/webcam/collect-data/local/lib/python2.7/site-packages")
+         ":"))
+(setenv "IPYTHONDIR" (concat +prog-data-path+ "/ipython"))
 
 ;;;; pylookup
 (add-to-list 'load-path (concat +emacs-config-path+
@@ -127,8 +195,16 @@
                (null (elt state 4)))))))))
 
 (make-align-function python-align-on-equals
-                     "\\(\\+\\|-\\|/\\|\\*\\*?\\|\\)?=[^=]"
+                     "\\(\\+\\|-\\|/\\|\\*\\*?\\)?=[^=]"
                      :require-one-or-more-spaces t)
+
+(defun python-backward-sexp (&optional count)
+  (interactive "p")
+  (python-nav-forward-sexp (- (or count 1))))
+
+(defun python-forward-sexp (&optional count)
+  (interactive "p")
+  (python-nav-forward-sexp (or count 1)))
 
 ;;;; run scripts compilation mode
 
@@ -190,22 +266,20 @@
   (add-hook 'after-save-hook #'make-script-file-exec nil t)
   (init-common :use-yasnippet t
                :use-render-formula t)
-  (setf highlight-indentation nil)
 
   ;; ;; make ' a string delimiter
   ;; (modify-syntax-entry ?' "\"")
-  ;; ;; make _ a symbol constituent
-  ;; (modify-syntax-entry ?_ "_")
+
+  ;; make _ a symbol constituent, mostly for me
+  (modify-syntax-entry ?_ "_")
+  ;; make . a symbol constituent, mostly for me too
+  (modify-syntax-entry ?. "_")
 
   (setf autopair-handle-action-fns
         (list #'autopair-default-handle-action
               #'autopair-python-triple-quote-action))
   (autopair-mode 1)
   (hs-minor-mode 1)
-
-  ;; (setq-local hs-set-up-overlay
-  ;;             (lambda (overlay)
-  ;;               (overlay-put ov 'display "...")))
 
   (setf vim:normal-mode-local-keymap           (make-keymap)
         vim:visual-mode-local-keymap           (make-sparse-keymap)
@@ -216,8 +290,11 @@
   (def-keys-for-map vim:normal-mode-local-keymap
     (", d"     pylookup-lookup)
     (", ?"     pylookup-lookup)
-    ("<f1>"    py-execute-buffer-no-switch)
+    ("<f1>"    python-shell-send-buffer)
     ("<f9>"    python-run-script)
+    ("S-<f9>"  python-check)
+
+    ("j"       python-shell-send-defun)
 
     ("SPC SPC" switch-to-python)
     (", s s"   vim:replace-symbol-at-point)
@@ -227,48 +304,61 @@
     ("z C"     python-hide-all)
     ("z O"     hs-show-all))
 
+  (def-keys-for-map vim:visual-mode-local-keymap
+    ("<f1>"  python-shell-send-region)
+    ("j"     python-shell-send-region)
+    ("g a"   nil)
+    ("g a =" python-align-on-equals))
+
+  (def-keys-for-map (vim:normal-mode-local-keymap
+                     vim:visual-mode-local-keymap)
+    ("g t"    end-of-defun)
+    ("g n"    beginning-of-defun)
+    ("<up>"   python-nav-backward-block)
+    ("<down>" python-nav-forward-block)
+
+    ("="      python-nav-backward-up-list)
+    ("q"      python-nav-up-list)
+    ("<home>" python-backward-sexp)
+    ("<end>"  python-forward-sexp)
+
+    ("*"      search-for-symbol-at-point-forward)
+    ("#"      search-for-symbol-at-point-backward))
+
+  (python-abbrev+-setup)
+
   ;; pabbrev isn't powerful enough
   ;; (pabbrev-mode 1)
   ;; (def-keys-for-map (vim:normal-mode-local-keymap
   ;;                     vim:insert-mode-local-keymap)
   ;;   ("M-/"     pabbrev-show-menu ;; pabbrev-expand-maybe
   ;;              ))
-
-  (def-keys-for-map (vim:normal-mode-local-keymap
-                     vim:insert-mode-local-keymap)
-    ("M-/"     ipython-complete))
-
-  (def-keys-for-map (vim:normal-mode-local-keymap
-                     vim:visual-mode-local-keymap)
-    ("g t" py-end-of-def-or-class)
-    ("g n" py-beginning-of-def-or-class)
-
-    ("*"   search-for-symbol-at-point-forward)
-    ("#"   search-for-symbol-at-point-backward))
-
-  (def-keys-for-map vim:visual-mode-local-keymap
-    ("<f1>"  py-execute-region-no-switch)
-    ("j"     py-execute-region-no-switch)
-    ("g a"   nil)
-    ("g a =" python-align-on-equals))
-
-  (python-abbrev+-setup)
-  (when pabbrev-mode
-    (pabbrev-scavenge-buffer))
+  ;; (when pabbrev-mode
+  ;;   (pabbrev-scavenge-buffer))
 
   (setup-outline-headers :header-start "^[ \t]*"
                          :header-symbol "#"
                          :length-min 3
                          :length-max 9))
 
+;; (define-switch-to-interpreter
+;;     switch-to-python
+;;   ("*IPython*")
+;;   (ipython)
+;;   :doc "Pop to ipython repl."
+;;   :save-buffer t
+;;   :error-msg "Can't switch to IPython repl"
+;;   :try-count 2)
+
 (define-switch-to-interpreter
     switch-to-python
-  ("*IPython*")
-  (ipython)
-  :doc "Pop to ipython repl."
+  ((concat "*" python-shell-buffer-name "*"))
+  (run-python (python-shell-assemble-command))
+  :doc "Pop to python repl."
   :save-buffer t
-  :error-msg "Can't switch to IPython repl"
+  :error-msg "Can't switch to python repl"
   :try-count 2)
+
 
 ;; (define-switch-to-interpreter
 ;;     switch-to-python
@@ -279,12 +369,13 @@
 ;;   :error-msg "Can't switch to Python repl"
 ;;   :try-count 2)
 
-(defun python-interpreter-setup ()
+(defun inferior-python-setup ()
   (init-common :use-yasnippet nil
                :use-comment nil
                :use-nxhtml-menu nil)
   (init-repl)
   (comint-setup)
+  (comint-read-input-ring t)
 
   ;; make ' a string delimiter
   (modify-syntax-entry ?' "\"")
@@ -294,10 +385,10 @@
   ;; python repl so it's best to avoid them altogether
   ;; moreover I virtually never use triple quotes at the repl
 
-  ;; (setf autopair-handle-action-fns
-  ;;       (list #'autopair-default-handle-action
-  ;;             #'autopair-python-triple-quote-action))
-  (autopair-mode t)
+  (setf autopair-handle-action-fns
+        (list #'autopair-default-handle-action
+              #'autopair-python-triple-quote-action))
+  (autopair-mode 1)
 
   (setf tab-width 4)
 
@@ -319,12 +410,15 @@
     ("C-<up>"   comint-previous-prompt)
     ("C-<down>" comint-next-prompt)
     ("S-<up>"   comint-previous-prompt)
-    ("S-<down>" comint-next-prompt))
+    ("S-<down>" comint-next-prompt)
+
+    ("<tab>"    python-complete))
 
   (def-keys-for-map vim:normal-mode-local-keymap
     ("SPC SPC"  comint-clear-prompt)))
 
-;; (add-hook 'python-mode-hook #'python-setup)
+(add-hook 'python-mode-hook #'python-setup)
+(add-hook 'inferior-python-mode-hook #'inferior-python-setup)
 ;; (add-hook 'py-shell-hook #'python-interpreter-setup)
 
 ;; this would be run in py-shell-hook anyway
