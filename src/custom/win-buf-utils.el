48f9fc15be364a95613d2e26a18edbf9b8951005
Sergey Vinokurov <serg.foo@gmail.com>
2012-12-15 15:40:58 +0200

    improve swap-buffers-forward/swap-buffers-backward to handle case of switching between windows with the same buffer

--- src/custom/win-buf-utils.el
+++ src/custom/win-buf-utils.el
@@ -35,30 +35,58 @@ the current buffer."
 (defun swap-buffers-forward ()
   "Swap current buffer with next buffer"
   (interactive)
-  (let* ((current-win (selected-window))
-         (current-buf (current-buffer))
-         (next-win (next-window current-win 0))
-         (next-buffer
-           (save-selected-window
-            (select-window next-win t)
-            (current-buffer))))
-    (switch-to-buffer next-buffer)
-    (select-window next-win)
-    (switch-to-buffer current-buf)))
+  (let* ((curr-win (selected-window))
+         (curr-buffer (current-buffer))
+         (curr-pos (with-selected-window curr-win
+                     (with-current-buffer curr-buffer
+                       (point))))
+         (next-win (next-window curr-win 0))
+         (next-buffer (save-selected-window (select-window next-win t)
+                                            (current-buffer)))
+         (next-pos (with-selected-window next-win
+                     (with-current-buffer next-buffer
+                       (point)))))
+    (if (eq? next-buffer curr-buffer)
+      (begin
+        (with-selected-window curr-win
+          (with-current-buffer curr-buffer
+            (goto-char next-pos)))
+        (select-window next-win)
+        (with-selected-window next-win
+          (with-current-buffer next-buffer
+            (goto-char curr-pos))))
+      (begin
+        (switch-to-buffer next-buffer)
+        (select-window next-win)
+        (switch-to-buffer curr-buffer)))))
 
 (defun swap-buffers-backward ()
   "Swap current buffer with previous buffer"
   (interactive)
-  (let* ((current-win (selected-window))
-         (current-buf (current-buffer))
-         (prev-win (previous-window current-win 0))
-         (prev-buffer
-           (save-selected-window
-            (select-window prev-win t)
-            (current-buffer))))
-    (switch-to-buffer prev-buffer)
-    (select-window prev-win)
-    (switch-to-buffer current-buf)))
+  (let* ((curr-win (selected-window))
+         (curr-buffer (current-buffer))
+         (curr-pos (with-selected-window curr-win
+                     (with-current-buffer curr-buffer
+                       (point))))
+         (prev-win (previous-window curr-win 0))
+         (prev-buffer (save-selected-window (select-window prev-win t)
+                                            (current-buffer)))
+         (prev-pos (with-selected-window prev-win
+                     (with-current-buffer prev-buffer
+                       (point)))))
+    (if (eq? prev-buffer curr-buffer)
+      (begin
+        (with-selected-window curr-win
+          (with-current-buffer curr-buffer
+            (goto-char prev-pos)))
+        (select-window prev-win)
+        (with-selected-window prev-win
+          (with-current-buffer prev-buffer
+            (goto-char curr-pos))))
+      (begin
+        (switch-to-buffer prev-buffer)
+        (select-window prev-win)
+        (switch-to-buffer curr-buffer)))))
 
 
 ;; Local Variables:
