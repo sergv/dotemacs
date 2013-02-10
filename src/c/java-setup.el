3a4d4bbdbef9ac70a8886dcc92df6c01773a604a
Sergey Vinokurov <serg.foo@gmail.com>
2013-02-10 16:20:45 +0200

    define map-files function; add java-indent-buffer, change other indentations to use constant temporary filename

--- src/c/java-setup.el
+++ src/c/java-setup.el
@@ -6,8 +6,38 @@
 ;; Created: Thursday, 17 January 2013
 ;; Description:
 
+(require 'common)
 (require 'cc-setup)
 (require 'ctags-setup)
+
+(defun java-indent-buffer ()
+  (interactive)
+  (unless (executable-find "astyle")
+    (error "Command astyle is not available"))
+  (let ((file +buffer-indent-temporary-filename+))
+    (write-region (point-min) (point-max) file)
+    (erase-buffer)
+    (shell-command
+     (mapconcat #'identity
+                (list "astyle"
+                      "--style=java"
+                      "--indent=spaces=4"
+                      "--brackets=attach"
+                      "--pad-oper"
+                      "--pad-header"
+                      "--unpad-paren"
+                      "--keep-one-line-statements"
+                      "--keep-one-line-blocks"
+                      "--convert-tabs"
+                      "--align-pointer=name"
+                      "--mode=java"
+                      "--suffix=none"
+                      "--lineend=linux"
+                      (format "<%s" file))
+                " ")
+     (current-buffer))))
+
+(push (cons 'java-mode #'java-indent-buffer) *mode-buffer-indent-function-alist*)
 
 (defun java-setup ()
   (cc-setup :define-special-keys nil)
