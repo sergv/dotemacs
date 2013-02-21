a663db3bd08912231291be661e8b67c484a56e37
Sergey Vinokurov <serg.foo@gmail.com>
2013-02-21 21:47:06 +0200

    add persistent-sessions.el and setup for them; add setup for revive; change util:construct-ex-replace-command to use , instead of / as a delimiter;

---
+++ src/revive-setup.el
@@ -0,0 +1,27 @@
+;; revive-setup.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: Tuesday, 22 January 2013
+;; Description:
+
+(eval-when-compile
+  (require 'cl))
+
+(require 'revive+)
+(require 'revive)
+
+(setf revive-plus:all-frames t
+      revive-plus:wconf-archive-file (concat +emacs-config-path+ "/wconf-archive")
+      revive-plus:last-wconf-file (concat +emacs-config-path+ "/last-wconf"))
+
+(setf revive:save-variables-mode-local-private
+      '((c++-mode c-indentation-style c-basic-offset)))
+
+(provide 'revive-setup)
+
+;; Local Variables:
+;; End:
+
+;; revive-setup.el ends here
