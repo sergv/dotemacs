b8195a8a66256b98b9aefe1fe0c276fe03062b77
Sergey Vinokurov <serg.foo@gmail.com>
2013-04-23 00:11:45 +0300

    enhanced preprocessor directive folding by hideshow for c modes

--- src/c/cc-setup.el
+++ src/c/cc-setup.el
@@ -6,8 +6,11 @@
 ;; Created: Monday,  8 October 2012
 ;; Description:
 
+(eval-when-compile (require 'cl-lib))
+
 (require 'custom)
 
+(require 'more-clojure)
 (require 'c-eldoc)
 (require 'ctypes)
 (require 'find-file)
@@ -22,31 +25,118 @@
                      "\\([+*|&/!%]\\|-\\|\\^\\)?=[^=]"
                      :require-one-or-more-spaces t)
 
+(defun c-forward-preprocessor-conditional (count)
+  "Clone of `c-scan-conditionals' with interface of `forward-sexp' for matching
+#if with #else and #else with #endif."
+  (save-match-data
+    (let* ((forward (> count 0))
+           (increment (if forward -1 1))
+           (search-function (if forward 're-search-forward 're-search-backward))
+           (target-depth 0)
+           new
+           case-fold-search)
+      (save-excursion
+        (while (/= count 0)
+          (let ((depth 0)
+                (found nil))
+            (save-excursion
+              ;; Find the "next" significant line in the proper direction.
+              (while (and (not found)
+                          ;; Rather than searching for a # sign that
+                          ;; comes at the beginning of a line aside from
+                          ;; whitespace, search first for a string
+                          ;; starting with # sign.  Then verify what
+                          ;; precedes it.  This is faster on account of
+                          ;; the fastmap feature of the regexp matcher.
+                          (funcall search-function
+                                   "#[ \t]*\\(if\\|elif\\|endif\\|else\\)"
+                                   nil t))
+                (message "depth: %s, line: %s"
+                         (pp-to-string depth)
+                         (current-line))
+                (beginning-of-line)
+                ;; Now verify it is really a preproc line.
+                (if (looking-at "^[ \t]*#[ \t]*\\(if\\|elif\\|endif\\|else\\)")
+                  (let ((dchange nil)
+                        (directive (match-string 1)))
+                    (cond ((or (string= directive "if")
+                               (string= directive "ifdef")
+                               (string= directive "ifndef"))
+                           (setq dchange (- increment)))
+                          ((string= directive "endif")
+                           (setq dchange increment))
+                          ((= depth 0)
+                           (setq dchange +1))
+                          ((= depth +1)
+                           (setq dchange -1)))
+                    (when dchange
+                      (message "dchange: %s, target-depth: %s"
+                               (pp-to-string dchange)
+                               (pp-to-string target-depth))
+                      (setq depth (+ depth dchange))
+                      ;; If we are trying to move across, and we find an
+                      ;; end before we find a beginning, get an error.
+                      (if (and (< depth target-depth) (< dchange 0))
+                        (error (concat (if forward
+                                         "No following conditional at this level"
+                                         "No previous conditional at this level")
+                                       ", depth = %s, target-depth = %s")
+                               depth
+                               target-depth)))
+                    ;; When searching forward, start from next line so
+                    ;; that we don't find the same line again.
+                    (if forward (forward-line 1))
+                    ;; We found something if we've arrived at the
+                    ;; target depth.
+                    (if (and dchange (= depth target-depth))
+                      (setq found (point))))
+                  ;; else
+                  (if forward (forward-line 1)))
+                (message "after: depth: %s"
+                         (pp-to-string depth))))
+            (or found
+                (error "No containing preprocessor conditional"))
+            (goto-char (setq new found)))
+          (setq count (+ count increment))))
+      (c-keep-region-active)
+      (goto-char new))))
+
+(defconst +c-preprocessor-directives-re+
+  (rx (seq bol
+           "#"
+           (* (syntax whitespace))
+           symbol-start
+           (or "ifdef"
+               "ifndef"
+               "if"
+               "elif"
+               "endif"
+               "else")
+           symbol-end)))
+
 (defun c-hideshow-forward-sexp (&optional arg)
   "Special version of `forward-sexp' for hideshow in c-mode."
   (if (char=? (char-after) ?\{)
     (forward-sexp arg)
-    (let ((on-start? (looking-at-pure? (rx (seq "#"
-                                                (* (syntax whitespace))
-                                                symbol-start
-                                                (or "ifdef"
-                                                    "ifndef"
-                                                    "if")
-                                                symbol-end)))))
-      (c-forward-conditional (or arg 1))
-      (when on-start?
-        (forward-char -1)))))
+    (let ((start (point)))
+      (save-match-data
+        (c-forward-preprocessor-conditional (or arg 1))
+        (re-search-backward +c-preprocessor-directives-re+ start t)))))
 
-(let ((hs-spec (list (rx (or (seq "#"
-                                  (* (syntax whitespace))
-                                  symbol-start
-                                  (or "ifdef"
-                                      "ifndef"
-                                      "if"
-                                      "endif"
-                                      "else")
-                                  symbol-end)
-                             "{"))
+(let ((hs-spec (list
+                ;; This is like `+c-preprocessor-directives-re+' but does not
+                ;; include ending directives like endif.
+                (rx (or (seq bol
+                             "#"
+                             (* (syntax whitespace))
+                             symbol-start
+                             (or "ifdef"
+                                 "ifndef"
+                                 "if"
+                                 "elif"
+                                 "else")
+                             symbol-end)
+                        "}"))
                      nil
                      "/[*/]"
                      #'c-hideshow-forward-sexp
@@ -54,8 +144,8 @@
   (setf hs-special-modes-alist
         (cons `(c-mode ,hs-spec)
               (cons `(c++-mode ,hs-spec)
-                    (remove-if (lambda (entry)
-                                 (memq (car entry) '(c-mode c++-mode)))
+                    (remove-if (comp (partial-first #'memq '(c-mode c++-mode))
+                                     #'first)
                                hs-special-modes-alist)))))
 
 
