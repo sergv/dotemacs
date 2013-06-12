bf92e43d7f8d06ffff875a8bd47220345661d323
Sergey Vinokurov <serg.foo@gmail.com>
2013-06-12 23:56:10 +0300

    defined dedicated variable enumerating version-controlled directories

--- src/common.el
+++ src/common.el
@@ -146,20 +146,13 @@ of random numbers from RANDOM-GEN."
   (remove-if (lambda (x) (member (file-name-nondirectory x) '("." "..")))
              (directory-files dir full)))
 
-(defun version-control-directory? (filename)
-  (string-match-pure? (rx (or "SCCS"
-                              "RCS"
-                              "CVS"
-                              "MCVS"
-                              ".svn"
-                              ".git"
-                              ".hg"
-                              ".bzr"
-                              "_MTN"
-                              "_darcs"
-                              "{arch}")
-                          (? "/"))
-                      filename))
+(defun version-control-directory? (filepath)
+  "Test whether FILEPATH contains version control directory as its subpart"
+  (string-match-pure? (concat "\\(?:"
+                              (regexp-opt
+                               *version-control-directories*)
+                              "\\)\\(?:/\\|$\\)")
+                      filepath))
 
 (defun* find-rec (path
                   &key
@@ -851,9 +844,14 @@ end of END-LINE in current buffer."
   '(".annot" ".cmi" ".cmxa" ".cma" ".cmx" ".cmo" ".o" "~" ".bin" ".out" ".exe" ".prof" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo" ".hi")
   "List of file name endings to generally ignore.")
 
+(defvar *version-control-directories*
+  '("SCCS" "RCS" "CVS" "MCVS"
+    ".svn" ".git" ".hg" ".bzr"
+    "_MTN" "_darcs" "{arch}")
+  "List of directory names used by version-control systems.")
 
 (defvar *ignored-directories*
-  '(".svn" ".hg" ".git" ".bzr" "CVS" "_darcs" "_MTN")
+  (append *version-control-directories*)
   "List of directory names to generally ignore.")
 
 (setf completion-ignored-extensions
