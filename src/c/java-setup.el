707519a48407b0bdf9bc4d9b86f46747339053c9
Sergey Vinokurov <serg.foo@gmail.com>
2013-06-12 23:57:41 +0300

    defined util/reindent-file utility function for indenting file according to inferred mode without creating special buffer; redefined java indentation to use customized astyle executable that provides non-standard indentation style (i.e. it does not indents toplevel classes and interfaces);

--- src/c/java-setup.el
+++ src/c/java-setup.el
@@ -14,36 +14,32 @@
 
 (defun java-indent-buffer ()
   (interactive)
-  (save-excursion
-    (indent-whole-buffer)))
-
-;; (defun java-indent-buffer ()
-;;   (interactive)
-;;   (unless (executable-find "astyle")
-;;     (error "Command astyle is not available"))
-;;   (save-current-line-column
-;;    (let ((file +buffer-indent-temporary-filename+))
-;;      (write-region (point-min) (point-max) file)
-;;      (erase-buffer)
-;;      (shell-command
-;;       (join-lines (list "astyle"
-;;                         "--style=java"
-;;                         "--indent=spaces=4"
-;;                         "--indent-classes"
-;;                         "--brackets=attach"
-;;                         "--pad-oper"
-;;                         "--pad-header"
-;;                         "--unpad-paren"
-;;                         "--keep-one-line-statements"
-;;                         "--keep-one-line-blocks"
-;;                         "--convert-tabs"
-;;                         "--align-pointer=name"
-;;                         "--mode=java"
-;;                         "--suffix=none"
-;;                         "--lineend=linux"
-;;                         (format "<%s" file))
-;;                   " ")
-;;       (current-buffer)))))
+  ;; (unless (executable-find "astyle")
+  ;;   (error "Command astyle is not available"))
+  (save-current-line-column
+   (let ((file +buffer-indent-temporary-filename+))
+     (write-region (point-min) (point-max) file)
+     (erase-buffer)
+     (shell-command
+      (join-lines (list (concat +emacs-config-path+ "/tmp/astyle.custom")
+                        "--style=java"
+                        "--indent=spaces=4"
+                        "--brackets=attach"
+                        "--pad-oper"
+                        "--pad-header"
+                        "--unpad-paren"
+                        "--keep-one-line-statements"
+                        "--keep-one-line-blocks"
+                        "--convert-tabs"
+                        "--align-pointer=name"
+                        "--mode=java"
+                        "--suffix=none"
+                        "--lineend=linux"
+                        ;; "--indent-namespaces" ;; uncomment to indent toplevel
+                        ;;                       ;; classes or interfaces
+                        (format "<%s" file))
+                  " ")
+      (current-buffer)))))
 
 (defun java-setup ()
   (cc-setup :define-special-keys nil)
