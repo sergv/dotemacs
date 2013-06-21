178d618cdaa16101111cc452c6cf06f50a954664
Sergey Vinokurov <serg.foo@gmail.com>
2013-06-21 23:27:20 +0300

    added setup for antlr grammars

---
+++ src/antlr-setup.el
@@ -0,0 +1,27 @@
+;; antlr-setup.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: Friday, 21 June 2013
+;; Description:
+
+(autoload 'antlr-mode "antlr-mode" nil t)
+
+(add-to-list 'auto-mode-alist '("\\.g\\'" . antlr-mode))
+
+(setf antlr-font-lock-maximum-decoration t)
+
+(defun antlr-setup ()
+  (init-common :use-yasnippet nil :use-render-formula nil)
+  (autopair-mode t)
+  (hs-minor-mode t))
+
+(add-hook 'antlr-mode-hook #'antlr-setup)
+
+(provide 'antlr-setup)
+
+;; Local Variables:
+;; End:
+
+;; antlr-setup.el ends here
