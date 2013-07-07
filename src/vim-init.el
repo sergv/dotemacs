8f98b470abb04e45a92a8c891c7bd410a52e8c63
Sergey Vinokurov <serg.foo@gmail.com>
2013-07-07 21:45:04 +0300

    tweaked icicles completion; added advice for set-window-buffer to keep track of past buffers for every window; defined switch-to-prev-buffer-in-window function and bound it to "C-b";

--- src/vim-init.el
+++ src/vim-init.el
@@ -96,7 +96,7 @@ like \"d w\".")
 
 
         ("C-u"     undo-tree-visualize)
-        ("C-b"     icicle-buffer)
+        ("M-b"     icicle-buffer)
 
         ("S-<backspace>" delete-whitespaces-backward)
         ("S-<delete>"    delete-whitespaces-forward)
