d2d4811f885006b1201363f2705aa6da1b8c274e
Sergey Vinokurov <serg.foo@gmail.com>
2013-09-09 00:28:10 +0300

    fix haskell syntax of ' and @; add haskell smartparens definitions; update haskell-autoload.el;

--- src/haskell/haskell-autoload.el
+++ src/haskell/haskell-autoload.el
@@ -6,15 +6,19 @@
 ;; Created: Thursday, 12 July 2012
 ;; Description:
 
-(load "haskell-site-file")
-;; (load (concat +emacs-config-path+ "/src/haskell/haskell-mode/haskell-site-file.el"))
+(load "haskell-autoloads-generated")
 
 (autoload 'haskell-setup "haskell-setup" "" nil nil)
 (autoload 'inferior-haskell-mode-setup "haskell-setup" "" nil nil)
 
-(add-to-list 'auto-mode-alist '("\\.hcr\\'" . ghc-core-mode))
-(add-to-list 'auto-mode-alist '("\\.hsc\\'" . haskell-c-mode))
-(add-to-list 'auto-mode-alist '("\\.chs\\'" . haskell-c-mode))
+(add-to-list 'auto-mode-alist        '("\\.hcr\\'" . ghc-core-mode))
+(add-to-list 'auto-mode-alist        '("\\.cabal\\'" . haskell-cabal-mode))
+(add-to-list 'auto-mode-alist        '("\\.\\(?:[gh]s\\|hi\\)\\'" . haskell-mode))
+(add-to-list 'auto-mode-alist        '("\\.l[gh]s\\'" . literate-haskell-mode))
+(add-to-list 'interpreter-mode-alist '("runghc" . haskell-mode))
+(add-to-list 'interpreter-mode-alist '("runhaskell" . haskell-mode))
+(add-to-list 'auto-mode-alist        '("\\.hsc\\'" . haskell-c-mode))
+(add-to-list 'auto-mode-alist        '("\\.chs\\'" . haskell-c-mode))
 
 (defalias 'ghci 'switch-to-haskell)
 
