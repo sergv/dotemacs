dc316b45c76ef972197c0ab5bce7e0c881066e45
Sergey Vinokurov <serg.foo@gmail.com>
2013-10-13 00:10:38 +0300

    add setups for verilog, vhdl and tcl

---
+++ src/tcl-setup.el
@@ -0,0 +1,25 @@
+;; tcl-setup.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: Saturday, 12 October 2013
+;; Description:
+
+(require 'common)
+
+(defun tcl-setup ()
+  (init-common :use-yasnippet t
+               :use-comment t
+               :use-render-formula t
+               :use-whitespace t
+               :sp-slurp-sexp-insert-space t))
+
+(add-hook 'vhdl-mode-hook #'tcl-setup)
+
+(provide 'tcl-setup)
+
+;; Local Variables:
+;; End:
+
+;; tcl-setup.el ends here
