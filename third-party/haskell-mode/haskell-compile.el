f12176bf6485b12187ce99fef86cc31b1d3f6cf8
Sergey Vinokurov <serg.foo@gmail.com>
2013-10-23 23:55:24 +0300

    Tweak filename and warning recognition for compilation regexps
    For example, ghc 7.4 outputs "Warning:" on different line than filename itself
    and previous regexps do not handle it:
    /home/hackerjoe/HelloWorld.hs:23:1-28:
        Warning: The import of `Control.Monad' is redundant
                   except perhaps to import instances from `Control.Monad'
                 To import instances alone, use: import Control.Monad()
    
    Of course, having spaces in filenames isn't the best idea, but context of
    compilation regexps seems to allow reliable filename recognition since
    filenames are given as full paths and beginning of filename part is either bol
    or space character.

--- third-party/haskell-mode/haskell-compile.el
+++ third-party/haskell-mode/haskell-compile.el
@@ -74,11 +74,11 @@ The `%s' placeholder is replaced by the current buffer's filename."
      1 (2 . 3) (4 . 5) (6 . nil)) ;; error/warning locus
 
     ;; multiple declarations
-    ("^    \\(?:Declared at:\\|            \\) \\(?1:[^ \t\r\n]+\\):\\(?2:[0-9]+\\):\\(?4:[0-9]+\\)$"
+    ("^    \\(?:Declared at:\\|            \\) \\(?1:.+?\\.\\(?:hs\\|lhs\\|hsc\\|c2hs\\|x\\|y\\)\\):\\(?2:[0-9]+\\):\\(?4:[0-9]+\\)$"
      1 2 4 0) ;; info locus
 
     ;; this is the weakest pattern as it's subject to line wrapping et al.
-    (" at \\(?1:[^ \t\r\n]+\\):\\(?2:[0-9]+\\):\\(?4:[0-9]+\\)\\(?:-\\(?5:[0-9]+\\)\\)?[)]?$"
+    (" at \\(?1:.+?\\.\\(?:hs\\|lhs\\|hsc\\|c2hs\\|x\\|y\\)\\):\\(?2:[0-9]+\\):\\(?4:[0-9]+\\)\\(?:-\\(?5:[0-9]+\\)\\)?[)]?$"
      1 2 (4 . 5) 0)) ;; info locus
   "Regexps used for matching GHC compile messages.
 See `compilation-error-regexp-alist' for semantics.")
