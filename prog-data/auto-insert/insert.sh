e6cf0efa3f4f013613627f73df74bc71755bc0d3
Sergey Vinokurov <serg.foo@gmail.com>
2013-11-11 23:22:51 +0159

    add new tricks to bash script autoinsert template

--- prog-data/auto-insert/insert.sh
+++ prog-data/auto-insert/insert.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
+#! /usb/bin/env bash
 #
 # File: ${filename}
 #
 # Created: ${date}
 #
+
+# treat undefined variable substitutions as errors
+set -u
+# propagate errors from all parts of pipes
+set -o pipefail
 
 
 exit 0
