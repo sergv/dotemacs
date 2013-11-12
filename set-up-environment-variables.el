3450739a6107ba56c034e31dce6499ccbc746357
Sergey Vinokurov <serg.foo@gmail.com>
2013-11-12 21:25:38 +0159

    changes set-up-environment-variables.el to use all defined variables

--- set-up-environment-variables.el
+++ set-up-environment-variables.el
@@ -14,36 +14,16 @@
 (let ((env-config-file (concat (platform-dependent-root) "/.bash_env")))
   (when (file-exists-p env-config-file)
     (save-match-data
-      (let* ((variables
-              '("PATH"
-                "LD_LIBRARY_PATH"
-                "PYTHONPATH"
-                "EDITOR"
-                "INFOPATH"
-                "MANPATH"
-                "PKG_CONFIG_PATH"
-                "JAVA_HOME"
-                "ANDROID_HOME"
-                "ANDROID_SDK"
-                "SDK_ROOT"
-                "SDK_HOME"
-                "NDK_HOME"
-                "ANDROID_NDK"
-                "LEIN_JAVA_CMD"
-                "CCACHE_BASEDIR"
-                "CCACHE_COMPRESS"
-                "CCACHE_DIR"))
-             ;; shell is expected to be a bash shell
-             (all-values (shell-command-to-string
-                          (format ". %s; printenv --null;"
-                                  env-config-file))))
+      (let ( ;; shell is expected to be a bash shell
+            (all-values (shell-command-to-string
+                         (format ". %s; printenv --null;"
+                                 env-config-file))))
         (dolist (entry (split-string all-values "[\0]" t))
           (let ((eq-pos (cl-position ?\= entry)))
             (when eq-pos
               (let ((var (substring entry 0 eq-pos)))
-                (when (member var variables)
-                  (setenv var
-                          (substring entry (+ eq-pos 1))))))))))))
+                (setenv var
+                        (substring entry (+ eq-pos 1)))))))))))
 
 
 (defun* env-var-into-list (env-var list &key (append t))
