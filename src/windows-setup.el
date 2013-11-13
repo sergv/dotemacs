306cca4ec49179ae14066661d2fb1d4f0bc6de75
Sergey Vinokurov <serg.foo@gmail.com>
2013-11-13 22:57:29 +0159

    add public windows-specific and private machine-specific setups

---
+++ src/windows-setup.el
@@ -0,0 +1,23 @@
+;; windows-setup.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: Wednesday, 13 November 2013
+;; Description:
+
+(require 'common)
+
+(assert (platform-os-type? 'windows) "Not on windows platform!")
+
+(require 'cygwin-mount)
+(cygwin-mount-activate)
+
+(add-to-list 'exec-suffixes ".sh")
+
+(provide 'windows-setup)
+
+;; Local Variables:
+;; End:
+
+;; windows-setup.el ends here
