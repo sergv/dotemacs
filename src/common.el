460b28400c03c2286fd711b52420ca500af2c77d
Sergey Vinokurov <serg.foo@gmail.com>
2014-01-01 21:03:15 +0200

    ignore .dex files

--- src/common.el
+++ src/common.el
@@ -886,7 +886,7 @@ end of END-LINE in current buffer."
 ;;;;
 
 (defvar *ignored-file-name-endings*
-  '(".annot" ".cmi" ".cmxa" ".cma" ".cmx" ".cmo" ".o" ".p_o" ".p_hi" ".prof_o" ".hi" "~" ".bin" ".out" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo" ".dll" ".pdb")
+  '(".annot" ".cmi" ".cmxa" ".cma" ".cmx" ".cmo" ".o" ".p_o" ".p_hi" ".prof_o" ".hi" "~" ".bin" ".out" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo" ".dll" ".pdb" ".dex")
   "List of file name endings to generally ignore.")
 
 (defvar *version-control-directories*
