772152c14c77bbc749ca7a54bc6041fa14401963
Sergey Vinokurov <serg.foo@gmail.com>
2014-01-05 23:05:06 +0200

    do not overwrite SHELL environment variable

--- src/shell/shell-setup.el
+++ src/shell/shell-setup.el
@@ -75,11 +75,12 @@
 
   (setf comint-scroll-to-bottom-on-input t))
 
-(when (and (platform-os-type? 'windows)
-           (platform-use? 'work)
-           (file-exists? "C:/GnuWin32/bin/bash.exe"))
-  (setf shell-file-name "C:/GnuWin32/bin/bash.exe")
-  (setenv "SHELL" shell-file-name))
+(cond ((and (platform-os-type? 'windows)
+            (platform-use? 'work)
+            (file-exists? "C:/GnuWin32/bin/bash.exe"))
+       (setf shell-file-name "C:/GnuWin32/bin/bash.exe"))
+      ((null? (getenv "SHELL"))
+       (setenv "SHELL" shell-file-name)))
 
 (provide 'shell-setup)
 
