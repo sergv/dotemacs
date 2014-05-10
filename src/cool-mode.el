2997d13671d9617215db8e7bfaedef0883c90991
Sergey Vinokurov <serg.foo@gmail.com>
2014-05-11 00:36:38 +0300

    add mode for Cool language

---
+++ src/cool-mode.el
@@ -0,0 +1,94 @@
+;; cool-mode.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: Thursday,  8 May 2014
+;; Description:
+
+(require 'common)
+
+(defvar cool-mode-syntax-table
+  (let ((tbl (make-syntax-table)))
+    (modify-syntax-entry ?\" "\""    tbl)
+    ;; ensure that (*/*) are comments
+    (modify-syntax-entry ?\( "()1nb" tbl)
+    (modify-syntax-entry ?\) ")(4nb" tbl)
+    (modify-syntax-entry ?* ". 23"   tbl)
+    (modify-syntax-entry ?- ". 123"  tbl)
+    (modify-syntax-entry ?! "."      tbl)
+    (modify-syntax-entry ?~ "."      tbl)
+    (modify-syntax-entry ?+ "."      tbl)
+    (modify-syntax-entry ?- "."      tbl)
+    (modify-syntax-entry ?= "."      tbl)
+    (modify-syntax-entry ?% "."      tbl)
+    (modify-syntax-entry ?< "."      tbl)
+    (modify-syntax-entry ?> "."      tbl)
+    (modify-syntax-entry ?& "."      tbl)
+    (modify-syntax-entry ?| "."      tbl)
+    (modify-syntax-entry ?@ "."      tbl)
+    (modify-syntax-entry ?_ "_"      tbl)
+    tbl))
+
+(setf cool-font-lock-keywords
+  `((,(rx (or "isvoid"
+              "not"))
+     (0 font-lock-builtin-face))
+    (,(rx (or (group-n 1 "SELF_TYPE")
+              (seq (or ":"
+                       "@"
+                       "class"
+                       "inherits"
+                       "new")
+                   (* whitespace)
+                   (group-n 1 (regex "[A-Z][A-Za-z0-9_]*")))))
+     (1 font-lock-type-face))
+    (,(rx bow
+          (or
+           ;; keywords
+           "class"
+           "inherits"
+           "new"
+           ;; statements
+           "self"
+           "let"
+           "in"
+           ;; conditionals
+           "if"
+           "then"
+           "else"
+           "fi"
+           "case"
+           "esac"
+           "of"
+           ;; looping
+           "while"
+           "loop"
+           "pool"
+           ;; booleans
+           "true"
+           "false")
+          eow)
+     (0 font-lock-keyword-face))
+    (,(rx symbol-start
+          (? (or "-" "+"))
+          (+ (regex "[0-9]"))
+          symbol-end)
+     (0 font-lock-constant-face))))
+
+(define-derived-mode cool-mode prog-mode "Cool"
+  "Major mode to edit .gitconfig files"
+  (setq-local comment-start "--+")
+  ;; (setq-local comment-end "\\*)")
+  (setq-local comment-start-skip "(\\*+ *\\|--+ *")
+
+  (setq-local indent-tabs-mode nil)
+  (setq-local font-lock-defaults
+              '(cool-font-lock-keywords
+                nil ;; perform syntactic fontification (e.g. strings, comments)
+                )))
+
+;; Local Variables:
+;; End:
+
+;; cool-mode.el ends here
