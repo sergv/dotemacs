6908746559ccddef34111ce7367d40311060d608
Sergey Vinokurov <serg.foo@gmail.com>
2014-05-28 00:09:40 +0300

    factor out happy-mode

--- src/haskell/haskell-grammar-tools-setup.el
+++ src/haskell/haskell-grammar-tools-setup.el
@@ -8,74 +8,18 @@
 
 (require 'common)
 (require 'mmm-auto)
+(require 'happy-mode-autoload)
 
 (setf mmm-global-mode 'maybe
       mmm-submode-decoration-level 0)
 
-(setf auto-mode-alist (cdr auto-mode-alist))
-
 (add-to-list 'auto-mode-alist '("\\.x\\'" . alex-mode))
+(mmm-add-mode-ext-class 'alex-mode "\\.x\\'" 'haskell-blocks)
+
 (add-to-list 'auto-mode-alist '("\\.happy\\'" . happy-mode))
 (add-to-list 'auto-mode-alist '("\\.ly\\'" . happy-mode))
-
-(mmm-add-mode-ext-class 'alex-mode "\\.x\\'" 'haskell-blocks)
+(add-to-list 'auto-mode-alist '("\\.y\\'" . happy-mode))
 (mmm-add-mode-ext-class 'happy-mode "\\.y\\'" 'haskell-blocks)
-
-(defconst haskell-blocks-default-syntax-table
-  (let ((tbl (make-syntax-table)))
-    (modify-syntax-entry ?\{ "(}1nb" tbl)
-    (modify-syntax-entry ?\} "){4nb" tbl)
-    (modify-syntax-entry ?\' "."     tbl)
-    (modify-syntax-entry ?\" "\""    tbl)
-    (modify-syntax-entry ?-  ". 123" tbl)
-    (modify-syntax-entry ?\n ">"     tbl)
-    tbl))
-
-(defun haskell-blocks-verify-location ()
-  (with-syntax-table haskell-blocks-default-syntax-table
-    (let* ((state (parse-partial-sexp (point-min)
-                                      (point)))
-           (parens-depth (nth 0 state))
-           (inside-string? (nth 3 state)))
-      (and (= 1 parens-depth)
-           (not inside-string?)))))
-
-(defun haskell-blocks-verify-front ()
-  (save-excursion
-    (goto-char (match-end 0))
-    (when (not (and (= ?\{ (char-before))
-                    (= ?- (char-after))))
-      (haskell-blocks-verify-location))))
-
-(defun haskell-blocks-verify-back ()
-  (save-excursion
-    (goto-char (match-beginning 0))
-    (unless (= ?- (char-before))
-      (haskell-blocks-verify-location))))
-
-(defun haskell-blocks-find-back (bound)
-  (with-syntax-table
-      ;; haskell-mode-syntax-table
-      haskell-blocks-default-syntax-table
-    (let ((p (point)))
-      (goto-char (match-beginning 0))
-      (forward-sexp)
-      (backward-char)
-      (if (< bound (point))
-        nil
-        (looking-at "")))))
-
-(mmm-add-classes
- '((haskell-blocks
-    :submode haskell-mode
-    ;;:face mmm-output-submode-face
-    :front "{%?"
-    :front-verify haskell-blocks-verify-front
-    :back haskell-blocks-find-back
-    :back-verify haskell-blocks-verify-back
-    ;;:include-front t
-    ;;:front-offset 5
-    )))
 
 (defun haskell-grammar-tools-setup ()
   (init-common :use-yasnippet nil :use-render-formula nil)
