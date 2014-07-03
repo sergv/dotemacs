3d91bd5b55cf693ff5865a5ad9eb187997741250
Sergey Vinokurov <serg.foo@gmail.com>
2014-07-03 22:46:48 +0300

    add dos mode for bat scripts

---
+++ src/dos-setup.el
@@ -0,0 +1,24 @@
+;; dos-setup.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: Thursday,  3 July 2014
+;; Description:
+
+(require 'common)
+
+(autoload 'dos-mode "dos" "Edit Dos scripts." t)
+(add-to-list 'auto-mode-alist '("\\.bat$" . dos-mode))
+
+(defun dos-setup ()
+  (init-common :use-yasnippet nil :use-whitespace t))
+
+(add-hook 'dos-mode-hook #'dos-setup)
+
+(provide 'dos-setup)
+
+;; Local Variables:
+;; End:
+
+;; dos-setup.el ends here
