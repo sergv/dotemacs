d31da960d218db2f25c90be06e41b791c3f9bfba
Sergey Vinokurov <serg.foo@gmail.com>
2014-08-28 00:11:18 +0300

    add diff-mode-setup

---
+++ src/diff-mode-setup.el
@@ -0,0 +1,24 @@
+;; diff-mode-setup.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: Thursday, 28 August 2014
+;; Description:
+
+(require 'common)
+
+(defun diff-mode-setup ()
+  (init-common :use-yasnippet nil
+               :use-comment nil)
+  (def-keys-for-map diff-mode-map
+    ("M-k" icicle-delete-window)))
+
+(add-hook 'diff-mode-hook #'diff-mode-setup)
+
+(provide 'diff-mode-setup)
+
+;; Local Variables:
+;; End:
+
+;; diff-mode-setup.el ends here
