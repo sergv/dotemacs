dbfe161dfd3d75defeeb766c2316012005c4f2c4
Sergey Vinokurov <serg.foo@gmail.com>
2015-02-02 01:21:58 +0200

    fix indentation and completion in python-mode

--- src/python/python-common.el
+++ src/python/python-common.el
@@ -207,6 +207,13 @@ greater indenation as current line."
     ("#"        search-for-symbol-at-point-backward)
     ("M-#"      search-for-symbol-at-point-backward-new-color))
 
+  (def-keys-for-map (vim:normal-mode-local-keymap
+                     vim:insert-mode-local-keymap)
+    ("<tab>"       tab-to-tab-stop)
+    ("<backtab>"   tab-to-tab-stop-backward)
+    ("S-<tab>"     tab-to-tab-stop-backward)
+    ("S-<iso-tab>" tab-to-tab-stop-backward))
+
   (python-abbrev+-setup)
 
   ;; pabbrev isn't powerful enough
