1523da2fba1206b79e2616afc886ab4b601f7541
Sergey Vinokurov <serg.foo@gmail.com>
2015-02-03 02:07:38 +0200

    try to decouple select-mode.el

--- src/select-mode.el
+++ src/select-mode.el
@@ -11,31 +11,21 @@
 
 (eval-when-compile (require 'cl-lib))
 
-(require 'common)
-(require 'more-scheme)
-(require 'keys-def)
 
-
-(when (or (require 'tabbar-setup nil t)
-          (require 'tabbar nil t))
-  (defun select-inhibit-tabbar ()
-    (eq major-mode 'select-mode)))
-
-
-(defparameter select/init-window-config nil)
-(defparameter select/init-window nil
+(defvar select/init-window-config nil)
+(defvar select/init-window nil
   "Window from which select-mode's instance was invoked")
-(defparameter select/init-buffer nil
+(defvar select/init-buffer nil
   "Buffer from which select-mode's instance was invoked")
 
 
-(defparameter select/selected-item nil
+(defvar select/selected-item nil
   "Index (number) of selected item")
-(defparameter select/items nil
+(defvar select/items nil
   "List of possible selections")
-(defparameter select/selection-overlay nil
+(defvar select/selection-overlay nil
   "Overlay that displays ")
-(defparameter select/selection-buffer nil
+(defvar select/selection-buffer nil
   "Buffer where selection takes place")
 
 (defface select-selection-face '((t (:inherit secondary-selection)))
@@ -45,7 +35,7 @@
 ;;   "Face to highlight currently selected item")
 
 
-(defparameter select/separator-function (lambda () "")
+(defvar select/separator-function (lambda () "")
   "Function of no arguments that returns current separator to use.
 
 Separator is used like this
@@ -56,17 +46,27 @@ bar
 ---------------
 baz
 <...>")
-(defparameter select/predisplay-function #'identity
+(defvar select/predisplay-function #'identity
   "This should be a function of one item to be displayed.
 
 Items will be passed to this function before insertion into buffer.")
 
-(defparameter select/on-selection-function #'ignore
+(defvar select/on-selection-function #'ignore
   "This should be a function of one argument - index of currently selected item.")
-(defparameter select/preamble-function (lambda () "")
+(defvar select/preamble-function (lambda () "")
   "Function that returns contents at the top of the buffer")
-(defparameter select/epilogue-function (lambda () "")
+(defvar select/epilogue-function (lambda () "")
   "Function that returns contents at the bottom of the buffer")
+
+(defvar select-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "<up>")     #'select-move-selection-up)
+    (define-key map (kbd "<down>")   #'select-move-selection-down)
+    (define-key map (kbd "<return>") #'select-do-select)
+    (define-key map (kbd "<escape>") #'select-exit)
+    (define-key map (kbd "q")        #'select-exit)
+    (define-key map (kbd "C-q")      #'select-exit)
+    map))
 
 (define-derived-mode select-mode text-mode "Selection"
   "Major mode for queries in auxiliary buffer."
@@ -88,25 +88,6 @@ Items will be passed to this function before insertion into buffer.")
   ;; (add-hook 'kill-buffer-hook #'select-finish-selection nil t)
   )
 
-(setf select-mode-map
-      (let ((map (make-sparse-keymap)))
-        (def-keys-for-map map
-          +vi-essential-keys+
-          +vi-search-keys+
-          +control-x-prefix+
-          +vim-word-motion-keys+
-          +vim-special-keys+
-
-          ("n"        select-move-selection-up)
-          ("t"        select-move-selection-down)
-          ("<up>"     select-move-selection-up)
-          ("<down>"   select-move-selection-down)
-          ("<return>" select-do-select)
-          ("<escape>" select-exit)
-          ("q"        select-exit))
-        map))
-
-
 ;; TODO: add option to use recursive edit?
 (defun* select-start-selection (items
                                 &key
@@ -126,7 +107,7 @@ Items will be passed to this function before insertion into buffer.")
         (init-win (selected-window))
         (init-buf (current-buffer)))
     (with-current-buffer (switch-to-buffer-other-window buffer-name)
-      (with-disabled-undo
+      (select/with-disabled-undo
        (setf select/selection-buffer (current-buffer))
 
        (setf select/init-window-config win-config
@@ -184,9 +165,9 @@ Items will be passed to this function before insertion into buffer.")
              (insert (funcall select/predisplay-function item))
              (values start (point))))))
     (with-current-buffer select/selection-buffer
-      (with-disabled-undo
-       (with-preserved-buffer-modified-p
-        (with-inhibited-read-only
+      (select/with-disabled-undo
+       (select/with-preserved-buffer-modified-p
+        (select/with-inhibited-read-only
          (erase-buffer)
          (goto-char (point-min))
          (insert (funcall select/preamble-function))
@@ -216,14 +197,14 @@ Items will be passed to this function before insertion into buffer.")
             (and (<= (car pos-pair) pos)
                  (< pos (cdr pos-pair)))))
          (selection-idx
-          (bisect pos
-                  select/item-positions
-                  0
-                  (length select/item-positions)
-                  pos-inside-pos-pair
-                  (lambda (pos pos-pair)
-                    (assert (< (car pos-pair) (cdr pos-pair)))
-                    (< pos (car pos-pair))))))
+          (select/bisect pos
+                         select/item-positions
+                         0
+                         (length select/item-positions)
+                         pos-inside-pos-pair
+                         (lambda (pos pos-pair)
+                           (assert (< (car pos-pair) (cdr pos-pair)))
+                           (< pos (car pos-pair))))))
     (if (and selection-idx
              (funcall pos-inside-pos-pair pos
                       (aref select/item-positions selection-idx)))
@@ -231,8 +212,6 @@ Items will be passed to this function before insertion into buffer.")
         (setf select/selected-item selection-idx)
         (select/move-selection-to select/selected-item :move-point nil))
       (move-overlay select/selection-overlay (point-min) (point-min)))))
-
-
 
 
 (defun select-move-selection-up ()
@@ -295,6 +274,50 @@ Items will be passed to this function before insertion into buffer.")
   (propertize base
               'face 'bold
               'font-lock-face 'bold))
+
+;;; utilities
+
+(defmacro select/with-disabled-undo (&rest body)
+  (declare (indent 0))
+  (let ((store (gensym)))
+    `(let ((,store buffer-undo-list)
+           ;; this disables further undo recording
+           (buffer-undo-list t))
+       ,@body)))
+
+(defmacro select/with-preserved-buffer-modified-p (&rest body)
+  "Execute BODY and restore `buffer-modified-p' flag after its done."
+  (declare (indent 0))
+  (let ((store (gensym)))
+    `(let ((,store (buffer-modified-p)))
+       (unwind-protect
+           (begin
+             ,@body)
+         (set-buffer-modified-p ,store)))))
+
+(defmacro select/with-inhibited-read-only (&rest body)
+  (declare (indent 0))
+  `(let ((inhibit-read-only t))
+     ,@body))
+
+(defun select/bisect (item items start end eq? less?)
+  "Binary search. Returns index into vector ITEMS.
+LESS? is predicate on items and elements of ITEMS.
+
+START is inclusive and END is exclusive in ITEMS."
+  (let ((orig-start start)
+        (orig-end end))
+    (while (< start end)
+      (let* ((mid (/ (+ end start) 2))
+             (mid-item (aref items mid)))
+        (cond ((funcall less? item mid-item)
+               (setf end mid))
+              ((funcall eq? item mid-item)
+               (setf start mid
+                     end mid))
+              (t
+               (setf start (+ mid 1))))))
+    start))
 
 (provide 'select-mode)
 
