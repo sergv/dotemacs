6e31da56c0d6a741d705f1db255b0a4d26bb25d9
Sergey Vinokurov <serg.foo@gmail.com>
2015-02-08 00:40:56 +0200

    remove temporary directory when closing emacs

--- set-up-paths.el
+++ set-up-paths.el
@@ -64,76 +64,54 @@ system restars.")
       small-temporary-file-directory +tmp-path+
       tramp-auto-save-directory (concat +prog-data-path+ "/tramp"))
 
+(defun remove-tmp-path ()
+  "Delete `+tmp-path+' directory on emacs exit."
+  (delete-directory +tmp-path+
+                    t   ;; recurse
+                    nil ;; don't just move to trash
+                    ))
+(add-hook 'kill-emacs-hook #'remove-tmp-path)
 
 (defalias 'strip-trailing-slash 'directory-file-name)
 
-(defun %emacs-boot--get-directory-contents (dir)
-  (remove-if (lambda (x) (member (file-name-nondirectory x) '("." "..")))
-             (directory-files dir t)))
-
-(defun* %emacs-boot--find-rec-special
-    (path
-     &key
-     (filep (lambda (p) t))
-     (dirp  (lambda (p) nil))
-     (do-not-visitp
-      (lambda (p)
-        (member* (file-name-nondirectory (strip-trailing-slash p))
-                 '("SCCS" "RCS" "CVS" "MCVS" ".svn"
-                   ".git" ".hg" ".bzr" "_MTN" "_darcs"
-                   "{arch}")
-                 :test #'string=))))
-  "Collect files and/or directories under PATH recursively.
-
-Collect files and directories which satisfy FILEP and
-DIRP respectively in directories which don't satisfy DO-NOT-VISITP.
-By default, version-control specific directories are omitted, e.g. .git etc."
-  (when (stringp filep)
-    (setf filep
-          (let ((regular-expression filep))
-            (lambda (p) (string-match-p regular-expression p)))))
-  (when (stringp dirp)
-    (setf dirp
-          (let ((regular-expression dirp))
-            (lambda (p) (string-match-p regular-expression p)))))
-  (when (stringp do-not-visitp)
-    (setf do-not-visitp
-          (let ((regular-expression do-no-visitp))
-            (lambda (p) (string-match-p regular-expression p)))))
-
-  (letrec ((collect-rec
-            (lambda (path accum)
-              (cond
-                ((and (file-directory-p path)
-                      (not (funcall do-not-visitp path)))
-                 (reduce (lambda (acc p)
-                           (funcall collect-rec p acc))
-                         (%emacs-boot--get-directory-contents path)
-                         :initial-value (if (funcall dirp path)
-                                          (cons path accum)
-                                          accum)))
-                ((funcall filep path)
-                 (cons path accum))
-                (t
-                 accum)))))
-    (funcall collect-rec path nil)))
-
-(setf load-path
-      (remove-duplicates
-       (append
-        (%emacs-boot--find-rec-special (concat +emacs-config-path+ "/src")
-                                       :filep (lambda (x) nil)
-                                       :dirp (lambda (x) t))
-        (%emacs-boot--find-rec-special (concat +emacs-config-path+ "/third-party")
-                                       :filep (lambda (x) nil)
-                                       :dirp (lambda (x) t))
-        load-path)
-       :test #'string=))
+(let ((%emacs-boot--find-rec-special
+        (lambda (path)
+          (let ((%emacs-boot--get-directory-contents
+               (lambda (dir)
+                 (remove-if (lambda (x) (member (file-name-nondirectory x) '("." "..")))
+                            (directory-files dir t))))
+              (do-not-visitp
+               (lambda (p)
+                 (member* (file-name-nondirectory (strip-trailing-slash p))
+                          '("SCCS" "RCS" "CVS" "MCVS" ".svn"
+                            ".git" ".hg" ".bzr" "_MTN" "_darcs"
+                            "{arch}")
+                          :test #'string=))))
+          (letrec ((collect-rec
+                    (lambda (path accum)
+                      (cond
+                        ((and (file-directory-p path)
+                              (not (funcall do-not-visitp path)))
+                         (reduce (lambda (acc p)
+                                   (funcall collect-rec p acc))
+                                 (funcall %emacs-boot--get-directory-contents path)
+                                 :initial-value (cons path accum)))
+                        (t
+                         accum)))))
+            (funcall collect-rec path nil))))))
+  (setf load-path
+        (remove-duplicates
+         (append
+          (funcall %emacs-boot--find-rec-special
+                   (concat +emacs-config-path+ "/src"))
+          (funcall %emacs-boot--find-rec-special
+                   (concat +emacs-config-path+ "/third-party"))
+          load-path)
+         :test #'string=)))
 
 ;; this must go to the end in order to give files in /src dir a chance
 ;; (add-to-list 'load-path +bytecode-lib+ t)
 (add-to-list 'load-path +color-themes-path+)
-
 
 (provide 'set-up-paths)
 
