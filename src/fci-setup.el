575c6cce275176ea6eed4cb9ee66bf67e622b77d
Sergey Vinokurov <serg.foo@gmail.com>
2015-09-22 10:23:50 +0300

    Add fci-setup, toggle fci mode when changing major mode and after loading persistent sessions

---
+++ src/fci-setup.el
@@ -0,0 +1,36 @@
+;; fci-setup.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: Tuesday, 22 September 2015
+;; Description:
+
+(require 'fill-column-indicator)
+
+(defun toggle-fci ()
+  (when fci-mode
+    (fci-mode -1)
+    (fci-mode +1)))
+
+(defun toggle-fci-everywhere ()
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (toggle-fci))))
+
+;; Tuesday, 15 September 2015 NB: it's somewhat expensive to toggle fci on every
+;; buffer switch, try to disable it in the future and see whether it's needed
+;; any more.
+(defadvice switch-to-buffer (after switch-to-buffer-toggle-fci activate compile)
+  "Check syntax with GHC when switching to a haskell-mode buffer."
+  (toggle-fci))
+
+(add-hook 'sessions/load-buffers-hook #'toggle-fci-everywhere)
+(add-hook 'after-change-major-mode-hook #'toggle-fci)
+
+(provide 'fci-setup)
+
+;; Local Variables:
+;; End:
+
+;; fci-setup.el ends here
