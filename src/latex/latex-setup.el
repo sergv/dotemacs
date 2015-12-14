8ca3f499f83251724a2098b15c9d3b7506b275f5
Sergey Vinokurov <serg.foo@gmail.com>
2015-12-14 09:52:47 +0200

    Never ask to save buffers before compiling - save them automatically instead

--- src/latex/latex-setup.el
+++ src/latex/latex-setup.el
@@ -105,8 +105,6 @@
 
     ;; don't ask - just compile
     (setq-local compilation-read-command nil)
-    ;; don't ask - just save
-    (setq-local compilation-ask-about-save nil)
     (setq-local compilation-auto-jump-to-first-error nil)
     ;; don't skip any messages
     (setq-local compilation-skip-threshold 0))
