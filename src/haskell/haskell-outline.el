5fa41eab793248b89327a9d45241f1ab82074bf9
Sergey Vinokurov <serg.foo@gmail.com>
2015-12-25 01:43:36 +0200

    Improve Haskell folding - use hs-minor-mode folding when point is at sexp beginning/end

--- src/haskell/haskell-outline.el
+++ src/haskell/haskell-outline.el
@@ -11,11 +11,14 @@
 (require 'haskell-misc)
 (require 'search)
 
+(setf yafolding-show-fringe-marks nil)
+
 (search-def-autoexpand-advices
- (yafolding-show)
+ (yafolding-show-element)
  (haskell-mode))
 
 (defun haskell-setup-folding ()
+  (hs-minor-mode +1)
   (setq buffer-display-table (make-display-table))
   (set-display-table-slot buffer-display-table
                           'selective-display
@@ -23,11 +26,29 @@
 
   (def-keys-for-map vim:normal-mode-local-keymap
     ("z C" yafolding-hide-all)
-    ("z c" yafolding-hide)
+    ("z c" haskell-hide-indented-or-sexp)
     ("z O" yafolding-show-all)
-    ("z o" yafolding-show)
-    ("z T" yafolding-toggle-all)))
+    ("z o" haskell-show-indented-or-sexp)
+    ("z T" yafolding-toggle-all))
+  (def-keys-for-map vim:visual-mode-local-keymap
+    ("z c" yafolding-hide-region)))
 
+(defun haskell-hide-indented-or-sexp ()
+  (interactive)
+  (if (haskell-outline-on-sexp?)
+    (hs-hide-block)
+    (yafolding-hide-element)))
+
+(defun haskell-show-indented-or-sexp ()
+  (interactive)
+  (if (haskell-outline-on-sexp?)
+    (hs-show-block)
+    (yafolding-show-element)))
+
+(defun haskell-outline-on-sexp? ()
+  (let ((synt (char-syntax (char-after))))
+    (or (char=? synt ?\()
+        (char=? synt ?\)))))
 
 (provide 'haskell-outline)
 
