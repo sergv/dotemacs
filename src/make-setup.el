68713aed61203381ae0ed426591c86b171801322
Sergey Vinokurov <serg.foo@gmail.com>
2016-01-06 00:38:35 +0200

    Add dedicated setup for makefiles

---
+++ src/make-setup.el
@@ -0,0 +1,22 @@
+;; make-setup.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: Wednesday,  6 January 2016
+;; Description:
+
+(defun makefile-setup ()
+  (init-common :use-yasnippet nil
+               :use-whitespace nil
+               :use-fci nil)
+  (which-function-mode -1))
+
+(add-hook 'makefile-mode-hook #'makefile-setup)
+
+(provide 'make-setup)
+
+;; Local Variables:
+;; End:
+
+;; make-setup.el ends here
