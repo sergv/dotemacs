fe1010c8cb192386cff30b67d16aa30e121ed3e1
Sergey Vinokurov <serg.foo@gmail.com>
2016-02-03 23:35:26 +0200

    Add yaml-mode as a submodule

--- src/yaml-mode-setup.el
+++ src/yaml-mode-setup.el
@@ -8,14 +8,13 @@
 
 (autoload 'yaml-mode "yaml-mode.el" "Simple mode to edit YAML." t)
 
-
 (add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.clang-format$" . yaml-mode))
 
 (defun yaml-setup ()
   (init-common :use-render-formula t))
 
 (add-hook 'yaml-mode-hook #'yaml-setup)
-
 
 (provide 'yaml-mode-setup)
 
