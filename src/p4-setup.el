cca283a2e83fd6c098f94c9ed8b9eac6f2b8314d
Sergey Vinokurov <serg.foo@gmail.com>
2016-02-03 09:44:33 +0200

    Set up p4.el

---
+++ src/p4-setup.el
@@ -0,0 +1,51 @@
+;; p4-setup.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: Wednesday,  3 February 2016
+;; Description:
+
+(autoload 'p4-add "p4" "" t)
+(autoload 'p4-annotate "p4" "" t)
+(autoload 'p4-client "p4" "" t)
+(autoload 'p4-changes "p4" "" t)
+(autoload 'p4-delete "p4" "" t)
+(autoload 'p4-depot-find-file "p4" "" t)
+(autoload 'p4-describe "p4" "" t)
+(autoload 'p4-diff "p4" "" t)
+(autoload 'p4-diff2 "p4" "" t)
+(autoload 'p4-ediff "p4" "" t)
+(autoload 'p4-edit "p4" "" t)
+(autoload 'p4-filelog "p4" "" t)
+(autoload 'p4-files "p4" "" t)
+(autoload 'p4-fix "p4" "" t)
+(autoload 'p4-help "p4" "" t)
+(autoload 'p4-have "p4" "" t)
+(autoload 'p4-get-client-name "p4" "" t)
+(autoload 'p4-info "p4" "" t)
+(autoload 'p4-integ "p4" "" t)
+(autoload 'p4-job "p4" "" t)
+(autoload 'p4-jobs "p4" "" t)
+(autoload 'p4-label "p4" "" t)
+(autoload 'p4-labels "p4" "" t)
+(autoload 'p4-labelsync "p4" "" t)
+(autoload 'p4-move "p4" "" t)
+(autoload 'p4-opened "p4" "" t)
+(autoload 'p4-print "p4" "" t)
+(autoload 'p4-refresh "p4" "" t)
+(autoload 'p4-reopen "p4" "" t)
+(autoload 'p4-resolve "p4" "" t)
+(autoload 'p4-revert "p4" "" t)
+(autoload 'p4-submit "p4" "" t)
+(autoload 'p4-toggle-vc-mode "p4" "" t)
+(autoload 'p4-update "p4" "" t)
+(autoload 'p4-user "p4" "" t)
+(autoload 'p4-users "p4" "" t)
+
+(provide 'p4-setup)
+
+;; Local Variables:
+;; End:
+
+;; p4-setup.el ends here
