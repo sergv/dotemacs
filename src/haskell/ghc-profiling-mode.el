9cccfed2367f5202876d02934a4faa30a8c41b37
Sergey Vinokurov <serg.foo@gmail.com>
2016-02-10 20:48:59 +0200

    Add highlighting of calls taking significant amount of time/space to ghc-profiling-mode

--- src/haskell/ghc-profiling-mode.el
+++ src/haskell/ghc-profiling-mode.el
@@ -6,7 +6,49 @@
 ;; Created: Thursday, 19 November 2015
 ;; Description:
 
-(define-derived-mode ghc-profiling-mode prog-mode "GHC-Prof")
+(require 'solarized)
+
+(defface ghc-profiling-expensive-face
+  `((t (:underline (:style wave :color ,+solarized-orange+))))
+  "Face to highlight expensive entries with in the profile")
+
+(defparameter ghc-profiling-mode-expensive-cumulative-threshold 10
+  "Highlight profile entries that take more percentage either in
+cpu time or allocations that the value of this variable.")
+
+(defparameter ghc-profiling-mode-font-lock-keywords
+  `((,(rxx ((ws (any ?\s ?\t))
+            (non-ws (not (any ?\s ?\t ?\n ?\r)))
+            (integer (+ digit))
+            (float (seq integer "." integer)))
+        bol
+        (* ws)
+        (+ non-ws) ;; Function name
+        (+ ws)
+        (+ non-ws) ;; Module
+        (+ ws)
+        integer
+        (+ ws)
+        integer
+        (+ ws)
+        float
+        (+ ws)
+        float
+        (+ ws)
+        (group float)
+        (+ ws)
+        (group float)
+        (* ws)
+        eol)
+     (0 (when (<= ghc-profiling-mode-expensive-cumulative-threshold
+                  (max (string->number (match-string 1))
+                       (string->number (match-string 2))))
+          'ghc-profiling-expensive-face)))))
+
+(define-derived-mode ghc-profiling-mode prog-mode "GHC-Prof"
+  "Major mode for viewing Haskell profiling reports."
+  (set (make-local-variable 'font-lock-defaults)
+       '(ghc-profiling-mode-font-lock-keywords)))
 
 (defun ghc-profiling-mode-setup ()
   (init-common :use-yasnippet nil
