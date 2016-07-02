5f18be5d7bcc47c2b7941e82034288dfb5190934
Sergey Vinokurov <serg.foo@gmail.com>
2016-07-02 23:31:01 +0300

    Make unconditional reuse of previous choices customizable in eproj-symbnav

---
+++ src/eproj/eproj-customization.el
@@ -0,0 +1,18 @@
+;; eproj-customization.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: Saturday,  2 July 2016
+;; Description:
+
+(defcustom eproj-symbnav-remember-choices nil
+  "Whether to remember choices of previous tags and unconditionally jump to them
+when the same navigation ambiguity occurs.")
+
+(provide 'eproj-customization)
+
+;; Local Variables:
+;; End:
+
+;; eproj-customization.el ends here
