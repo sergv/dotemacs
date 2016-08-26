11ba0502243c8a027272eff9d6d27e158a6e39f4
Sergey Vinokurov <serg.foo@gmail.com>
2016-08-26 10:59:55 +0300

    Further improve persistent sessions. Encode more value types. Optimize text properties collection

--- tests/persistent-sessions-tests.el
+++ tests/persistent-sessions-tests.el
@@ -8,7 +8,20 @@
 
 (eval-when-compile (require 'cl-lib))
 
+(require 'persistent-sessions-error-reporting)
+(require 'persistent-sessions-serializers)
+
 (require 'ert)
+
+(ert-deftest persistent-sessions-tests/sessions/position-ranges/add-range ()
+  (let ((ranges (sessions/empty-position-ranges)))
+    (sessions/position-ranges/add-range 1 5 ranges)
+    (sessions/position-ranges/add-range 5 10 ranges)
+    (sessions/position-ranges/add-range 15 20 ranges)
+    (sessions/position-ranges/add-range 25 27 ranges)
+    (sessions/position-ranges/add-range 27 29 ranges)
+    (should (equal (sessions/position-ranges/ranges ranges)
+                   '((25 . 29) (15 . 20) (1 . 10))))))
 
 (ert-deftest persistent-sessions-tests/sessions/get-all-text-properties-in-string ()
   (let ((test-string "foo bar"))
@@ -17,24 +30,35 @@
     (let ((props (sessions/get-all-text-properties-in-string test-string)))
       (should (listp props))
       (should (= 2 (length props)))
-      (should (equal (car props) '(quux baz ((4 . 5)))))
-      (should (equal (cadr props) '(foo bar (0)))))))
+      (should (equal (car props) '(quux baz ((4 . 6)))))
+      (should (equal (cadr props) '(foo bar ((0 . 1))))))))
+
+(ert-deftest persistent-sessions-tests/sessions/get-all-text-properties-in-string-2 ()
+  (let ((test-string "foo bar"))
+    (put-text-property 0 7 'foo 'bar test-string)
+    (put-text-property 4 7 'quux 'baz test-string)
+    (let ((props (sessions/get-all-text-properties-in-string test-string)))
+      (should (listp props))
+      (should (= 2 (length props)))
+      (should (equal (car props) '(quux baz ((4 . 7)))))
+      (should (equal (cadr props) '(foo bar ((0 . 7))))))))
 
 (ert-deftest persistent-sessions-tests/store-and-restore-string ()
   (let ((test-string "foo bar"))
     (put-text-property 0 1 'foo 'bar test-string)
     (put-text-property 4 6 'quux 'baz test-string)
-    (should (equal (text-properties-at 0 test-string) '(foo bar)))
-    (should (equal (text-properties-at 1 test-string) nil))
-    (should (equal (text-properties-at 2 test-string) nil))
-    (should (equal (text-properties-at 3 test-string) nil))
-    (should (equal (text-properties-at 4 test-string) '(quux baz)))
-    (should (equal (text-properties-at 5 test-string) '(quux baz)))
-    (should (equal (text-properties-at 6 test-string) nil))
+    (progn
+      (should (equal (text-properties-at 0 test-string) '(foo bar)))
+      (should (equal (text-properties-at 1 test-string) nil))
+      (should (equal (text-properties-at 2 test-string) nil))
+      (should (equal (text-properties-at 3 test-string) nil))
+      (should (equal (text-properties-at 4 test-string) '(quux baz)))
+      (should (equal (text-properties-at 5 test-string) '(quux baz)))
+      (should (equal (text-properties-at 6 test-string) nil)))
     (let* ((encoded-string
-            (sessions/store-string test-string))
+            (sessions/store-value test-string))
            (converted-string
-            (sessions/restore-string encoded-string)))
+            (sessions/versioned/restore-value nil encoded-string)))
       (should (listp encoded-string))
       (should (equal 3 (length encoded-string)))
       (should (eq 'string (car encoded-string)))
@@ -58,18 +82,89 @@
         (put-text-property start (+ start 1) 'foo n new-string)
         (ring-insert test-ring new-string)))
     (let ((restored-ring
-           (sessions/restore-ring
-            (sessions/store-ring
-             test-ring))))
+           (sessions/versioned/restore-value
+            nil
+            (sessions/store-value test-ring))))
       (should (equal size (ring-size restored-ring)))
       (should (equal test-ring restored-ring))
       (should (equal (prin1-to-string test-ring)
                      (prin1-to-string restored-ring))))))
 
+(ert-deftest persistent-sessions-tests/store-and-restore-proper-list ()
+  (let ((test-list (list 'a 'b 'c
+                         1 2 3
+                         "foo"
+                         (propertize "bar" 'foo 'bar 'baz 15)
+                         "baz")))
+    (let ((restored-list
+           (sessions/versioned/restore-value
+            nil
+            (sessions/store-value test-list))))
+      (should (equal test-list restored-list))
+      (should (equal (prin1-to-string test-list)
+                     (prin1-to-string restored-list))))))
+
+(ert-deftest persistent-sessions-tests/store-and-restore-vector ()
+  (let ((test-vector (vector 'a 'b 'c
+                             1 2 3
+                             nil
+                             "foo"
+                             (propertize "bar" 'foo 'bar 'baz 15)
+                             "baz")))
+    (let ((restored-vector
+           (sessions/versioned/restore-value
+            nil
+            (sessions/store-value test-vector))))
+      (should (equal test-vector restored-vector))
+      (should (equal (prin1-to-string test-vector)
+                     (prin1-to-string restored-vector))))))
+
+(ert-deftest persistent-sessions-tests/store-and-restore-hash-table ()
+  (let ((test-hash-table (alist->hash-table
+                          (list (cons 'foo 'bar)
+                                (cons 'baz 5)
+                                (cons 'quux (propertize "quux" 'foo 'bar 'baz 15))
+                                (cons 5 "frob"))
+                          #'eql)))
+    (let ((restored-hash-table
+           (sessions/versioned/restore-value
+            nil
+            (sessions/store-value test-hash-table))))
+      (should (equal (hash-table-test test-hash-table)
+                     (hash-table-test restored-hash-table)))
+      (should (equal (persistent-sessions-tests/hash-table-contents test-hash-table)
+                     (persistent-sessions-tests/hash-table-contents restored-hash-table))))))
+
+(defun persistent-sessions-tests/hash-table-contents (tbl)
+  "Normalize contents of hash table TBL and return it as an alist of key-value
+pairs."
+  (assert (hash-table-p tbl))
+  (sort (hash-table->alist tbl)
+        (lambda (a b)
+          (let ((x (car a))
+                (y (car b)))
+            (cond
+              ((and (numberp x)
+                    (numberp y))
+               (< x y))
+              ((and (symbolp x)
+                    (symbolp y))
+               (string< (symbol->string x)
+                        (symbol->string y)))
+              ((and (numberp x)
+                    (symbolp y))
+               t)
+              ((and (symbolp x)
+                    (numberp y))
+               nil)
+              (t
+               (error "persistent-sessions-tests/hash-table-contents: unhandled comparison keys: a = %s, b = %s"
+                      x
+                      y)))))))
+
 (progn
   (ert "persistent-sessions-tests/.*")
-  ;; (ert #'persistent-sessions-tests/store-string-1)
-  ;; (ert #'persistent-sessions-tests/store-and-restore-ring)
+  ;; (ert #'persistent-sessions-tests/sessions/get-all-text-properties-in-string-2)
   nil)
 
 (provide 'persistent-sessions-tests)
