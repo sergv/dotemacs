95021ed6404c49a37f974508c66ff9407a57bf1c
Sergey Vinokurov <serg.foo@gmail.com>
2016-12-23 10:48:30 +0200

    Improve setup for diff-mode

--- src/diff-mode-setup.el
+++ src/diff-mode-setup.el
@@ -12,7 +12,16 @@
 (defun diff-mode-setup ()
   (init-common :use-yasnippet nil
                :use-comment nil
-               :use-whitespace 'tabs-only))
+               :use-whitespace 'tabs-only)
+
+  (def-keys-for-map vim:normal-mode-local-keymap
+    ("<up>"     diff-hunk-prev)
+    ("<down>"   diff-hunk-next)
+    ("C-<up>"   diff-file-prev)
+    ("C-<down>" diff-file-next)
+    ("SPC"      diff-goto-source)
+    ("<return>" diff-goto-source)
+    ("g ,"      diff-hunk-kill)))
 
 ;;;###autoload
 (add-hook 'diff-mode-hook #'diff-mode-setup)
