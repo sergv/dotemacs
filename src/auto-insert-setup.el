ec04a6188cd8bc7089f28d736a46ca3816e068dd
Sergey Vinokurov <serg.foo@gmail.com>
2017-05-02 11:20:04 +0300

    Reduce boilerplate in auto-insert setup

--- src/auto-insert-setup.el
+++ src/auto-insert-setup.el
@@ -14,50 +14,35 @@
       auto-insert-query nil
       auto-insert-alist
       (mapcar (lambda (spec)
-                (let ((actions (cdr spec)))
-                  (cons (car spec)
-                        (list->vector
-                         (-map (lambda (action)
-                                 (or
-                                  (when (and (string? action)
-                                             (platform-use? 'work))
-                                    (let ((work-specific-insert-file
-                                           (concat (file-name-sans-extension action)
-                                                   ".work"
-                                                   "."
-                                                   (file-name-extension action))))
-                                      (when (file-exists?
-                                             (concat auto-insert-directory
-                                                     "/"
-                                                     work-specific-insert-file))
-                                        work-specific-insert-file)))
-                                  action))
-                               actions)))))
-              '(("\\.sh$"      . ["insert.sh"    auto-insert-update])
-                ("\\.clj$"     . ["insert.clj"   auto-insert-update])
-                ("\\.el$"      . ["insert.el"    auto-insert-update])
-                ("\\.hs$"      . ["insert.hs"    auto-insert-update])
-                ("\\.cabal$"   . ["insert.cabal" auto-insert-update])
-                ("\\.chs$"     . ["insert.chs"   auto-insert-update])
-                ("\\.awk$"     . ["insert.awk"   auto-insert-update])
-                ("\\.tex$"     . ["insert.tex"   auto-insert-update])
-                ("\\.snip$"    . ["insert.snip"  auto-insert-update])
-                ("\\.h$"       . ["insert.h"     auto-insert-update])
-                ("\\.xhtml?$"  . ["insert.xhtml" auto-insert-update])
-                ("\\.html?$"   . ["insert.html"  auto-insert-update])
-                ("\\.py$"      . ["insert.py"    auto-insert-update])
-                ("\\.org$"     . ["insert.org"   auto-insert-update])
-                ("\\.eproj-info$" . ["insert.eproj-info" auto-insert-update])
+                (pcase spec
+                  (`(,filename-regexp ,insert-template)
+                   (cons filename-regexp
+                         (vector insert-template #'auto-insert-update)))))
+              '(("\\.sh\\'"         "insert.sh")
+                ("\\.clj\\'"        "insert.clj")
+                ("\\.el\\'"         "insert.el")
+                ("\\.hs\\'"         "insert.hs")
+                ("\\.cabal\\'"      "insert.cabal")
+                ("\\.chs\\'"        "insert.chs")
+                ("\\.awk\\'"        "insert.awk")
+                ("\\.tex\\'"        "insert.tex")
+                ("\\.snip\\'"       "insert.snip")
+                ("\\.h\\'"          "insert.h")
+                ("\\.xhtml?\\'"     "insert.xhtml")
+                ("\\.html?\\'"      "insert.html")
+                ("\\.py\\'"         "insert.py")
+                ("\\.org\\'"        "insert.org")
+                ("\\.eproj-info\\'" "insert.eproj-info")
 
-                ("AndroidManifest.xml$"      . ["insert-android-manifest.xml" auto-insert-update])
-                ("/res/drawable.*/.*\\.xml$" . ["insert-android-drawable.xml" auto-insert-update])
-                ("/res/layout.*/.*\\.xml$"   . ["insert-android-layout.xml"   auto-insert-update])
-                ("/res/menu.*/.*\\.xml$"     . ["insert-android-menu.xml"     auto-insert-update])
-                ("/res/values.*/.*\\.xml$"   . ["insert-android-values.xml"   auto-insert-update])
+                ("AndroidManifest.xml\\'"      "insert-android-manifest.xml")
+                ("/res/drawable.*/.*\\.xml\\'" "insert-android-drawable.xml")
+                ("/res/layout.*/.*\\.xml\\'"   "insert-android-layout.xml")
+                ("/res/menu.*/.*\\.xml\\'"     "insert-android-menu.xml")
+                ("/res/values.*/.*\\.xml\\'"   "insert-android-values.xml")
 
-                ("\\.scm$"     . ["insert.scm"   auto-insert-update])
-                ("\\.\\(?:l\\|cl\\|asd\\|lsp\\|lisp\\|clisp\\)$"
-                 . ["insert.lisp" auto-insert-update]))))
+                ("\\.scm\\'"        "insert.scm")
+                ("\\.\\(?:c?l\\|asdf?\\|li?sp\\|clisp\\)\\'"
+                 "insert.lisp"))))
 
 (defparameter auto-insert-fields
   (list
@@ -83,7 +68,7 @@
          (lambda ()
            (save-match-data
              (let ((name buffer-file-name))
-               (if (string-match "^.*/src\\(?:/clojure\\)?/\\(.*\\)\\.clj$" name)
+               (if (string-match "^.*/src\\(?:/clojure\\)?/\\(.*\\)\\.clj\\'" name)
                    (replace-regexp-in-string "/"
                                              "."
                                              (match-string-no-properties 1 name))
