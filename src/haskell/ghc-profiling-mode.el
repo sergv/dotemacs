920739d3ac10264a6f40d9cc3a28887285cd4aa1
Sergey Vinokurov <serg.foo@gmail.com>
2017-06-02 00:32:41 +0300

    Unify forward and backward searching. Improve ghc profiling mode with search-like navigation over expensive entries. Improve profiling entry regexp in ghc-profiling-mode for GHC 8.0

--- src/haskell/ghc-profiling-mode.el
+++ src/haskell/ghc-profiling-mode.el
@@ -17,34 +17,74 @@
   "Highlight profile entries that take more percentage either in
 cpu time or allocations that the value of this variable.")
 
+(defparameter ghc-profiling-mode-entry-re
+  (rxx ((ws (any ?\s ?\t))
+        (non-ws (not (any ?\s ?\t ?\n ?\r)))
+        (integer (+ digit))
+        (float (seq integer "." integer)))
+    bol
+    (* ws)
+    (+ non-ws) ;; Function name
+    (+ ws)
+    (+ non-ws) ;; Module
+    (+ ws)
+    (\?
+     (+ non-ws) ;; SRC, from 8.0 onwards
+     (+ ws))
+    integer ;; no.
+    (+ ws)
+    integer ;; entries
+    (+ ws)
+    float ;; individual %time
+    (+ ws)
+    float ;; individual %alloc
+    (+ ws)
+    (group float) ;; inherited %time
+    (+ ws)
+    (group float) ;; inherited %alloc
+    (* ws)
+    eol))
+
+(defun ghc-profiling-mode--matched-expersive-entry? ()
+  (let ((inherited-time (string->number (match-string 1)))
+        (inherited-alloc (string->number (match-string 2))))
+    (<= ghc-profiling-mode-expensive-cumulative-threshold
+        (max inherited-time inherited-alloc))))
+
 (defparameter ghc-profiling-mode-font-lock-keywords
-  `((,(rxx ((ws (any ?\s ?\t))
-            (non-ws (not (any ?\s ?\t ?\n ?\r)))
-            (integer (+ digit))
-            (float (seq integer "." integer)))
-        bol
-        (* ws)
-        (+ non-ws) ;; Function name
-        (+ ws)
-        (+ non-ws) ;; Module
-        (+ ws)
-        integer
-        (+ ws)
-        integer
-        (+ ws)
-        float
-        (+ ws)
-        float
-        (+ ws)
-        (group float)
-        (+ ws)
-        (group float)
-        (* ws)
-        eol)
-     (0 (when (<= ghc-profiling-mode-expensive-cumulative-threshold
-                  (max (string->number (match-string 1))
-                       (string->number (match-string 2))))
-          'ghc-profiling-expensive-face)))))
+  `((,ghc-profiling-mode-entry-re
+     (0
+      (when (ghc-profiling-mode--matched-expersive-entry?)
+        'ghc-profiling-expensive-face)))))
+
+(defun ghc-profiling-mode--generic-searh-for-expensive-entry (direction)
+  (save-match-data
+    (re-search-generic-matching
+     direction
+     #'ghc-profiling-mode--matched-expersive-entry?
+     ghc-profiling-mode-entry-re
+     nil
+     t)))
+
+(defun ghc-profiling-mode--search-for-expensive-entry (direction)
+  (let ((start-point (point))
+        (column (current-column)))
+    (fold-direction direction
+      (end-of-line)
+      (beginning-of-line))
+    (if (wrap-search-around
+         direction
+         (lambda () (ghc-profiling-mode--generic-searh-for-expensive-entry direction)))
+        (move-to-column column)
+      (goto-char start-point))))
+
+(defun ghc-profiling-mode--search-for-expensive-entry-forward ()
+  (interactive)
+  (ghc-profiling-mode--search-for-expensive-entry 'forward))
+
+(defun ghc-profiling-mode--search-for-expensive-entry-backward ()
+  (interactive)
+  (ghc-profiling-mode--search-for-expensive-entry 'backward))
 
 (define-derived-mode ghc-profiling-mode prog-mode "GHC-Prof"
   "Major mode for viewing Haskell profiling reports."
@@ -58,7 +98,10 @@ cpu time or allocations that the value of this variable.")
                :use-hl-line t)
   ;; Display every line as a single screen line.
   (setq-local truncate-lines t)
-  (haskell-setup-folding :enable-hs-minor-mode nil))
+  (haskell-setup-folding :enable-hs-minor-mode nil)
+  (def-keys-for-map vim:normal-mode-local-keymap
+    ("C-h" ghc-profiling-mode--search-for-expensive-entry-forward)
+    ("C-t" ghc-profiling-mode--search-for-expensive-entry-backward)))
 
 (provide 'ghc-profiling-mode)
 
