255660672e84c38f72cbec9b8bdf7de82671c2b7
Sergey Vinokurov <serg.foo@gmail.com>
2017-07-23 00:38:47 +0300

    Add magit-version.el

---
+++ third-party/magit/lisp/magit-version.el
@@ -0,0 +1,19 @@
+;; magit-version.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: 23 July 2017
+;; Description:
+
+(setq magit-version "2.10.3 (d741197dd962f3f42aa86d24caf521c6e62f3c28)")
+
+(provide 'magit-version)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+
+;; magit-version.el ends here
