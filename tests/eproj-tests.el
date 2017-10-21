ad55e2e97c0ffedc1c71cee0f99f0c9aec96687f
Sergey Vinokurov <serg.foo@gmail.com>
2017-10-22 01:14:13 +0300

    Improve facility for running tests

--- tests/eproj-tests.el
+++ tests/eproj-tests.el
@@ -15,7 +15,7 @@
 (require 'eproj-haskell)
 
 (defun eproj-tests/non-special-files (path)
-  "Construct list of non-special files (i.e. that probably would be under version control)
+  "Construct list of non-special files (i.e. that typically would be under version control)
 under ROOT directory."
   (-filter (comp #'not
                  (partial-first #'member '(".gitignore" ".eproj-info"))
@@ -42,6 +42,12 @@ under ROOT directory."
   (string=? (expand-file-name (strip-trailing-slash path-a))
             (expand-file-name (strip-trailing-slash path-b))))
 
+(defmacro eproj-tests--set-up-environment (&rest body)
+  (declare (indent 0))
+  `(let ((eproj/default-projects (make-hash-table :test #'eq))
+         ;; don't want verbose messages in the output
+         (eproj-verbose-tag-loading nil))
+     ,@body))
 
 
 (defconst eproj-tests/project-dir
@@ -66,63 +72,66 @@ under ROOT directory."
   (expand-file-name (concat eproj-tests/project-dir "/haskell-project-with-ignored-files")))
 
 (ert-deftest eproj-tests/eproj-get-all-related-projects ()
-  (let* ((path eproj-tests/project-with-git-with-related)
-         (proj (eproj-get-project-for-path path)))
-    (should (eproj-tests/paths=? path (eproj-project/root proj)))
-    (should (not (null? (eproj-project/related-projects proj))))
-    (should (equal (eproj-tests/normalize-file-list
-                    (-map #'eproj-project/root
-                          (cons proj
-                                (eproj-get-all-related-projects proj nil))))
-                   (eproj-tests/normalize-file-list
-                    (-filter #'file-directory?
-                             (directory-files
-                              eproj-tests/folder-with-related-projects
-                              t
-                              directory-files-no-dot-files-regexp)))))))
+  (eproj-tests--set-up-environment
+    (let* ((path eproj-tests/project-with-git-with-related)
+           (proj (eproj-get-project-for-path path)))
+      (should (eproj-tests/paths=? path (eproj-project/root proj)))
+      (should (not (null? (eproj-project/related-projects proj))))
+      (should (equal (eproj-tests/normalize-file-list
+                      (-map #'eproj-project/root
+                            (cons proj
+                                  (eproj-get-all-related-projects proj nil))))
+                     (eproj-tests/normalize-file-list
+                      (-filter #'file-directory?
+                               (directory-files
+                                eproj-tests/folder-with-related-projects
+                                t
+                                directory-files-no-dot-files-regexp))))))))
 
 (ert-deftest eproj-tests/aux-files ()
-  (let* ((path eproj-tests/project-with-aux-files)
-         (proj (eproj-get-project-for-path path)))
-    (should (not (null? proj)))
-    (should (eproj-tests/paths=? path (eproj-project/root proj)))
-    (should (not (null? (eproj-project/aux-files proj))))
-    (should (equal (eproj-tests/normalize-file-list
-                    (eproj-project/aux-files proj))
-                   (eproj-tests/normalize-file-list
-                    (-filter #'file-regular?
-                             (directory-files
-                              (concat eproj-tests/project-with-aux-files
-                                      "/aux-files")
-                              t
-                              directory-files-no-dot-files-regexp)))))))
+  (eproj-tests--set-up-environment
+    (let* ((path eproj-tests/project-with-aux-files)
+           (proj (eproj-get-project-for-path path)))
+      (should (not (null? proj)))
+      (should (eproj-tests/paths=? path (eproj-project/root proj)))
+      (should (not (null? (eproj-project/aux-files proj))))
+      (should (equal (eproj-tests/normalize-file-list
+                      (eproj-project/aux-files proj))
+                     (eproj-tests/normalize-file-list
+                      (-filter #'file-regular?
+                               (directory-files
+                                (concat eproj-tests/project-with-aux-files
+                                        "/aux-files")
+                                t
+                                directory-files-no-dot-files-regexp))))))))
 
 (ert-deftest eproj-tests/tags-of-c-files ()
-  (let* ((path eproj-tests/project-with-c-files)
-         (proj (eproj-get-project-for-path path))
-         (tags-table
-          (cdr-safe (assq 'c-mode
-                          (eproj-project/tags proj))))
-         (project-names
-          '("bigint"
-            "bigint_list_s"
-            "bigint_list_s::item"
-            "bigint_list_s::next"
-            "bigint_list_t"
-            "factorial"
-            "FACTORIAL_H_"
-            "item"
-            "next")))
-    (should-not (null? proj))
-    (should (eproj-tests/paths=? path (eproj-project/root proj)))
-    (should-not (null? tags-table))
-    (should-not (= 0 (length (eproj-project/tags proj))))
-    (should (hash-table-p tags-table))
-    (should-not (= 0 (hash-table-count tags-table)))
-    (should-not (= 0 (length (eproj-tests/hash-table-keys tags-table))))
-    (should (equal (eproj-tests/normalize-string-list project-names)
-                   (eproj-tests/normalize-string-list
-                    (eproj-tests/hash-table-keys tags-table))))))
+  (eproj-tests--set-up-environment
+    (let* ((path eproj-tests/project-with-c-files)
+           (proj (eproj-get-project-for-path path))
+           (tags-table
+            (cdr-safe (assq 'c-mode
+                            (eproj-project/tags proj))))
+           (project-names
+            '("bigint"
+              "bigint_list_s"
+              "bigint_list_s::item"
+              "bigint_list_s::next"
+              "bigint_list_t"
+              "factorial"
+              "FACTORIAL_H_"
+              "item"
+              "next")))
+      (should-not (null? proj))
+      (should (eproj-tests/paths=? path (eproj-project/root proj)))
+      (should-not (null? tags-table))
+      (should-not (= 0 (length (eproj-project/tags proj))))
+      (should (hash-table-p tags-table))
+      (should-not (= 0 (hash-table-count tags-table)))
+      (should-not (= 0 (length (eproj-tests/hash-table-keys tags-table))))
+      (should (equal (eproj-tests/normalize-string-list project-names)
+                     (eproj-tests/normalize-string-list
+                      (eproj-tests/hash-table-keys tags-table)))))))
 
 (defmacro eproj-tests/test-ctags-get-tags-from-buffer
   (input
@@ -137,156 +146,162 @@ under ROOT directory."
        ,@body)))
 
 (ert-deftest eproj-tests/eproj/get-fast-tags-from-buffer ()
-  (let ((test-filename "foo.bar"))
-    (eproj-tests/test-ctags-get-tags-from-buffer
-     (format
-      "\
+  (eproj-tests--set-up-environment
+    (let ((test-filename "foo.bar"))
+      (eproj-tests/test-ctags-get-tags-from-buffer
+       (format
+        "\
 foo1	%s	100;\"	x
 foo2	%s	101 ;\"	y
 foo3	%s	102	;\"	z
 "
-      test-filename
-      test-filename
-      test-filename)
-     tags-table
-     (eproj/get-fast-tags-tags-from-buffer (current-buffer))
-     (should-not (= 0 (hash-table-size tags-table)))
+        test-filename
+        test-filename
+        test-filename)
+       tags-table
+       (eproj/get-fast-tags-tags-from-buffer (current-buffer))
+       (should-not (= 0 (hash-table-size tags-table)))
 
-     (let ((tag1 (car-safe (gethash "foo1" tags-table))))
-       (should tag1)
-       (should (string=? test-filename (eproj-tag/file tag1)))
-       (should (= 100 (eproj-tag/line tag1)))
-       (should (equal (cons 'type "x") (assq 'type (eproj-tag/properties tag1)))))
+       (let ((tag1 (car-safe (gethash "foo1" tags-table))))
+         (should tag1)
+         (should (string=? test-filename (eproj-tag/file tag1)))
+         (should (= 100 (eproj-tag/line tag1)))
+         (should (equal (cons 'type "x") (assq 'type (eproj-tag/properties tag1)))))
 
-     (let ((tag2 (car-safe (gethash "foo2" tags-table))))
-       (should tag2)
-       (should (string=? test-filename (eproj-tag/file tag2)))
-       (should (= 101 (eproj-tag/line tag2)))
-       (should (equal (cons 'type "y") (assq 'type (eproj-tag/properties tag2)))))
+       (let ((tag2 (car-safe (gethash "foo2" tags-table))))
+         (should tag2)
+         (should (string=? test-filename (eproj-tag/file tag2)))
+         (should (= 101 (eproj-tag/line tag2)))
+         (should (equal (cons 'type "y") (assq 'type (eproj-tag/properties tag2)))))
 
-     (let ((tag3 (car-safe (gethash "foo3" tags-table))))
-       (should tag3)
-       (should (string=? test-filename (eproj-tag/file tag3)))
-       (should (= 102 (eproj-tag/line tag3)))
-       (should (equal (cons 'type "z") (assq 'type (eproj-tag/properties tag3))))))))
+       (let ((tag3 (car-safe (gethash "foo3" tags-table))))
+         (should tag3)
+         (should (string=? test-filename (eproj-tag/file tag3)))
+         (should (= 102 (eproj-tag/line tag3)))
+         (should (equal (cons 'type "z") (assq 'type (eproj-tag/properties tag3)))))))))
 
 (ert-deftest eproj-tests/eproj/get-fast-tags-from-buffer/filenames-with-spaces ()
-  (let ((test-filename "/home/admin/my projects/test project/hello.c"))
-    (eproj-tests/test-ctags-get-tags-from-buffer
-     (format
-      "\
+  (eproj-tests--set-up-environment
+    (let ((test-filename "/home/admin/my projects/test project/hello.c"))
+      (eproj-tests/test-ctags-get-tags-from-buffer
+       (format
+        "\
 foo1	%s	100;\"	x
 foo2	%s	101 ;\"	y
 foo3	%s	102	;\"	z
 "
-      test-filename
-      test-filename
-      test-filename)
-     tags-table
-     (eproj/get-fast-tags-tags-from-buffer (current-buffer))
-     (should-not (= 0 (hash-table-size tags-table)))
+        test-filename
+        test-filename
+        test-filename)
+       tags-table
+       (eproj/get-fast-tags-tags-from-buffer (current-buffer))
+       (should-not (= 0 (hash-table-size tags-table)))
 
-     (let ((tag1 (car-safe (gethash "foo1" tags-table))))
-       (should tag1)
-       (should (string=? test-filename (eproj-tag/file tag1)))
-       (should (= 100 (eproj-tag/line tag1)))
-       (should (equal (cons 'type "x") (assq 'type (eproj-tag/properties tag1)))))
+       (let ((tag1 (car-safe (gethash "foo1" tags-table))))
+         (should tag1)
+         (should (string=? test-filename (eproj-tag/file tag1)))
+         (should (= 100 (eproj-tag/line tag1)))
+         (should (equal (cons 'type "x") (assq 'type (eproj-tag/properties tag1)))))
 
-     (let ((tag2 (car-safe (gethash "foo2" tags-table))))
-       (should tag2)
-       (should (string=? test-filename (eproj-tag/file tag2)))
-       (should (= 101 (eproj-tag/line tag2)))
-       (should (equal (cons 'type "y") (assq 'type (eproj-tag/properties tag2)))))
+       (let ((tag2 (car-safe (gethash "foo2" tags-table))))
+         (should tag2)
+         (should (string=? test-filename (eproj-tag/file tag2)))
+         (should (= 101 (eproj-tag/line tag2)))
+         (should (equal (cons 'type "y") (assq 'type (eproj-tag/properties tag2)))))
 
-     (let ((tag3 (car-safe (gethash "foo3" tags-table))))
-       (should tag3)
-       (should (string=? test-filename (eproj-tag/file tag3)))
-       (should (= 102 (eproj-tag/line tag3)))
-       (should (equal (cons 'type "z") (assq 'type (eproj-tag/properties tag3))))))))
+       (let ((tag3 (car-safe (gethash "foo3" tags-table))))
+         (should tag3)
+         (should (string=? test-filename (eproj-tag/file tag3)))
+         (should (= 102 (eproj-tag/line tag3)))
+         (should (equal (cons 'type "z") (assq 'type (eproj-tag/properties tag3)))))))))
 
 
 (ert-deftest eproj-tests/eproj/get-fast-tags-from-buffer/ignore-constructor-tags-that-repeat-type-tags ()
-  (let ((test-filename "/home/sergey/Test.hs"))
-    (eproj-tests/test-ctags-get-tags-from-buffer
-     (format
-      "\
+  (eproj-tests--set-up-environment
+    (let ((test-filename "/home/sergey/Test.hs"))
+      (eproj-tests/test-ctags-get-tags-from-buffer
+       (format
+        "\
 Identity	%s	100;\"	C
 Identity	%s	101 ;\"	t
 test	%s	102	;\"	f
 "
-      test-filename
-      test-filename
-      test-filename)
-     tags-table
-     (eproj/get-fast-tags-tags-from-buffer (current-buffer))
-     (should-not (= 0 (hash-table-size tags-table)))
+        test-filename
+        test-filename
+        test-filename)
+       tags-table
+       (eproj/get-fast-tags-tags-from-buffer (current-buffer))
+       (should-not (= 0 (hash-table-size tags-table)))
 
-     (let ((type-tag (car-safe (gethash "Identity" tags-table))))
-       (should type-tag)
-       (should (string=? test-filename (eproj-tag/file type-tag)))
-       (should (= 101 (eproj-tag/line type-tag)))
-       (should (equal (cons 'type "t") (assq 'type (eproj-tag/properties type-tag)))))
+       (let ((type-tag (car-safe (gethash "Identity" tags-table))))
+         (should type-tag)
+         (should (string=? test-filename (eproj-tag/file type-tag)))
+         (should (= 101 (eproj-tag/line type-tag)))
+         (should (equal (cons 'type "t") (assq 'type (eproj-tag/properties type-tag)))))
 
-     (let ((function-tag (car-safe (gethash "test" tags-table))))
-       (should function-tag)
-       (should (string=? test-filename (eproj-tag/file function-tag)))
-       (should (= 102 (eproj-tag/line function-tag)))
-       (should (equal (cons 'type "f") (assq 'type (eproj-tag/properties function-tag))))))))
+       (let ((function-tag (car-safe (gethash "test" tags-table))))
+         (should function-tag)
+         (should (string=? test-filename (eproj-tag/file function-tag)))
+         (should (= 102 (eproj-tag/line function-tag)))
+         (should (equal (cons 'type "f") (assq 'type (eproj-tag/properties function-tag)))))))))
 
 
 (ert-deftest eproj-tests/project-with-eproj-file-and-tags-file ()
-  (let* ((path eproj-tests/project-with-eproj-file-and-tags-file)
-         (proj (eproj-get-project-for-path path)))
-    (should (not (null? (eproj/find-eproj-file-location path))))
-    (should (not (null? (eproj/find-eproj-file-location (concat path "/Foo")))))
-    (should (not (null? (eproj/find-eproj-file-location (concat path "/Foo/Bar")))))
-    (should (not (null? proj)))
-    (should (eproj-tests/paths=? path (eproj-project/root proj)))
+  (eproj-tests--set-up-environment
+    (let* ((path eproj-tests/project-with-eproj-file-and-tags-file)
+           (proj (eproj-get-project-for-path path)))
+      (should (not (null? (eproj/find-eproj-file-location path))))
+      (should (not (null? (eproj/find-eproj-file-location (concat path "/Foo")))))
+      (should (not (null? (eproj/find-eproj-file-location (concat path "/Foo/Bar")))))
+      (should (not (null? proj)))
+      (should (eproj-tests/paths=? path (eproj-project/root proj)))
 
-    (should (equal (eproj-tests/normalize-file-list
-                    (find-rec path
-                              :filep (lambda (path) (string-match-p "\\.hs$" path))))
-                   (eproj-tests/normalize-file-list (eproj-get-project-files proj))))))
+      (should (equal (eproj-tests/normalize-file-list
+                      (find-rec path
+                                :filep (lambda (path) (string-match-p "\\.hs$" path))))
+                     (eproj-tests/normalize-file-list (eproj-get-project-files proj)))))))
 
 (ert-deftest eproj-tests/project-with-file-list ()
-  (let* ((path eproj-tests/project-with-file-list)
-         (proj (eproj-get-project-for-path path)))
-    (should (not (null? proj)))
-    (should (eproj-tests/paths=? path (eproj-project/root proj)))
+  (eproj-tests--set-up-environment
+    (let* ((path eproj-tests/project-with-file-list)
+           (proj (eproj-get-project-for-path path)))
+      (should (not (null? proj)))
+      (should (eproj-tests/paths=? path (eproj-project/root proj)))
 
-    (should (equal (eproj-tests/normalize-file-list
-                    (list (concat path "/Foo/Bar/Test.hs")))
-                   (eproj-tests/normalize-file-list (eproj-get-project-files proj))))))
+      (should (equal (eproj-tests/normalize-file-list
+                      (list (concat path "/Foo/Bar/Test.hs")))
+                     (eproj-tests/normalize-file-list (eproj-get-project-files proj)))))))
 
 (ert-deftest eproj-tests/project-with-ignored-files ()
-  (let* ((path eproj-tests/project-with-ignored-files)
-         (proj (eproj-get-project-for-path path)))
-    (should (not (null? proj)))
-    (should (eproj-tests/paths=? path (eproj-project/root proj)))
+  (eproj-tests--set-up-environment
+    (let* ((path eproj-tests/project-with-ignored-files)
+           (proj (eproj-get-project-for-path path)))
+      (should (not (null? proj)))
+      (should (eproj-tests/paths=? path (eproj-project/root proj)))
 
-    (should (equal (eproj-tests/normalize-file-list
-                    (list (concat path "/Foo/Bar/IdentityMonad.hs")))
-                   (eproj-tests/normalize-file-list (eproj-get-project-files proj))))
-    (should (eproj-get-matching-tags proj
-                                     'haskell-mode
-                                     "IdentityM"
-                                     nil))
-    (should-not (eproj-get-matching-tags proj
-                                         'haskell-mode
-                                         "IgnoredM"
-                                         nil))))
+      (should (equal (eproj-tests/normalize-file-list
+                      (list (concat path "/Foo/Bar/IdentityMonad.hs")))
+                     (eproj-tests/normalize-file-list (eproj-get-project-files proj))))
+      (should (eproj-get-matching-tags proj
+                                       'haskell-mode
+                                       "IdentityM"
+                                       nil))
+      (should-not (eproj-get-matching-tags proj
+                                           'haskell-mode
+                                           "IgnoredM"
+                                           nil)))))
 
-(setf eproj-tests/tests
-      '(eproj-tests/eproj-get-all-related-projects
-        eproj-tests/aux-files
-        eproj-tests/tags-of-c-files
-
-        eproj-tests/eproj/ctags-get-tags-from-buffer
-        eproj-tests/eproj/ctags-get-tags-from-buffer/filenames-with-spaces
-
-        eproj-tests/project-with-eproj-file-and-tags-file
-        eproj-tests/project-with-file-list
-        eproj-tests/project-with-ignored-files))
+;; (setf eproj-tests/tests
+;;       '(eproj-tests/eproj-get-all-related-projects
+;;         eproj-tests/aux-files
+;;         eproj-tests/tags-of-c-files
+;;
+;;         eproj-tests/eproj/ctags-get-tags-from-buffer
+;;         eproj-tests/eproj/ctags-get-tags-from-buffer/filenames-with-spaces
+;;
+;;         eproj-tests/project-with-eproj-file-and-tags-file
+;;         eproj-tests/project-with-file-list
+;;         eproj-tests/project-with-ignored-files))
 
 ;; (let ((ert-debug-on-error nil))
 ;;   (eproj-reset-projects)
