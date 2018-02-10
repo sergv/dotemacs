e0b7da88c0f774d92d517ff67e6439bf1fde13d1
Sergey Vinokurov <serg.foo@gmail.com>
2018-02-10 20:16:48 +0000

    Add eproj test data

---
+++ tests/eproj-sample-projects/related-projects-as-subdirs/main-project/subproj1/Bar1.hs
@@ -0,0 +1,18 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Bar1
+-- Copyright   :  (c) Sergey Vinokurov 2018
+-- License     :  BSD3-style (see LICENSE)
+--
+-- Maintainer  :  serg.foo@gmail.com
+-- Created     :  10 February 2018
+-- Stability   :
+-- Portability :
+--
+--
+----------------------------------------------------------------------------
+
+module Bar1 (bar1) where
+
+bar1 :: a -> a
+bar1 x = x
---
+++ tests/eproj-sample-projects/related-projects-as-subdirs/main-project/subproj1/Foo1.hs
@@ -0,0 +1,18 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Foo1
+-- Copyright   :  (c) Sergey Vinokurov 2018
+-- License     :  BSD3-style (see LICENSE)
+--
+-- Maintainer  :  serg.foo@gmail.com
+-- Created     :  10 February 2018
+-- Stability   :
+-- Portability :
+--
+--
+----------------------------------------------------------------------------
+
+module Foo1 (foo1) where
+
+foo1 :: a -> a
+foo1 x = x
