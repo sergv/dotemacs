0d699a04776255f3fdb07dcc048cebe34e70d239
Sergey Vinokurov <serg.foo@gmail.com>
2018-02-10 20:16:48 +0000

    Add eproj test data

---
+++ tests/eproj-sample-projects/related-projects-as-subdirs/main-project/subproj2/src/Quux.hs
@@ -0,0 +1,18 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Quux
+-- Copyright   :  (c) Sergey Vinokurov 2018
+-- License     :  BSD3-style (see LICENSE)
+--
+-- Maintainer  :  serg.foo@gmail.com
+-- Created     :  10 February 2018
+-- Stability   :
+-- Portability :
+--
+--
+----------------------------------------------------------------------------
+
+module Quux (quux) where
+
+quux :: a -> a
+quux x = x
