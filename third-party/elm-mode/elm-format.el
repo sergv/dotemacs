b9bf9f239425d7823b646b9b7f9cb12972ab74f4
Sergey Vinokurov <serg.foo@gmail.com>
2018-02-10 20:02:42 +0000

    Merge commit '09c6e62e14a2c9afaad03a867c7a268b6bc68ab0' from 'https://github.com/jcollard/elm-mode.git' into 'third-party/elm-mode'

--- third-party/elm-mode/elm-format.el
+++ third-party/elm-mode/elm-format.el
@@ -30,27 +30,24 @@
   :group 'elm-format
   :type 'boolean)
 
-(defcustom elm-format-elm-version "0.17"
+(defcustom elm-format-elm-version "0.18"
   "The version of Elm against which code should be formatted."
   :group 'elm-format
-  :type '(choice (const :tag "Default: 0.17" "0.17")
+  :type '(choice (const :tag "Default: 0.18" "0.18")
+                 (const :tag "0.17" "0.17")
                  (const :tag "0.16" "0.16")))
 
-(defvar elm-format-command "elm-format"
-  "The name of the `elm-format' command.")
-
-
-(defun elm-format--display-error (err-file)
-  "Displays the error in file ERR-FILE to the user."
-  (with-temp-buffer
-    (insert-file-contents err-file nil nil nil t)
-    (ansi-color-apply-on-region (point-min) (point-max))
-    (message "Error: elm-format failed on current buffer.\n\n%s" (buffer-string))))
+(defcustom elm-format-command "elm-format"
+  "The name of the `elm-format' command."
+  :group 'elm-format
+  :type 'string)
 
 ;;;###autoload
-(defun elm-mode-format-buffer ()
-  "Apply `elm-format' to the current buffer."
-  (interactive)
+(defun elm-mode-format-buffer (&optional is-interactive)
+  "Apply `elm-format' to the current buffer.
+When called interactively, or with prefix argument
+IS-INTERACTIVE, show a buffer if the formatting fails."
+  (interactive "p")
   (let* (;; elm-format requires that the file have a .elm extension
          (in-file (make-temp-file "elm-format" nil ".elm"))
          (err-file (make-temp-file "elm-format"))
@@ -61,6 +58,7 @@
     (unwind-protect
         (let* ((command elm-format-command)
                (version elm-format-elm-version)
+               (error-buffer (get-buffer-create "*elm-format errors*"))
                (retcode
                 (with-temp-buffer
                   (call-process command
@@ -70,9 +68,18 @@
                                 "--output" out-file
                                 "--elm-version" version
                                 "--yes"))))
-          (if (/= retcode 0)
-              (elm-format--display-error err-file)
-            (insert-file-contents out-file nil nil nil t)))
+          (with-current-buffer error-buffer
+            (read-only-mode 0)
+            (insert-file-contents err-file nil nil nil t)
+            (ansi-color-apply-on-region (point-min) (point-max))
+            (special-mode))
+          (if (eq retcode 0)
+              (progn
+                (insert-file-contents out-file nil nil nil t)
+                (message "elm-format applied"))
+            (if is-interactive
+                (display-buffer error-buffer)
+              (message "elm-format failed: see %s" (buffer-name error-buffer)))))
       (delete-file in-file)
       (delete-file err-file)
       (delete-file out-file))))
