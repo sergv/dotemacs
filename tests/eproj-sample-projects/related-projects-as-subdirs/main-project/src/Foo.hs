70884137b2e7526da63f52eb9c40b54b4ebc80b1
Sergey Vinokurov <serg.foo@gmail.com>
2018-02-10 20:16:48 +0000

    Add eproj test data

---
+++ tests/eproj-sample-projects/related-projects-as-subdirs/main-project/src/Foo.hs
@@ -0,0 +1,18 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Foo
+-- Copyright   :  (c) Sergey Vinokurov 2018
+-- License     :  BSD3-style (see LICENSE)
+--
+-- Maintainer  :  serg.foo@gmail.com
+-- Created     :  10 February 2018
+-- Stability   :
+-- Portability :
+--
+--
+----------------------------------------------------------------------------
+
+module Foo (foo) where
+
+foo :: a -> a
+foo x = x
