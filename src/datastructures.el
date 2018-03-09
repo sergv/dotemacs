846dfcc91c704aa2acddeb59e267f867452a19b9
Sergey Vinokurov <serg.foo@gmail.com>
2018-03-09 21:24:47 +0000

    Define bimap data structure

--- src/datastructures.el
+++ src/datastructures.el
@@ -10,7 +10,7 @@
 
 ;;; (prematurely optimized) persistent sorted sets
 
-(defstruct (sorted-set
+(cl-defstruct (sorted-set
             (:conc-name sorted-set/))
   items
   lt-pred
@@ -155,6 +155,50 @@ items and remove any duplicates."
 (defun sorted-set/empty? (set)
   "Check whether SET is empty."
   (null? (sorted-set/items set)))
+
+(cl-defstruct (bimap
+               (:conc-name bimap/))
+  forward-map   ;; hash table with #'equal comparison
+  backward-map  ;; hash table with #'equal comparison
+  size)
+
+(defsubst bimap-size (bm)
+  (hash-table-count (bimap/forward-map bm)))
+
+(defun bimap-empty ()
+  (make-bimap
+   :forward-map (make-hash-table :test #'equal)
+   :backward-map (make-hash-table :test #'equal)
+   :size 0))
+
+(defun bimap-lookup (key bm &optional default)
+  (gethash key (bimap/forward-map bm) default))
+
+(defun bimap-lookup-reverse (value bm &optional default)
+  (gethash value (bimap/backward-map bm) default))
+
+(defun bimap-insert (key value bm)
+  (let* ((fwd         (bimap/forward-map bm))
+         (bwd         (bimap/backward-map bm))
+         (key-image   (gethash key fwd))
+         (value-image (gethash value bwd)))
+    (remhash key-image bwd)
+    (remhash value-image fwd)
+    (puthash key value fwd)
+    (puthash value key bwd)))
+
+(defun alist->bimap (items)
+  (let ((bm (bimap-empty)))
+    (dolist (entry items)
+      (bimap-insert (car entry) (cdr entry) bm))
+    bm))
+
+(defun bimap-delete (key bm)
+  (let ((fwd (bimap/forward-map bm))
+        (bwd (bimap/backward-map bm)))
+    (remhash (gethash key fwd) bwd)
+    (remhash key fwd)
+    bm))
 
 (provide 'datastructures)
 
