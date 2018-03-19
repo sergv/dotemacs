da350b42bbe4543bc09b24b23d498db0badcf0bf
Sergey Vinokurov <serg.foo@gmail.com>
2018-03-19 20:36:28 +0000

    Add test that eproj tags can locate entries under a folder named 'Distribution'

---
+++ tests/eproj-sample-projects/haskell-project-with-ignored-files/Foo/Distribution/Test.hs
@@ -0,0 +1,19 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Foo.Distribution.Test
+-- Copyright   :  (c) Sergey Vinokurov 2018
+-- License     :  BSD3-style (see LICENSE)
+--
+-- Maintainer  :  serg.foo@gmail.com
+-- Created     :  19 March 2018
+-- Stability   :
+-- Portability :
+--
+--
+----------------------------------------------------------------------------
+
+module Foo.Distribution.Test (distributionTest1, distributionTest2) where
+
+distributionTest1, distributionTest2 :: Int -> Itn
+distributionTest1 = (+1)
+distributionTest2 = (+2)
