1551093b0933a0945d3dcbebf693b90150b53943
Sergey Vinokurov <serg.foo@gmail.com>
2018-03-20 22:46:30 +0000

    Improve 'haskell-watch--get-project-root-for-path'

--- src/haskell/haskell-watch.el
+++ src/haskell/haskell-watch.el
@@ -35,30 +35,30 @@ roots (i.e. valid and existing keys within
 (defun haskell-watch--get-project-root-for-path (file)
   "Obtain root of a Haskell project that FILE is part of."
   (cl-assert (file-name-absolute-p file))
-  (let ((search-start (file-name-directory file)))
-    (or (locate-dominating-file
-         search-start
-         (lambda (dir)
-           (directory-files dir
-                            nil ;; Relative names.
-                            (rx bos
-                                (or "cabal.project"
-                                    ".cabal.sandbox"
-                                    (seq "stack" (* nonl) ".yaml"))
-                                eos)
-                            t ;; Do not sort.
-                            )))
-        (locate-dominating-file
-         search-start
-         (lambda (dir)
-           (directory-files dir
-                            nil ;; Relative names.
-                            (rx bos
-                                (or (seq (+ nonl) ".cabal")
-                                    "package.yaml")
-                                eos)
-                            t ;; Do not sort.
-                            ))))))
+  (let* ((build-dir-regexp
+          (rx bos
+              (or "cabal.project"
+                  ".cabal.sandbox"
+                  (seq "stack" (* nonl) ".yaml"))
+              eos))
+         (proj-file-regexp
+          (rx bos
+              (or (seq (+ nonl) ".cabal")
+                  "package.yaml")
+              eos))
+         (search-start (file-name-directory file))
+         (lookup-with
+          (lambda (filename-regexp)
+            (let ((get-files
+                   (lambda (dir-name)
+                     (directory-files dir-name
+                                      nil ;; Relative names.
+                                      filename-regexp
+                                      t ;; Do not sort.
+                                      ))))
+              (locate-dominating-file search-start get-files)))))
+    (or (funcall lookup-with build-dir-regexp)
+        (funcall lookup-with proj-file-regexp))))
 
 (defun haskell-watch-get-project-root ()
   "Get absolute project root for current buffer."
