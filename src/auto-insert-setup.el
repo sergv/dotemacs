cc9dbec7d65bfadda075dc8148fc4c96b0cd72f5
Sergey Vinokurov <serg.foo@gmail.com>
2018-05-14 00:45:37 +0100

    Remove redundant auto-insert template for .chs files. Instead, unify it with .hs template

--- src/auto-insert-setup.el
+++ src/auto-insert-setup.el
@@ -19,10 +19,9 @@
                    (cons filename-regexp
                          (vector insert-template #'auto-insert-update)))))
               '(("tests?[/\\].*Test[^/\\]*\\.hs\\'" "insert-haskell-tests.hs")
-                ("\\.hs\\'"         "insert.hs.template")
+                ("\\.\\(?:hsc?\\|chs\\)\\'"         "insert.hs.template")
                 ("\\.cabal\\'"      "insert.cabal.template")
                 ("stack\\.yaml\\'"  "insert-stack.yaml.template")
-                ("\\.chs\\'"        "insert.chs")
                 ("\\.eproj-info\\'" "insert.eproj-info")
                 ("\\.gitignore\\'"  "insert.haskell.gitignore")
 
