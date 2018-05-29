bafda34da20bbf2d4f9afe13e6f78f953fc9c627
Sergey Vinokurov <serg.foo@gmail.com>
2018-05-29 01:26:15 +0100

    Improve module headers for Haskell auto-insert templates
    It turns out there's no 'Created' field. Moreover, empty 'Stability'
    and 'Portability' just clutter the header.

--- resources/auto-insert/insert-haskell-tests.hs
+++ resources/auto-insert/insert-haskell-tests.hs
@@ -1,14 +1,9 @@
+----------------------------------------------------------------------------
 -- |
 -- Module      :  ${haskell path to module name}
 -- Copyright   :  (c) ${author} ${date year}
 -- License     :  BSD3-style (see LICENSE)
---
 -- Maintainer  :  ${email}
--- Created     :  ${date}
--- Stability   :
--- Portability :
---
---
 ----------------------------------------------------------------------------
 
 module ${haskell path to module name} (main) where
