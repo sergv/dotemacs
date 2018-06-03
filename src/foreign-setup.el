eb4d6ca5a4af66b8467797b6e53a71ec150494ec
Sergey Vinokurov <serg.foo@gmail.com>
2018-06-04 00:47:36 +0100

    Prefer file search implemented in native code

---
+++ src/foreign-setup.el
@@ -0,0 +1,22 @@
+;; foreign-setup.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created:  3 June 2018
+;; Description:
+
+(defcustom use-foreign-libraries? t
+  "Whether to use foreign extensions to Emacs.")
+
+(when use-foreign-libraries?
+  (load (fold-platform-os-type
+         "libemacs-native"
+         (error "Shared module is not yet available on Windows"))))
+
+(provide 'foreign-setup)
+
+;; Local Variables:
+;; End:
+
+;; foreign-setup.el ends here
