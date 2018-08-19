3fe503df9a7a81e51eec3dcb0ddd6d76ceb2b9e1
Sergey Vinokurov <serg.foo@gmail.com>
2018-08-20 00:15:36 +0100

    Fix eproj-update-current-buffer-within-its-project!

--- src/eproj/eproj-tag-index.el
+++ src/eproj/eproj-tag-index.el
@@ -11,6 +11,12 @@
 (defun empty-eproj-tag-index ()
   (cons 'eproj-tag-index (make-hash-table :test #'equal :size 997)))
 
+(defsubst eproj-tag-index--create (tbl)
+  (cons 'eproj-tag-index tbl))
+
+(defun empty-eproj-tag-index ()
+  (eproj-tag-index--create (make-hash-table :test #'equal :size 997)))
+
 (defun eproj-tag-index-p (index)
   (and (consp index)
        (eq 'eproj-tag-index (car index))
@@ -34,6 +40,28 @@
 
 (defun eproj-tag-index-keys (index)
   (hash-table-keys (cdr index)))
+
+(defun eproj-tag-index-map-values! (f index)
+  "Destructively rewrite values in INDEX index by mapping function F."
+  (cl-assert (eproj-tag-index-p index))
+  (let* ((old-tbl (cdr index))
+         (new-tbl (make-hash-table :test #'equal
+                                   :size (hash-table-size old-tbl))))
+    (maphash (lambda (key value)
+               (puthash key (funcall f value) new-tbl))
+             old-tbl)
+    (setcdr index new-tbl)))
+
+(defun eproj-tag-index-merge! (index-a index-b)
+  "Add all entries of INDEX-B to INDEX-A while combining values for
+equal keys using `append'."
+  (cl-assert (eproj-tag-index-p index-a))
+  (cl-assert (eproj-tag-index-p index-b))
+  (hash-table-merge-with!
+   (lambda (tags-a tags-b)
+     (append tags-a tags-b))
+   (cdr index-a)
+   (cdr index-b)))
 
 (provide 'eproj-tag-index)
 
