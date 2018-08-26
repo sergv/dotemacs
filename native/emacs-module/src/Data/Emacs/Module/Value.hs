77e897b816ca97769ec37f7b64691807cb419783
Sergey Vinokurov <serg.foo@gmail.com>
2018-08-26 19:46:46 +0100

    Merge commit '420a4a509824b9e02bb47e5b53ca964aca6adb71' from 'https://github.com/sergv/emacs-module.git' into 'native/emacs-module'

---
+++ native/emacs-module/src/Data/Emacs/Module/Value.hs
@@ -0,0 +1,15 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Emacs.Module.RawValue
+-- Copyright   :  (c) Sergey Vinokurov 2018
+-- License     :  BSD3-style (see LICENSE)
+-- Maintainer  :  serg.foo@gmail.com
+----------------------------------------------------------------------------
+
+module Data.Emacs.Module.Value
+  ( RawValue
+  , Value(..)
+  ) where
+
+import Data.Emacs.Module.Raw.Value (RawValue)
+import Data.Emacs.Module.Value.Internal
