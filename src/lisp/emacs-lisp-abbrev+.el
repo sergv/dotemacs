57d70af78fce34dc0868d38d6d82f83d60b419c9
Sergey Vinokurov <serg.foo@gmail.com>
2019-09-06 21:27:28 +0100

    Optimize elisp abbreviations to do more in compile time

--- src/lisp/emacs-lisp-abbrev+.el
+++ src/lisp/emacs-lisp-abbrev+.el
@@ -37,50 +37,57 @@
         abbrev+-abbreviations
         (list
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("beginning" 3)
-                      ("of"        1)
-                      ("line"      1)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("beginning" 3)
+                        ("of"        1)
+                        ("line"      1))))
           :action-type 'literal-string
           :action-data "(beginning-of-line")
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("buffer"    3)
-                      ("substring" 3)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("buffer"    3)
+                        ("substring" 3))))
           :action-type 'literal-string
           :action-data "(buffer-substring")
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("buffer"     3)
-                      ("substring"  3)
-                      ("no"         1)
-                      ("properties" 0)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("buffer"     3)
+                        ("substring"  3)
+                        ("no"         1)
+                        ("properties" 0))))
           :action-type 'literal-string
           :action-data "(buffer-substring-no-properties")
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("def"      1)
-                      ("keys"     1)
-                      ("for"      1)
-                      ("map"      1)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("def"      1)
+                        ("keys"     1)
+                        ("for"      1)
+                        ("map"      1))))
           :action-type 'literal-string
           :action-data "(def-keys-for-map")
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("multiple" 1)
-                      ("value"    1)
-                      ("bind"     1)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("multiple" 1)
+                        ("value"    1)
+                        ("bind"     1))))
           :action-type 'literal-string
           :action-data "(multiple-value-bind")
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("destructuring" 3)
-                      ("bind"          1)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("destructuring" 3)
+                        ("bind"          1))))
           :action-type 'literal-string
           :action-data "(destructuring-bind")
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("lambda" 3)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("lambda" 3))))
           :action-type 'literal-string
           :action-data "(lambda")
          (make-abbrev+-abbreviation
@@ -88,69 +95,79 @@
           :action-type 'literal-string
           :action-data "(message")
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("condition" 2)
-                      ("case"      3)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("condition" 2)
+                        ("case"      3))))
           :action-type 'literal-string
           :action-data "(condition-case")
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("point" 1)
-                      ("min"   2)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("point" 1)
+                        ("min"   2))))
           :action-type 'literal-string
           :action-data "(point-min")
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("point" 1)
-                      ("max"   2)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("point" 1)
+                        ("max"   2))))
           :action-type 'literal-string
           :action-data "(point-max")
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("unwind"  2)
-                      ("protect" 2)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("unwind"  2)
+                        ("protect" 2))))
           :action-type 'literal-string
           :action-data "(unwind-protect")
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("beginning"  2)
-                      ("of"         1)
-                      ("line"       1)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("beginning"  2)
+                        ("of"         1)
+                        ("line"       1))))
           :action-type 'literal-string
           :action-data "(beginning-of-line")
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("end"  2)
-                      ("of"   1)
-                      ("line" 1)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("end"  2)
+                        ("of"   1)
+                        ("line" 1))))
           :action-type 'literal-string
           :action-data "(end-of-line")
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("line"      1)
-                      ("beginning" 1)
-                      ("position"  3)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("line"      1)
+                        ("beginning" 1)
+                        ("position"  3))))
           :action-type 'literal-string
           :action-data "(line-beginning-position")
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("line"     1)
-                      ("end"      1)
-                      ("position" 3)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("line"     1)
+                        ("end"      1)
+                        ("position" 3))))
           :action-type 'literal-string
           :action-data "(line-end-position")
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("with"     1)
-                      ("current"  1)
-                      ("buffer"   1)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("with"     1)
+                        ("current"  1)
+                        ("buffer"   1))))
           :action-type 'literal-string
           :action-data "(with-current-buffer")
          (make-abbrev+-abbreviation
-          :trigger (make-abbrev+-re-for-lisp-func-name
-                    '(("with"   1)
-                      ("temp"   1)
-                      ("buffer" 1)))
+          :trigger (eval-when-compile
+                     (make-abbrev+-re-for-lisp-func-name
+                      '(("with"   1)
+                        ("temp"   1)
+                        ("buffer" 1))))
           :action-type 'literal-string
           :action-data "(with-temp-buffer")
 
