39ac46aa2fde8cf70b8611ef670cd8ec24256dcf
Sergey Vinokurov <serg.foo@gmail.com>
2020-02-21 19:37:21 +0000

    Compute abbrev+s only once during Emacs run
    Before this change they were recomputed anew for each buffer for no good reason.

--- src/haskell/haskell-ghc-extension-list.el
+++ src/haskell/haskell-ghc-extension-list.el
@@ -6,45 +6,28 @@
 ;; Created: 17 January 2020
 ;; Description:
 
-(defvar haskell-language-extensions nil
-  "List of Haskell extensions for current GHC in the PATH.
-
-See http://www.haskell.org/ghc/docs/7.6.3/html/users_guide/flag-reference.html
-for more information.")
-
-(defvar haskell-language-extensions--initialized? nil
-  "Whether `haskell-language-extensions' has been properly initialized.")
-
-;;;###autoload
-(defun get-haskell-language-extensions ()
-  ;; make this list from documentation, e.g.
+;;;###autoload (autoload 'get-haskell-language-extensions "haskell-ghc-extension-list" nil t)
+(defun-once get-haskell-language-extensions
+  ;; Make this list from documentation, e.g.
   ;; http://www.haskell.org/ghc/docs/7.6.3/html/users_guide/flag-reference.html
   ;; command: '<,'>s/^-X\([^\t]+\)\t\([^\t]+\)\t[^\t]+\t-\(?:X\(.*\)\)?/("\1" "\2" "\3")/
-  (unless haskell-language-extensions--initialized?
-    (if-let (ghc-exec (cached-executable-find "ghc"))
+  (if-let (ghc-exec (cached-executable-find "ghc"))
       (with-temp-buffer
         (call-process ghc-exec
                       nil
                       (current-buffer)
                       nil
                       "--supported-extensions")
-        (setf haskell-language-extensions
-              (sort
-               (split-string (buffer-substring-no-properties (point-min) (point-max))
-                             "[\n\r]+"
-                             t
-                             "[ \t]+")
-               #'string<)
-              haskell-language-extensions--initialized? t)
-
-        ;; ;; Propagate info to `attrap.el'
-        ;; (setf attrap-haskell-extensions haskell-language-extensions)
-        )
-      (error "Cannot find ghc in PATH - refusing to proceed")))
-  haskell-language-extensions)
+        (sort
+         (split-string (buffer-substring-no-properties (point-min) (point-max))
+                       "[\n\r]+"
+                       t
+                       "[ \t]+")
+         #'string<))
+    (error "Cannot find ghc in PATH - refusing to proceed")))
 
 
-(provide 'haskell-ghc-etxension-list)
+(provide 'haskell-ghc-extension-list)
 
 ;; Local Variables:
 ;; End:
