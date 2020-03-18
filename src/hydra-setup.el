3080fad09eaf0904cc07bda77c320361ce6c3a66
Sergey Vinokurov <serg.foo@gmail.com>
2020-03-18 16:32:07 +0000

    Hydras for Haskell and vim

--- src/hydra-setup.el
+++ src/hydra-setup.el
@@ -13,17 +13,23 @@
                                   'posframe
                                 'lv))
 
-;; (def-keys-for-map hydra-base-map
-;;   )
-
-(defmacro defhydra-ext (name body &optional docstring &rest heads)
+(defmacro defhydra-ext (name args &optional docstring &rest heads)
   "Like `defhydra' but also binds standard keys I expect to find across
 all hydras in my setup."
   (declare (indent defun) (doc-string 3))
-  `(defhydra ,name ,body
+  `(defhydra ,name ,args
      ,docstring
      ,@heads
      ("<escape>" nil)))
+
+(defmacro defhydra-derive (name parent args &optional docstring &rest heads)
+  (declare (indent defun) (doc-string 4))
+  `(defhydra-ext ,name ,args
+     ,(when docstring (concat (hydra--prop parent "/docstring") "\n" (s-trim-left docstring)))
+     ,@(cl-delete-duplicates
+        (append (hydra--prop parent "/heads") heads)
+        :key #'car
+        :test #'equal)))
 
 (provide 'hydra-setup)
 
