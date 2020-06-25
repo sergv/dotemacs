dc5dd6ef5d8752231fe0917fea0df2bd55c72035
Sergey Vinokurov <serg.foo@gmail.com>
2020-06-25 11:17:48 +0100

    Add setup for text mode. Allow hideshow to be enabled in flycheck-error-message-mode buffers

---
+++ src/text-mode-setup.el
@@ -0,0 +1,27 @@
+;; text-mode-setup.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <sergey@debian>
+;; Created: 25 June 2020
+;; Description:
+
+;;;###autoload
+(defun text-mode-setup ()
+  (init-common :use-yasnippet nil
+               :use-comment nil
+               :use-render-formlua nil
+               :use-whitespace nil
+               :sp-slurp-sexp-insert-space nil)
+  (setq-local hs-allow-nesting t)
+  (setup-folding t nil))
+
+;;;###autoload
+(add-hook 'text-mode-hook #'text-mode-setup)
+
+(provide 'text-mode-setup)
+
+;; Local Variables:
+;; End:
+
+;; text-mode-setup.el ends here
