c3aa6b01891798a4306bbff3f8566c7ff0dea4e6
Sergey Vinokurov <serg.foo@gmail.com>
2020-09-04 00:29:48 +0100

    Enable electric quotes everywhere

--- src/typography-setup.el
+++ src/typography-setup.el
@@ -6,6 +6,7 @@
 ;; Created: Tuesday, 25 October 2016
 ;; Description:
 
+(require 'electric)
 (require 'typopunct)
 
 (provide 'typography-setup)
@@ -17,9 +18,15 @@
 
 (setq-default typopunct-buffer-language 'english)
 
+(setf electric-quote-replace-double t
+      electric-quote-comment t
+      electric-quote-string nil
+      electric-quote-context-sensitive t)
+
 ;;;###autoload
 (defun typography-setup ()
-  (typopunct-mode 1))
+  (typopunct-mode 1)
+  (electric-quote-local-mode 1))
 
 ;; Local Variables:
 ;; End:
