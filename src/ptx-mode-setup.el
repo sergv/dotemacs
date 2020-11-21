f42cf34a9692b6723be356e7f112b99a56d99d57
Sergey Vinokurov <serg.foo@gmail.com>
2020-11-21 16:50:16 +0000

    Add magic function for detecting ptx file type

--- src/ptx-mode-setup.el
+++ src/ptx-mode-setup.el
@@ -6,6 +6,8 @@
 ;; Created: 23 August 2018
 ;; Description:
 
+(require 'macro-util)
+
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.ptx\\'" . ptx-mode))
 
@@ -18,6 +20,25 @@
    :use-yasnippet nil
    :use-comment t
    :use-fci t))
+
+;;;###autoload
+(defun ptx-file-magic-function ()
+  (when-buffer-has-file
+    (save-match-data
+      (--every-p
+       (save-excursion (re-search-forward it nil t))
+       `(,(rx bol ".version" symbol-end)
+         ,(rx bol ".target" symbol-end)
+         ,(rx bol ".address_size" symbol-end)
+         ,(rx ".reg" symbol-end)
+         ,(rx "."
+              (or (seq (any ?b ?f ?u ?s)
+                       (or "8" "16" "32" "64"))
+                  "pred")
+              eow))))))
+
+;;;###autoload
+(add-to-list 'magic-mode-alist (cons #'ptx-file-magic-function #'ptx-mode))
 
 (provide 'ptx-mode-setup)
 
