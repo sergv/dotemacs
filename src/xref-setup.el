16f41f193e402f15d495d45775204f65ca0299f4
Sergey Vinokurov <serg.foo@gmail.com>
2021-01-07 22:39:12 +0000

    Add setup for xref

---
+++ src/xref-setup.el
@@ -0,0 +1,26 @@
+;; xref-setup.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <sergey@debian>
+;; Created:  7 January 2021
+;; Description:
+
+;;;###autoload
+(defun xref-setup ()
+  (def-keys-for-map xref--xref-buffer-mode-map
+    ("n" nil)
+    ("p" nil)
+    ("H" xref-revert-buffer)
+    ("h" xref-next-line)
+    ("t" xref-prev-line)))
+
+;;;###autoload
+(add-hook 'xref--xref-buffer-mode-hook #'xref-setup)
+
+(provide 'xref-setup)
+
+;; Local Variables:
+;; End:
+
+;; xref-setup.el ends here
