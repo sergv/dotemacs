b99a23899eb830de639c5b453e764a3e77129aa0
Sergey Vinokurov <serg.foo@gmail.com>
2021-01-09 18:07:40 +0000

    Merge commit '409c6abba97ca16cb59bc8e832c52478ec857206' from 'https://github.com/sergv/v.el.git' into 'third-party/v.el'

--- third-party/v.el/v-tests.el
+++ third-party/v.el/v-tests.el
@@ -8,7 +8,7 @@
 
 (require 'v)
 
-(ert-deftest v/v--find ()
+(ert-deftest v/v-find ()
   (let ((xs [1 2 3 4 5]))
     (should (equal 3
                    (v--find (< 2 it) xs)))
@@ -17,7 +17,7 @@
     (should (equal nil
                    (v--find (= 6 it) xs)))))
 
-(ert-deftest v/v--assq ()
+(ert-deftest v/v-assq ()
   (let ((xs (vector (cons 'x 1) (cons 'y "foo") (cons 'z nil))))
     (should (equal (cons 'x 1)
                    (v-assq 'x xs)))
@@ -28,7 +28,7 @@
     (should (equal nil
                    (v-assq 'w xs)))))
 
-(ert-deftest v/v--member ()
+(ert-deftest v/v-member ()
   (let ((xs (vector (cons 'x 1) (cons 'y "foo") (cons 'z nil) "foo" "bar" "baz")))
     (should-not (v-member 'x xs))
     (should-not (v-member 'y xs))
@@ -39,6 +39,24 @@
     (should (v-member "bar" xs))
     (should (v-member "baz" xs))
     (should (v-member (cons 'x 1) xs))))
+
+(ert-deftest v/v-memq ()
+  (let ((xs (vector (cons 'x 1) (cons 'y "foo") (cons 'z nil) "foo" "bar" "baz" 1 2 'a 'b)))
+    (should-not (v-memq 'x xs))
+    (should-not (v-memq 'y xs))
+    (should-not (v-memq 'z xs))
+    (should-not (v-memq 'w xs))
+
+    (should-not (v-memq "foo" xs))
+    (should-not (v-memq "bar" xs))
+    (should-not (v-memq "baz" xs))
+    (should-not (v-memq (cons 'x 1) xs))
+
+    (should (v-memq 1 xs))
+    (should (v-memq 2 xs))
+    (should (v-memq 'a xs))
+    (should (v-memq 'b xs))
+    (should-not (v-memq 'c xs))))
 
 (ert "v/.*")
 
