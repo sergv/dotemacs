ece4c602715c79bdf8c11c11a5ade1a5160ee021
Sergey Vinokurov <serg.foo@gmail.com>
2021-08-03 00:21:33 +0100

    Merge commit '5b2daf688e7bf2590b1d1c8cea06fa57114282f9' from 'git@github.com:emacs-lsp/lsp-mode.git' into 'third-party/lsp-mode'

--- third-party/lsp-mode/clients/lsp-elixir.el
+++ third-party/lsp-mode/clients/lsp-elixir.el
@@ -1,6 +1,6 @@
 ;;; lsp-elixir.el --- description -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2020 emacs-lsp maintainers
+;; Copyright (C) 2021 emacs-lsp maintainers
 
 ;; Author: emacs-lsp maintainers
 ;; Keywords: lsp, elixir
@@ -105,6 +105,19 @@ Leave as default to let `executable-find' search for it."
   :type '(repeat string)
   :package-version '(lsp-mode . "7.1"))
 
+
+(defconst lsp-elixir-ls-server-dir
+  (f-join lsp-server-install-dir "elixir-ls")
+  "Elixir-ls local server Directory")
+
+(defcustom lsp-elixir-local-server-command
+  (f-join lsp-elixir-ls-server-dir
+          (cl-first lsp-elixir-server-command))
+  "Command to start local elixir-ls binary."
+  :group 'lsp-elixir
+  :type '(repeat string)
+  :package-version '(lsp-mode . "7.1"))
+
 (defcustom lsp-elixir-enable-test-lenses t
   "Suggest Tests."
   :type 'boolean
@@ -150,9 +163,12 @@ Leave as default to let `executable-find' search for it."
 (lsp-register-client
  (make-lsp-client :new-connection (lsp-stdio-connection
                                    (lambda ()
-                                     `(,(or (executable-find
-                                            (cl-first lsp-elixir-server-command))
-                                           (lsp-package-path 'elixir-ls))
+                                     `(,(or (when (f-exists? lsp-elixir-local-server-command)
+                                              lsp-elixir-local-server-command)
+                                            (or (executable-find
+                                                 (cl-first lsp-elixir-server-command))
+                                                (lsp-package-path 'elixir-ls))
+                                            "language_server.bat")
                                        ,@(cl-rest lsp-elixir-server-command))))
                   :major-modes '(elixir-mode)
                   :priority -1
@@ -167,6 +183,8 @@ Leave as default to let `executable-find' search for it."
                                      (ht ("save" t)
                                          ("change" 2))
                                      (lsp--workspace-server-capabilities workspace)))))
+
+(lsp-consistency-check lsp-elixir)
 
 (provide 'lsp-elixir)
 ;;; lsp-elixir.el ends here
