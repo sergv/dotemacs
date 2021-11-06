66925325bed645822e34699ca721e70e471cecec
Sergey Vinokurov <serg.foo@gmail.com>
2021-11-06 17:40:35 +0000

    Merge commit '1ac42cd103ba11c37e8566e240a2272966e6f813' from 'https://github.com/NixOS/nix-mode.git' into 'third-party/nix-mode'

---
+++ third-party/nix-mode/flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "An emacs major mode for editing Nix expressions";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: let
+    systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+  in {
+    packages = forAllSystems (system: with (import nixpkgs { inherit system; }); {
+      nix-mode = let
+        emacs = emacsWithPackages (epkgs: with epkgs; [
+          org-plus-contrib
+          company
+          mmm-mode
+          f
+          magit-section
+        ]);
+      in stdenvNoCC.mkDerivation {
+        pname = "nix-mode";
+        version = "1.4.5";
+        src = self;
+        nativeBuildInputs = [ emacs texinfo git ];
+        makeFlags = [ "PREFIX=$(out)" ];
+        shellHook = ''
+          echo Run make run to get vanilla emacs with nix-mode loaded.
+        '';
+        doCheck = true;
+        meta.description = "An emacs major mode for editing Nix expressions";
+      };
+    });
+
+    defaultPackage = forAllSystems (system: self.packages.${system}.nix-mode);
+
+    # checks are run in ‘make check’ right now we should probably move
+    # these to its own derivation
+    checks = forAllSystems (system: {
+      inherit (self.packages.${system}) nix-mode;
+    });
+  };
+}
