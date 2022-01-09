d21324ef8098c676d794dc86a0d186864ac695a2
Sergey Vinokurov <serg.foo@gmail.com>
2022-01-09 14:57:08 +0000

    Add test for navigation files

---
+++ tests/eproj-sample-projects/haskell-project-with-aux-files/dep/subdep2/src/Subdep2/Bar.hs
@@ -0,0 +1,12 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Subdep2.Bar
+-- Copyright   :  (c) Sergey Vinokurov 2022
+-- License     :  Apache-2.0 (see LICENSE)
+-- Maintainer  :  serg.foo@gmail.com
+----------------------------------------------------------------------------
+
+module Subdep2.Bar where
+
+subbar :: Int -> Int
+subbar = (+3)
