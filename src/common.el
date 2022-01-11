07ab878e0f2a5bcad6523dd34f520a6c8b4bf001
Sergey Vinokurov <serg.foo@gmail.com>
2022-01-11 01:11:38 +0000

    Optimize memory allocations in lv.el

--- src/common.el
+++ src/common.el
@@ -1594,6 +1594,22 @@ are CHAR1 and CHAR2 repsectively."
 
 ;;
 
+(defun count-chars-in-string (c str)
+    "Function to count number of times character C occurs in string STR.
+
+Implementation is very straightforward and because of that fast and reliable."
+    (cl-assert (characterp c))
+    (cl-assert (stringp str))
+    (let ((res (comp-hint-fixnum 0))
+          (i (comp-hint-fixnum 0))
+          (len (length str)))
+      (while (< i len)
+        (when (eq c (aref str i))
+          (setq res (1+ (comp-hint-fixnum res))))
+        (cl-incf i))
+      res))
+;;
+
 (provide 'common)
 
 ;; Local Variables:
