293eed541a0f849f8efacac1352a98667a295423
Sergey Vinokurov <serg.foo@gmail.com>
2022-01-14 03:50:59 +0000

    Add command for editing vim macro definitions, ‘vim-cmd-edit-macro’

---
+++ src/vim-edmacro-setup.el
@@ -0,0 +1,34 @@
+;; vim-edmacro-setup.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: 14 January 2022
+;; Description:
+
+(eval-when-compile
+  (require 'macro-util))
+
+(require 'hydra-setup)
+(require 'vim-setup)
+
+(defhydra-derive hydra-vim-edmacro-normal-g hydra-vim-normal-g-ext (:exit t :foreign-keys nil :hint nil)
+  ""
+  ("#" vim-edmacro-finish))
+
+;;;###autoload
+(defun vim-edmacro-setup ()
+  (init-common :use-whitespace 'tabs-only)
+
+  (def-keys-for-map vim-normal-mode-local-keymap
+    ("g" hydra-git-rebase-from-vim-normal/body)))
+
+;;;###autoload
+(add-hook 'vim-edmacro-mode-hook #'vim-edmacro-setup)
+
+(provide 'vim-edmacro-setup)
+
+;; Local Variables:
+;; End:
+
+;; vim-edmacro-setup.el ends here
