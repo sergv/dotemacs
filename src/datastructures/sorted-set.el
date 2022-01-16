f0b337229ebfccda27a50ddd65bd709c5ad47819
Sergey Vinokurov <serg.foo@gmail.com>
2022-01-16 17:08:18 +0000

    Split datastructure.el

--- src/datastructures/sorted-set.el
+++ src/datastructures/sorted-set.el
@@ -1,9 +1,9 @@
-;; datastructures.el --- -*- lexical-binding: t; -*-
+;; sorted-set.el --- -*- lexical-binding: t; -*-
 
 ;; Copyright (C) Sergey Vinokurov
 ;;
 ;; Author: Sergey Vinokurov <serg.foo@gmail.com>
-;; Created: Tuesday,  5 March 2013
+;; Created: 16 January 2022
 ;; Description:
 
 (eval-when-compile
@@ -158,53 +158,9 @@ items and remove any duplicates."
   "Check whether SET is empty."
   (null? (sorted-set/items set)))
 
-(cl-defstruct (bimap
-               (:conc-name bimap/))
-  forward-map   ;; hash table with #'equal comparison
-  backward-map  ;; hash table with #'equal comparison
-  size)
-
-(defsubst bimap-size (bm)
-  (hash-table-count (bimap/forward-map bm)))
-
-(defun bimap-empty ()
-  (make-bimap
-   :forward-map (make-hash-table :test #'equal)
-   :backward-map (make-hash-table :test #'equal)
-   :size 0))
-
-(defun bimap-lookup (key bm &optional default)
-  (gethash key (bimap/forward-map bm) default))
-
-(defun bimap-lookup-reverse (value bm &optional default)
-  (gethash value (bimap/backward-map bm) default))
-
-(defun bimap-insert (key value bm)
-  (let* ((fwd         (bimap/forward-map bm))
-         (bwd         (bimap/backward-map bm))
-         (key-image   (gethash key fwd))
-         (value-image (gethash value bwd)))
-    (remhash key-image bwd)
-    (remhash value-image fwd)
-    (puthash key value fwd)
-    (puthash value key bwd)))
-
-(defun alist->bimap (items)
-  (let ((bm (bimap-empty)))
-    (dolist (entry items)
-      (bimap-insert (car entry) (cdr entry) bm))
-    bm))
-
-(defun bimap-delete (key bm)
-  (let ((fwd (bimap/forward-map bm))
-        (bwd (bimap/backward-map bm)))
-    (remhash (gethash key fwd) bwd)
-    (remhash key fwd)
-    bm))
-
-(provide 'datastructures)
+(provide 'sorted-set)
 
 ;; Local Variables:
 ;; End:
 
-;; datastructures.el ends here
+;; sorted-set.el ends here
