5fba0e7da5444fc9f3e2a01baf1172ee7ee69737
Sergey Vinokurov <serg.foo@gmail.com>
2022-01-18 03:13:19 +0000

    Use beginning of defun in vim to get to defun start. Configure beginning-of-defun-function for Haskell

--- src/vim-mode/vim-motions.el
+++ src/vim-mode/vim-motions.el
@@ -1240,15 +1240,6 @@ but only on the current line."
                           :end (cdr bounds)
                           :type 'inclusive))))))
 
-(defun vim--go-to-start-of-enclosing-entity ()
-  (move-to-column 0)
-  (let ((c (char-after)))
-    (while (and (not (bobp))
-                c
-                (whitespace-char? c))
-      (forward-line -1)
-      (setf c (char-after)))))
-
 (defun vim--bounds-of-string (p)
   "Return beginning and end of string at poith P."
   (save-excursion
@@ -1257,7 +1248,7 @@ but only on the current line."
             ;; scope. The “function” here is defined as any
             ;; non-whitespace entity which starts at column 0.
             (save-excursion
-              (vim--go-to-start-of-enclosing-entity)
+              (beginning-of-defun)
               (point)))
            (end p)
            (state (parse-partial-sexp start end))
