8d851c5d425898b40b1bace81c1eed58b8294f85
Sergey Vinokurov <serg.foo@gmail.com>
2022-01-22 13:20:58 +0000

    Simplify append lists. We don’t really need shared tail, this is not a concurrent chanell use case

--- src/datastructures/append-list.el
+++ src/datastructures/append-list.el
@@ -11,35 +11,29 @@
   (require 'cl)
   (require 'macro-util))
 
-(defvar append-list-tag '#:append-list)
-(defvar append-list-shared-tail-tag '#:shared-tail)
+(defvar append-list-tag '#:append-list-tag)
 
 (defsubst append-list-p (x)
   (and (consp x)
-       (eq append-list-tag (caar x))
-       (eq append-list-shared-tail-tag (cadr x))))
+       (eq append-list-tag (car (car-sure x)))))
 
 (defsubst append-list-null (xs)
   (cl-assert (append-list-p xs) "Invalid append list: %s" xs)
-  (cdar-sure xs))
+  (null (cdar-sure xs)))
 
 (defsubst append-list-empty ()
   (let ((cell (cons append-list-tag nil)))
-    (cons cell
-          ;; Extra indirection to support ‘append-list-fork-tail’ operation.
-          (cons append-list-shared-tail-tag cell))))
+    (cons cell cell)))
 
 (defsubst append-list-singleton (item)
   (let ((cell (cons item nil)))
-    (cons (cons append-list-tag cell)
-          (cons append-list-shared-tail-tag cell))))
+    (cons (cons append-list-tag cell) cell)))
 
 (defsubst append-list-append! (xs x)
   (cl-assert (append-list-p xs) "Invalid append list: %s" xs)
-  (let ((shared-tl (cdr-sure xs)))
-    (setcdr-sure shared-tl
-                 (setcdr-sure (cdr-sure shared-tl)
-                              (cons x nil)))))
+  (setcdr-sure xs
+                 (setcdr-sure (cdr-sure xs)
+                              (cons x nil))))
 
 (defun append-list-prepend! (xs x)
   (cl-assert (append-list-p xs) "Invalid append list: %s" xs)
@@ -49,18 +43,11 @@
                               (cdr-sure hd)))
       (append-list-append! xs x))))
 
-(defsubst append-list-fork-tail (xs)
+(defun append-list-get-last-cons (xs)
   (cl-assert (append-list-p xs) "Invalid append list: %s" xs)
-  (let ((hd        (car-sure xs))
-        (shared-tl (cdr-sure xs)))
-    (if (cdr-sure hd)
-        (cons (cons append-list-tag
-                    (cdr-sure shared-tl))
-              shared-tl)
-      (progn
-        (cl-assert (eq hd (cdr shared-tl)))
-        (cons (cdr-sure shared-tl)
-              shared-tl)))))
+  (when (append-list-null xs)
+    (error "Cannot get last cons of an empty list"))
+  (cdr-sure xs))
 
 (defsubst append-list-reify (xs)
   (cl-assert (append-list-p xs) "Invalid append list: %s" xs)
@@ -68,17 +55,15 @@
 
 (defsubst append-list-extend-with! (xs ys)
   "Make sure XS ends with elements from YS. It’s *not specified* what happens if
-YS is appended to after this operation. XS may or may not receive those elements."
+YS is appended to after this operation. XS may or may not receive those elements.
+
+Semantically YS should be considered consumed here and best not used afterwards."
   (cl-assert (append-list-p xs) "Invalid append list: %s" xs)
   (cl-assert (append-list-p ys) "Invalid append list: %s" ys)
-  ;; Don’t do anything if secon list is empty
-  (when-let (ys-contents  (cdar-sure ys))
-    (let ((shared-tl-xs (cdr-sure xs))
-          (shared-tl-ys (cdr-sure ys)))
-      (cl-assert (eq append-list-shared-tail-tag (car shared-tl-xs)))
-      (cl-assert (eq append-list-shared-tail-tag (car shared-tl-ys)))
-      (setcdr-sure xs shared-tl-ys)
-      (setcdr-sure (cdr-sure shared-tl-xs) ys-contents))))
+  ;; Don’t do anything if second list is empty
+  (when-let (ys-contents (cdar-sure ys))
+    (setcdr-sure (cdr-sure xs) ys-contents)
+    (setcdr-sure xs (cdr-sure ys))))
 
 (provide 'append-list)
 
