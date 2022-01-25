63458bd17236579c77caa6fcec4729b834644a4f
Sergey Vinokurov <serg.foo@gmail.com>
2022-01-25 01:17:58 +0000

    Pull constants out of ‘haskell-autoloads.el’ into ‘haskell-constants.el’

---
+++ src/haskell/haskell-constants.el
@@ -0,0 +1,21 @@
+;; haskell-constants.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: 25 January 2022
+;; Description:
+
+(defconst +haskell-extensions+
+  '("hs" "hsig" "lhs" "hsc" "chs" "hs-boot" "lhs-boot" "alex" "x" "lx" "happy" "y" "ly" "ag" "lag"))
+
+(defconst +haskell-syntax-modes+
+  '(haskell-mode haskell-literate-mode haskell-c-mode haskell-c2hs-mode alex-mode happy-mode uuag-mode)
+  "List of modes that use haskell syntax.")
+
+(provide 'haskell-constants)
+
+;; Local Variables:
+;; End:
+
+;; haskell-constants.el ends here
