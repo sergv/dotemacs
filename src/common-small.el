c209a69915eedb3399c23dea8f2874db79b485ce
Sergey Vinokurov <serg.foo@gmail.com>
2022-02-06 18:35:52 +0000

    Better place or compatibility definitions of ‘comp-hint-cons’ and ‘comp-hint-fixnum’

--- src/common-small.el
+++ src/common-small.el
@@ -6,6 +6,20 @@
 ;; Created:  3 August 2021
 ;; Description:
 
+(when-emacs-version (or (< it 28)
+                        (not (and (fboundp #'native-comp-available-p)
+                                  (native-comp-available-p))))
+  (defsubst comp-hint-cons (x)
+    (cl-assert (and x (consp x)))
+    x)
+  (defsubst comp-hint-fixnum (x)
+    (cl-assert (fixnump x))
+    x))
+
+(when-emacs-version (<= 28 it )
+  ;; For comp-hint-cons and comp-hint-fixnum.
+  (require 'comp))
+
 (defsubst char= (a b)
   (declare (pure t) (side-effect-free t))
   (char-equal a b))
@@ -77,6 +91,7 @@
   (declare (pure t) (side-effect-free t))
   (concat v))
 
+;; todo: remove
 (defsubst char=? (a b)
   (declare (pure t) (side-effect-free t))
   (char-equal a b))
