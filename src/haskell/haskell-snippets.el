5a4e50986bfca0c17b515bbca9f61c4e89a05e1e
Sergey Vinokurov <serg.foo@gmail.com>
2022-05-11 20:22:58 +0100

    Consolidate pragmas and abbrevs. Improve abbrev for insering pragmas, add tests

---
+++ src/haskell/haskell-snippets.el
@@ -0,0 +1,47 @@
+;; haskell-snippets.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: 11 May 2022
+;; Description:
+
+(require 'yasnippet-setup)
+
+(defconst haskell-snippets--cabal-run-header--body
+  "#!/usr/bin/env cabal
+{- cabal:
+build-depends:
+  , base
+  , containers
+  , filepath
+  , directory
+  , text${1:
+  , ${2:vector ^>= 0.12}
+}-}")
+
+(defconst haskell-snippets--cabal-run-header-snippet
+  (concat
+   "# key: #!
+# name: cabal run header for standalone scripts
+# --\n"
+   haskell-snippets--cabal-run-header--body))
+
+(defun haskell-snippets-install! (mode)
+  (let ((snippets (list haskell-snippets--cabal-run-header-snippet))
+        (defs nil))
+    (with-temp-buffer
+      (dolist (snip snippets)
+        (erase-buffer)
+        (insert snip)
+        (setf defs
+              (append (yas--parse-multiple-templates)
+                      defs))))
+    (yas-define-snippets mode defs)))
+
+(provide 'haskell-snippets)
+
+;; Local Variables:
+;; End:
+
+;; haskell-snippets.el ends here
