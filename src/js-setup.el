f40d036e7c0a4add1205eab04b81606fc7a72476
Sergey Vinokurov <serg.foo@gmail.com>
2022-07-25 00:05:18 +0100

    Improve integration of hideshow with yafolding. Now folding functions fold with hideshow when point is either at the start of a sexp or a comment. Previously is was folding only for sexps

--- src/js-setup.el
+++ src/js-setup.el
@@ -25,14 +25,14 @@
 ;;;###autoload
 (defun js2-hide-indented-or-sexp ()
   (interactive)
-  (if (folding-outline-on-sexp?)
+  (if (folding-outline-on-sexp-or-commented?)
       (hs-hide-block)
     (js2-mode-hide-element)))
 
 ;;;###autoload
 (defun js2-show-indented-or-sexp ()
   (interactive)
-  (if (folding-outline-on-sexp?)
+  (if (folding-outline-on-sexp-or-commented?)
       (hs-show-block)
     (js2-mode-show-element)))
 
