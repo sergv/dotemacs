786f42b8f3d4947899a9a0c255cc1deed7d0ebf0
Sergey Vinokurov <serg.foo@gmail.com>
2022-09-01 22:57:54 +0100

    Revamp compilations. Make command selection smarter and be able to automatically prepend ‘nix develop --command’ to the compilation command

---
+++ src/nix-integration.el
@@ -0,0 +1,27 @@
+;; nix-integration.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created:  1 September 2022
+;; Description:
+
+;;;###autoload
+(defun nix-maybe-call-via-flakes (args &optional proj-dir)
+  "If current project has flake.nix then return ‘nix develop
+--command ARGS’, otherwise just return ARGS."
+  (let ((nix-exe (cached-executable-find "nix")))
+    (if nix-exe
+        (if-let ((root (or proj-dir (configurable-compilation-proj-dir))))
+            (let ((flake (concat root "/flake.nix")))
+              (if (file-exists-p flake)
+                  (cons nix-exe (cons "develop" (cons "--no-warn-dirty" (cons "--command" args))))
+                args))
+          args))))
+
+(provide 'nix-integration)
+
+;; Local Variables:
+;; End:
+
+;; nix-integration.el ends here
