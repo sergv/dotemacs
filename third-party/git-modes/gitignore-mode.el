7fbe3947be2b33aff47df95072c5129840cd11ac
Sergey Vinokurov <serg.foo@gmail.com>
2022-09-21 22:14:07 +0100

    Merge commit 'eca3bb42ea8abed9ef8549b2ac91bbea445c5bb5' from 'https://github.com/magit/git-modes.git' into 'third-party/git-modes'

--- third-party/git-modes/gitignore-mode.el
+++ third-party/git-modes/gitignore-mode.el
@@ -1,27 +1,27 @@
-;;; gitignore-mode.el --- Major mode for editing .gitignore files -*- lexical-binding: t; -*-
+;;; gitignore-mode.el --- Major mode for editing .gitignore files  -*- lexical-binding:t -*-
 
-;; Copyright (c) 2012-2013  Sebastian Wiesner
-;; Copyright (C) 2012-2018  The Magit Project Contributors
+;; Copyright (c) 2012-2013 Sebastian Wiesner
+;; Copyright (C) 2012-2022 The Magit Project Contributors
 
 ;; Author: Sebastian Wiesner <lunaryorn@gmail.com>
 ;; Maintainer: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/magit/git-modes
 ;; Keywords: convenience vc git
 
-;; This file is not part of GNU Emacs.
+;; SPDX-License-Identifier: GPL-3.0-or-later
 
-;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
-
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 3 of the License,
+;; or (at your option) any later version.
+;;
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
-;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
@@ -45,7 +45,7 @@
   ;; Disable syntactic font locking, because comments are only valid at
   ;; beginning of line.
   (setq font-lock-defaults '(gitignore-mode-font-lock-keywords t t))
-  (set (make-local-variable 'conf-assignment-sign) nil))
+  (setq-local conf-assignment-sign nil))
 
 ;;;###autoload
 (dolist (pattern (list "/\\.gitignore\\'"
