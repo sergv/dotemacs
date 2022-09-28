b67def09c6ad964c5a3e0642f3dc32bd551e2cba
Sergey Vinokurov <serg.foo@gmail.com>
2022-09-28 22:47:10 +0100

    Add auto-insert template for haskell benchmarks

---
+++ resources/auto-insert/insert-haskell-bench.hs
@@ -0,0 +1,23 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  ${haskell path to module name}
+-- Copyright   :  (c) ${author} ${date year}
+-- License     :  ${license spdx} (see LICENSE)
+-- Maintainer  :  ${email}
+----------------------------------------------------------------------------
+
+module ${haskell path to module name} (main) where
+
+import Test.Tasty.Bench
+
+fibo :: Int -> Integer
+fibo n = if n < 2 then toInteger n else fibo (n - 1) + fibo (n - 2)
+
+main :: IO ()
+main = defaultMain
+  [ bgroup "fibonacci numbers"
+    [ bcompare "tenth"  $ bench "fifth"     $ nf fibo  5
+    ,                     bench "tenth"     $ nf fibo 10
+    , bcompare "tenth"  $ bench "twentieth" $ nf fibo 20
+    ]
+  ]
