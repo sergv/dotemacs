0610bee048e2a39968e0dfd9fabac139fb74a946
Sergey Vinokurov <serg.foo@gmail.com>
2022-11-21 19:36:08 +0000

    Fix sorting of import blocks surrounded by CPP directives

--- third-party/haskell-mode/haskell-sort-imports.el
+++ third-party/haskell-mode/haskell-sort-imports.el
@@ -108,7 +108,7 @@ within that region."
 
 (defun haskell-sort-imports-goto-group-start ()
   "Go to the start of the import group."
-  (when (or (and (search-backward "\n\n" nil t 1)
+  (when (or (and (search-backward-regexp (rx (or (seq bol "#") (seq (char ?\n) (char ?\n)))) nil t 1)
                  (goto-char (+ 2 (line-end-position))))
             (when (search-backward-regexp "^module " nil t 1)
               (goto-char (1+ (line-end-position))))
