c86045a6ebf8810a5bfddc2d5b8020493a69e8e7
Sergey Vinokurov <serg.foo@gmail.com>
2022-12-19 00:57:25 +0000

    New eproj option ‘no-default-proj’ to ignore default projects for specified modes in the context of current project

---
+++ tests/eproj-sample-projects/haskell-project-with-manual-default/default/Baz.hs
@@ -0,0 +1,12 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Baz
+-- Copyright   :  (c) sergey 2022
+-- License     :  Apache-2.0 (see LICENSE)
+-- Maintainer  :  serg.foo@gmail.com
+----------------------------------------------------------------------------
+
+module Baz where
+
+baz :: Int -> Int
+baz = (+3)
