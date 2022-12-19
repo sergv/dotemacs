17c48afa932bfbdad0002d17361a719ec0e1fe46
Sergey Vinokurov <serg.foo@gmail.com>
2022-12-19 00:57:25 +0000

    New eproj option ‘no-default-proj’ to ignore default projects for specified modes in the context of current project

---
+++ tests/eproj-sample-projects/haskell-project-with-manual-default/main/Foo.hs
@@ -0,0 +1,12 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Foo
+-- Copyright   :  (c) sergey 2022
+-- License     :  Apache-2.0 (see LICENSE)
+-- Maintainer  :  serg.foo@gmail.com
+----------------------------------------------------------------------------
+
+module Foo where
+
+foo :: Int -> Int
+foo = (+1)
