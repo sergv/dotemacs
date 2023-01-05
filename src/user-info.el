c5de21a9f2a053f5a34d2df9660ab51a40935495
Sergey Vinokurov <serg.foo@gmail.com>
2023-01-05 22:38:37 +0000

    Consolidate user info under src/user-info.el. Load it every time so that it will have effect when restoring dumps

---
+++ src/user-info.el
@@ -0,0 +1,17 @@
+;; user-info.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created:  5 January 2023
+;; Description:
+
+(setf user-mail-address "serg.foo@gmail.com"
+      user-full-name "Sergey Vinokurov")
+
+(provide 'user-info)
+
+;; Local Variables:
+;; End:
+
+;; user-info.el ends here
