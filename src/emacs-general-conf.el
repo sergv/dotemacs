ceb1297d80f3c59f52344fa63aa92736084fe3a2
Sergey Vinokurov <serg.foo@gmail.com>
2023-01-05 22:38:37 +0000

    Consolidate user info under src/user-info.el. Load it every time so that it will have effect when restoring dumps

--- src/emacs-general-conf.el
+++ src/emacs-general-conf.el
@@ -155,9 +155,6 @@
       ;; LSP may produce pretty large outputs during its work.
       read-process-output-max 32768
 
-      user-mail-address "serg.foo@gmail.com"
-      user-full-name "Sergey Vinokurov"
-
       ;; I’ll compile everything I need ahead of time.
       native-comp-deferred-compilation nil
 
