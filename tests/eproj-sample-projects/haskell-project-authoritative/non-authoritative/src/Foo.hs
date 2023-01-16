178ad7b63f7cf7ff66ff35ca90da65df0a20a071
Sergey Vinokurov <serg.foo@gmail.com>
2023-01-16 19:16:06 +0000

    Allow eproj projects to be authoritative source of tags that shadow other non-authoritative sources in case multiple tags are found

---
+++ tests/eproj-sample-projects/haskell-project-authoritative/non-authoritative/src/Foo.hs
@@ -0,0 +1,15 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Foo
+-- Copyright   :  (c) Sergey Vinokurov 2023
+-- License     :  Apache-2.0 (see LICENSE)
+-- Maintainer  :  serg.foo@gmail.com
+----------------------------------------------------------------------------
+
+module Foo where
+
+foo :: Int -> Int
+foo = id
+
+bar :: Double
+bar = 3.1415
