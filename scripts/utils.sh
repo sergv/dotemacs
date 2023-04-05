b863c5f64075e0117494ba0eb9bc657f68259785
Sergey Vinokurov <serg.foo@gmail.com>
2023-04-05 19:14:26 +0100

    Make emacs configurable in recompilation scripts

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -6,7 +6,7 @@
 #
 
 function native-comp-available() {
-    emacs -Q --batch \
+    "$emacs" -Q --batch \
           --eval "(message \"%s\" (and (fboundp #'native-comp-available-p) (native-comp-available-p)))" 2>&1
 }
 
