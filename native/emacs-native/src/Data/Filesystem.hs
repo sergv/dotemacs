13f11e34c8858ca77125bb2e29756fdf327547ce
Sergey Vinokurov <serg.foo@gmail.com>
2023-05-09 02:58:06 +0100

    Merge commit 'e03acbc0d87eb91686505d4c6b5b03937491b390' from 'git@github.com:sergv/emacs-native.git' into 'native/emacs-native'

---
+++ native/emacs-native/src/Data/Filesystem.hs
@@ -0,0 +1,113 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Filesystem
+-- Copyright   :  (c) Sergey Vinokurov 2018
+-- License     :  BSD3-style (see LICENSE)
+-- Maintainer  :  serg.foo@gmail.com
+----------------------------------------------------------------------------
+
+module Data.Filesystem
+  ( FollowSymlinks(..)
+  , findRec
+  , AbsDir(..)
+  , RelDir(..)
+  , AbsFile(..)
+  , RelFile(..)
+  ) where
+
+import Control.Concurrent.Async
+import Control.Monad.Catch
+import Data.Coerce
+import Data.Foldable
+import Data.NBSem
+import System.Directory.OsPath.FileType as Streaming
+import System.Directory.OsPath.Streaming as Streaming
+import System.OsPath
+
+import Emacs.Module.Assert
+
+data FollowSymlinks a =
+    -- | Recurse into symlinked directories
+    FollowSymlinks
+  | -- | Do not recurse into symlinked directories, but possibly report them.
+    ReportSymlinks (AbsDir -> RelDir -> IO (Maybe a))
+
+newtype AbsDir  = AbsDir  { unAbsDir  :: OsPath }
+newtype RelDir  = RelDir  { unRelDir  :: OsPath }
+newtype AbsFile = AbsFile { unAbsFile :: OsPath }
+newtype RelFile = RelFile { unRelFile :: OsPath }
+
+{-# INLINE findRec #-}
+findRec
+  :: forall a f ff. (WithCallStack, Foldable f, Foldable ff)
+  => FollowSymlinks a
+  -> Int                        -- ^ Extra search threads to run in parallel.
+  -> (AbsDir -> RelDir -> Bool) -- ^ Whether to visit a directory.
+  -> (AbsDir -> AbsFile -> RelFile -> IO (f a))
+                                -- ^ What to do with a file. Receives original directory it was located in.
+  -> (a -> IO ())               -- ^ Consume output
+  -> ff AbsDir                  -- ^ Where to start search.
+  -> IO ()
+findRec followSymlinks extraJobs dirPred filePred consumeOutput roots = do
+  sem <- newNBSem extraJobs
+  let runWithRoot :: AbsDir -> IO () -> IO ()
+      runWithRoot currRoot goNext = doDir currRoot (coerce takeFileName currRoot) goNext
+        where
+          currRootWithTrailingSep :: AbsDir
+          currRootWithTrailingSep = AbsDir $ addTrailingPathSeparator $ unAbsDir currRoot
+
+          doDir :: AbsDir -> RelDir -> IO () -> IO ()
+          doDir absPath relPath processNextDir
+            | dirPred absPath relPath = do
+              acquired <- tryAcquireNBSem sem
+              if acquired
+              then
+                withAsync (goDirRelease absPath) $ \yAsync ->
+                  processNextDir *> wait yAsync
+              else
+                goDir absPath *> processNextDir
+            | otherwise =
+              processNextDir
+
+          goDir :: AbsDir -> IO ()
+          goDir d =
+            bracket
+              (Streaming.openDirStream (unAbsDir d))
+              Streaming.closeDirStream
+              (goDirStream d)
+
+          goDirRelease :: AbsDir -> IO ()
+          goDirRelease d =
+            bracket
+              (Streaming.openDirStream (unAbsDir d))
+              (\stream -> Streaming.closeDirStream stream *> releaseNBSem sem)
+              (goDirStream d)
+
+          goDirStream :: AbsDir -> Streaming.DirStream -> IO ()
+          goDirStream (AbsDir root) stream = go
+            where
+              go :: IO ()
+              go = do
+                x <- Streaming.readDirStream stream
+                case x of
+                  Nothing -> pure ()
+                  Just y  -> do
+                    let y' :: OsPath
+                        y' = root </> y
+                    ft <- Streaming.getFileType y'
+                    case ft of
+                      Streaming.Other        -> go
+                      Streaming.File         -> doFile (AbsFile y') (RelFile y) *> go
+                      Streaming.FileSym      -> doFile (AbsFile y') (RelFile y) *> go
+                      Streaming.Directory    -> doDir (AbsDir y') (RelDir y) go
+                      Streaming.DirectorySym ->
+                        case followSymlinks of
+                          FollowSymlinks        -> doDir (AbsDir y') (RelDir y) go
+                          ReportSymlinks report ->
+                            traverse_ consumeOutput =<< report (AbsDir y') (RelDir y)
+
+          doFile :: AbsFile -> RelFile -> IO ()
+          doFile absPath relPath =
+            traverse_ consumeOutput =<< filePred currRootWithTrailingSep absPath relPath
+
+  foldr runWithRoot (pure ()) roots
---
+++ native/emacs-native/src/Data/Regex.hs
@@ -0,0 +1,138 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Regex
+-- Copyright   :  (c) Sergey Vinokurov 2018
+-- License     :  BSD3-style (see LICENSE)
+-- Maintainer  :  serg.foo@gmail.com
+----------------------------------------------------------------------------
+
+{-# LANGUAGE CPP               #-}
+{-# LANGUAGE LinearTypes       #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Data.Regex
+  ( fileGlobsToRegex
+
+  , compileRe
+  , compileReWithOpts
+  , reMatches
+  , reMatchesOsPath
+  , reMatchesString
+  , reMatchesByteString
+  , reMatchesShortByteString
+  , reAllByteStringMatches
+
+    -- * Reexports
+  , module Text.Regex.TDFA
+  ) where
+
+import Control.Monad.Catch (MonadThrow(..))
+import Data.ByteString.Char8 qualified as C8
+import Data.ByteString.Short (ShortByteString)
+import Data.ByteString.Short qualified as BSS
+import Data.Foldable
+import Data.Text (Text)
+import Data.Text.Builder.Linear.Buffer
+import Data.Text.Ext (textFoldLinear)
+import Prettyprinter
+import System.OsPath
+import Text.Regex.TDFA
+import Text.Regex.TDFA.Text qualified as TDFA
+
+import Data.Emacs.Path
+import Emacs.Module.Assert (WithCallStack)
+import Emacs.Module.Errors
+
+fileGlobsToRegex
+  :: (WithCallStack, MonadThrow m, Foldable f, Functor f)
+  => f Text -> m Regex
+fileGlobsToRegex patterns = compileReWithOpts compOpts $ runBuffer initRe
+  where
+    initRe :: Buffer %1 -> Buffer
+    initRe buf =
+      mkRe (toList patterns) (buf |>. '^' |>. '(') |>. ')' |>. '$'
+
+    mkRe :: [Text] -> Buffer %1 -> Buffer
+    mkRe []       buf = buf
+    mkRe (x : xs) buf =
+      -- mkRe' xs ((textFoldLinear f (buf |>. '(') x) |>. ')')
+      mkRe' xs (textFoldLinear f buf x)
+
+    mkRe' :: [Text] -> Buffer %1 -> Buffer
+    mkRe' []       buf = buf
+    mkRe' (x : xs) buf =
+      -- mkRe' xs ((textFoldLinear f (buf |>. '|' |>. '(') x) |>. ')')
+      mkRe' xs (textFoldLinear f (buf |>. '|') x)
+
+    f :: Char -> Buffer %1 -> Buffer
+    f c buf = case c of
+      '*'   -> buf |> ".*"
+      '.'   -> buf |> "\\."
+      '|'   -> buf |> "\\|"
+      '+'   -> buf |> "\\+"
+      '['   -> buf |> "\\["
+      ']'   -> buf |> "\\]"
+      '('   -> buf |> "\\("
+      ')'   -> buf |> "\\)"
+      '^'   -> buf |> "\\^"
+      '$'   -> buf |> "\\$"
+      '?'   -> buf |> "\\?"
+#ifdef mingw32_HOST_OS
+      '\\'  -> buf |> "[\\/]"
+      '/'   -> buf |> "[\\/]"
+#else
+      '\\'  -> buf |> "\\\\"
+#endif
+      other -> buf |>. other
+
+    compOpts = defaultCompOpt
+      { multiline      = False
+      , caseSensitive  = isLinux
+      , lastStarGreedy = True
+      }
+    isLinux =
+#ifdef mingw32_HOST_OS
+      False
+#else
+      True
+#endif
+
+compileRe :: (WithCallStack, MonadThrow m) => Text -> m Regex
+compileRe = compileReWithOpts compOpts
+  where
+    compOpts = defaultCompOpt
+      { multiline     = False
+      , caseSensitive = True
+      }
+
+compileReWithOpts
+  :: (WithCallStack, MonadThrow m)
+  => CompOption -> Text -> m Regex
+compileReWithOpts compOpts re =
+  case TDFA.compile compOpts execOpts re of
+    Left err -> throwM $ mkUserError "compileRe" $
+      "Failed to compile regular expression:" <+> pretty err <> ":" <> line <> pretty re
+    Right x  -> pure x
+  where
+    execOpts = defaultExecOpt
+      { captureGroups = False
+      }
+
+reMatches :: Regex -> Text -> Bool
+reMatches = match
+
+reMatchesOsPath :: Regex -> OsPath -> Bool
+reMatchesOsPath re = match re . pathToText
+
+reMatchesString :: Regex -> String -> Bool
+reMatchesString = match
+
+reMatchesByteString :: Regex -> C8.ByteString -> Bool
+reMatchesByteString = match
+
+reMatchesShortByteString :: Regex -> ShortByteString -> Bool
+reMatchesShortByteString re = match re . BSS.fromShort
+
+reAllByteStringMatches
+  :: Regex -> C8.ByteString -> AllMatches [] (MatchOffset, MatchLength)
+reAllByteStringMatches = match
