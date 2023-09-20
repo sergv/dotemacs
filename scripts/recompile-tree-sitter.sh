e2fc7b2179bd66b7f7d0d90b3f77e8b92d57c19c
Sergey Vinokurov <serg.foo@gmail.com>
2023-09-20 23:19:26 +0100

    Use Emacs help to correctly select shared library extension

--- scripts/recompile-tree-sitter.sh
+++ scripts/recompile-tree-sitter.sh
@@ -12,12 +12,25 @@ set -o pipefail
 
 set -e
 
+shared_ext=$((emacs --batch --eval '(message "%s" (car dynamic-library-suffixes))' 2>&1 || true) | head -n 1)
+
+case "${shared_ext}" in
+    ".dll" | ".so" )
+        ;;
+    * )
+        echo "Invalid shared extension: '$shared_ext'" >&2
+        exit 1
+        ;;
+esac
+
 for x in native/tree-sitter*; do
     echo "$x"
     if [[ -f "$x/src/scanner.c" ]]; then
-        "${CC:-cc}" -Os -fPIC "-I$x/src" "$x/src/parser.c" "$x/src/scanner.c" -shared -o "lib/lib$(basename "$x").so"
+        "${CC:-cc}" -Os -fPIC "-I$x/src" "$x/src/parser.c" "$x/src/scanner.c" -shared -o "lib/lib$(basename "$x")$shared_ext"
+    elif [[ -f "$x/src/parser.c" ]]; then
+        "${CC:-cc}" -Os -fPIC "-I$x/src" "$x/src/parser.c" -shared -o "lib/lib$(basename "$x")$shared_ext"
     else
-        "${CC:-cc}" -Os -fPIC "-I$x/src" "$x/src/parser.c" -shared -o "lib/lib$(basename "$x").so"
+        echo "Invalid treesitter library: '$x'" >&2
     fi
 done
 
