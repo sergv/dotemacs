e83870cf3de2911ae1108f5b2472f67ae88683d8
Sergey Vinokurov <serg.foo@gmail.com>
2023-09-25 02:06:13 +0100

    Set up ‘file-name-handler-alist’ to handle MSYS paths on Windows

--- src/windows-setup.el
+++ src/windows-setup.el
@@ -12,20 +12,30 @@
   "Return a canonical directory for comparison purposes.
 Such a directory is all lowercase, has forward-slashes as delimiters,
 and ends with a forward slash."
-  (file-name-as-directory
-   (replace-regexp-in-string "^/\\([a-z]\\)/" "\\1:"
-                             (downcase
-                              (subst-char-in-string ?\\ ?/ dir)))))
+  (when dir
+    (file-name-as-directory
+     (replace-regexp-in-string "^/\\([a-z]\\)/" "\\1:"
+                               (downcase
+                                (subst-char-in-string ?\\ ?/ dir))))))
 
 (defun cygwin-directory-name-to-emacs (dir)
   "Return a canonical directory for comparison purposes.
 Such a directory is all lowercase, has forward-slashes as delimiters,
 and ends with a forward slash."
-  (file-name-as-directory
-   (replace-regexp-in-string "^/\\([a-z]\\)/" "\\1:"
-                             (string-remove-prefix "/cygdrive"
-                                                   (downcase
-                                                    (subst-char-in-string ?\\ ?/ dir))))))
+  (when dir
+    (file-name-as-directory
+     (replace-regexp-in-string "^/\\([a-z]\\)/" "\\1:"
+                               (string-remove-prefix "/cygdrive"
+                                                     (downcase
+                                                      (subst-char-in-string ?\\ ?/ dir)))))))
+
+(defun msys-file-name-handler (func &rest args)
+  "Make Emacs transparently handle MSYS-style paths when put in
+‘file-name-handler-alist’."
+  (apply func (mapcar #'msys-directory-name-to-emacs args)))
+
+(add-to-list 'file-name-handler-alist
+             (cons "^/[a-z]/" #'msys-file-name-handler))
 
 (provide 'windows-setup)
 
