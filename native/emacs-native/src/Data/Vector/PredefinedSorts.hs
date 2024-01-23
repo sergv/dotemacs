d6db5d691dbf7e18a4d1cf946a2a0ca213ff34e2
Sergey Vinokurov <serg.foo@gmail.com>
2024-01-23 03:06:43 +0000

    Merge commit '5555ef7939f18a3267bd45c9f0293fdc1d644ce7' from 'git@github.com:sergv/emacs-native.git' into 'native/emacs-native'

--- native/emacs-native/src/Data/Vector/PredefinedSorts.hs
+++ native/emacs-native/src/Data/Vector/PredefinedSorts.hs
@@ -6,9 +6,12 @@
 -- Maintainer  :  serg.foo@gmail.com
 ----------------------------------------------------------------------------
 
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 module Data.Vector.PredefinedSorts
   ( sortInt32
   , sortWord64
+  , sortPackedCharAndIdx
   , sortVectorUnsafeChar
   , sortSortKeyPar
   ) where
@@ -18,9 +21,36 @@ import Data.FuzzyMatch.SortKey
 import Data.Int
 import Data.Vector.Primitive qualified as P
 import Data.Vector.Primitive.Mutable qualified as PM
+import Data.Vector.Unboxed.Mutable qualified as UM
 import Data.Word
 
+import Data.Packed
+
+import Data.Vector.Algorithms.FixedSort qualified as Quick
+import Data.Vector.Algorithms.Heapsort qualified as Quick
 import Data.Vector.Algorithms.Quicksort.Parameterised qualified as Quick
+
+{-# SPECIALIZE Quick.sortInplaceFM :: Quick.Sequential -> Quick.Median3 Int32 -> PM.MVector s Int32 -> ST s () #-}
+{-# SPECIALIZE Quick.heapSort      :: PM.MVector s Int32 -> ST s ()        #-}
+{-# SPECIALIZE Quick.bitonicSort   :: Int -> PM.MVector s Int32 -> ST s () #-}
+
+{-# SPECIALIZE Quick.sortInplaceFM :: Quick.Sequential -> Quick.Median3 Word64 -> PM.MVector s Word64 -> ST s () #-}
+{-# SPECIALIZE Quick.heapSort      :: PM.MVector s Word64 -> ST s ()        #-}
+{-# SPECIALIZE Quick.bitonicSort   :: Int -> PM.MVector s Word64 -> ST s () #-}
+
+{-# SPECIALIZE Quick.sortInplaceFM :: Quick.Sequential -> Quick.Median3 CharAndIdxs -> UM.MVector s CharAndIdxs -> ST s () #-}
+{-# SPECIALIZE Quick.heapSort      :: UM.MVector s CharAndIdxs -> ST s ()        #-}
+{-# SPECIALIZE Quick.bitonicSort   :: Int -> UM.MVector s CharAndIdxs -> ST s () #-}
+
+{-# SPECIALIZE Quick.sortInplaceFM :: Quick.Sequential -> Quick.Median3 Char -> PM.MVector s Char -> ST s () #-}
+{-# SPECIALIZE Quick.heapSort      :: PM.MVector s Char  -> ST s ()                                          #-}
+{-# SPECIALIZE Quick.bitonicSort   :: Int                -> PM.MVector s Char -> ST s ()                     #-}
+
+
+{-# SPECIALIZE Quick.sortInplaceFM :: Quick.ParStrategies -> Quick.Median3or5 SortKey -> PM.MVector s SortKey -> ST s () #-}
+{-# SPECIALIZE Quick.heapSort      :: PM.MVector s SortKey  -> ST s ()                                          #-}
+{-# SPECIALIZE Quick.bitonicSort   :: Int                -> PM.MVector s SortKey -> ST s ()                     #-}
+
 
 {-# NOINLINE sortInt32 #-}
 sortInt32 :: PM.MVector s Int32 -> ST s ()
@@ -29,6 +59,10 @@ sortInt32 = Quick.sortInplaceFM Quick.Sequential (Quick.Median3 @Int32)
 {-# NOINLINE sortWord64 #-}
 sortWord64 :: PM.MVector s Word64 -> ST s ()
 sortWord64 = Quick.sortInplaceFM Quick.Sequential (Quick.Median3 @Word64)
+
+{-# NOINLINE sortPackedCharAndIdx #-}
+sortPackedCharAndIdx :: UM.MVector s CharAndIdxs -> ST s ()
+sortPackedCharAndIdx = Quick.sortInplaceFM Quick.Sequential (Quick.Median3 @CharAndIdxs)
 
 sortChar :: PM.MVector s Char -> ST s ()
 sortChar = Quick.sortInplaceFM Quick.Sequential (Quick.Median3 @Char)
