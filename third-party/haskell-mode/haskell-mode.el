a3fc286cbcaf549a2d2a20c992bced6aa8164676
Sergey Vinokurov <serg.foo@gmail.com>
2024-02-24 14:00:39 +0000

    Add safety check since ‘haskell-forward-sexp’ is not useable outside of modes derived from ‘haskell-mode’ that employ its underlying lexical analysis

--- third-party/haskell-mode/haskell-mode.el
+++ third-party/haskell-mode/haskell-mode.el
@@ -951,6 +951,10 @@ If unable to move over a sexp, signal `scan-error' with three
 arguments: a message, the start of the obstacle (a parenthesis or
 list marker of some kind), and end of the obstacle."
   (interactive "^p")
+  ;; If we’re not derived then lexeme parsing doesn’t work.
+  (cl-assert (derived-mode-p 'haskell-mode)
+             nil
+             "The ‘haskell-forward-sexp’ function doesn’t work in modes not derived from ‘haskell-mode’")
   (or arg (setq arg 1))
   (if (< arg 0)
       (while (< arg 0)
