3c9e5d24555b931f4f0e70b58b69c674cd6dd0f1
Sergey Vinokurov <serg.foo@gmail.com>
2024-04-03 23:56:57 +0100

    Improve template for Haskell benchmarks to showcase automated way to perform comparisons with tasty-bench

--- resources/auto-insert/insert-haskell-bench.hs
+++ resources/auto-insert/insert-haskell-bench.hs
@@ -7,15 +7,29 @@
 module ${haskell path to module name} (main) where
 
 import Test.Tasty.Bench
+import Test.Tasty.Patterns.Printer (printAwkExpr)
 
 fibo :: Int -> Integer
 fibo n = if n < 2 then toInteger n else fibo (n - 1) + fibo (n - 2)
 
 main :: IO ()
-main = defaultMain
-  [ bgroup "fibonacci numbers"
-    [ bcompare "tenth"  $ bench "fifth"     $ nf fibo  5
-    ,                     bench "tenth"     $ nf fibo 10
-    , bcompare "tenth"  $ bench "twentieth" $ nf fibo 20
+main = do
+  let baselineName = "fibo 7"
+  defaultMain
+    [ bgroup "fibonacci numbers"
+      [ bcompare "tenth"  $ bench "fifth"     $ nf fibo  5
+      ,                     bench "tenth"     $ nf fibo 10
+      , bcompare "tenth"  $ bench "twentieth" $ nf fibo 20
+      ]
+    , mapLeafBenchmarks (addCompare baselineName) $ bgroup "fibonacci #2"
+      [ bench baselineName $ nf fibo 7
+      , bench "fibo 8" $ nf fibo 8
+      , bench "fibo 9" $ nf fibo 9
+      ]
     ]
-  ]
+
+addCompare :: String -> [String] -> Benchmark -> Benchmark
+addCompare targetBenchName (name : path)
+  | name /= targetBenchName
+  = bcompare (printAwkExpr (locateBenchmark (targetBenchName : path)))
+addCompare _ _ = id
