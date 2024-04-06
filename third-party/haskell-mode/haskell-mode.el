8aaefd0120a2bf89f2262393429b5c3640019c14
Sergey Vinokurov <serg.foo@gmail.com>
2024-04-06 01:28:15 +0100

    Move adjustments to ‘haskell-mode-syntax-table’ to its definition site so that its numerous copies will also get the adjustments

--- third-party/haskell-mode/haskell-mode.el
+++ third-party/haskell-mode/haskell-mode.el
@@ -480,6 +480,19 @@ be set to the preferred literate style."
     (dolist (charcodes haskell--char-syntax-identifiers)
       (modify-syntax-entry charcodes "w" table))
 
+    ;; Need to modify original definition of haskell-mode-syntax-table because
+    ;; it’s copied many times and copies should have these modifications too.
+
+    ;; The underscore should remain part of word so we never search within
+    ;; _c_style_identifiers.
+    (modify-syntax-entry ?_  "_" table)
+    (modify-syntax-entry ?\' "w" table)
+    (modify-syntax-entry ?@  "'" table)
+    ;; So that ‘backward-sexp’ will reach ‘Foo’ when run on ‘Foo.Ba_|_r’.
+    ;; This is relied upon by ‘paredit-raise-sexp’.
+    (modify-syntax-entry ?.  "_" table)
+    (modify-syntax-entry ?#  "_" table)
+
     table)
   "Syntax table used in Haskell mode.")
 
