2cef547a9d328c05f23782a945a8c36db560e532
Sergey Vinokurov <serg.foo@gmail.com>
2024-04-28 02:43:41 +0100

    Improve ‘transient-history--merge-entries’ to decrease number of false positive changes to persistent store contents

--- tests/misc-tests.el
+++ tests/misc-tests.el
@@ -50,6 +50,42 @@
                  '(#("foo" 0 3
                        (:file "/root/test-file.txt" :line 100 :kind "Function"))))))
 
+(ert-deftest misc-tests/transient-history--merge-entries-1 ()
+  (should (equal (transient-history--merge-entries
+                  '(transient-history
+                    (magit-branch nil)
+                    (magit-commit nil)
+                    (magit-fetch nil)
+                    (magit-gitignore nil)
+                    (magit-log ("-n256" "--graph" "--decorate"))
+                    (magit-merge nil)
+                    (magit-push ("--force-with-lease"))
+                    (magit-rebase nil ("--autostash"))
+                    (magit-stash nil)
+                    (nil))
+                  '(transient-history
+                    (magit-branch nil)
+                    (magit-commit nil)
+                    (magit-fetch nil)
+                    (magit-gitignore nil)
+                    (magit-log ("-n256" "--graph" "--decorate"))
+                    (magit-merge nil)
+                    (magit-push nil ("--force-with-lease"))
+                    (magit-rebase nil ("--autostash"))
+                    (magit-stash nil)
+                    (nil)))
+                 '(transient-history
+                   (magit-branch nil)
+                   (magit-commit nil)
+                   (magit-fetch nil)
+                   (magit-gitignore nil)
+                   (magit-log ("-n256" "--graph" "--decorate"))
+                   (magit-merge nil)
+                   (magit-push ("--force-with-lease"))
+                   (magit-rebase ("--autostash"))
+                   (magit-stash nil)
+                   (nil)))))
+
 (provide 'misc-tests)
 
 ;; Local Variables:
