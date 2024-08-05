00cb10a73321846e154d7274f38bd21487e2bffb
Sergey Vinokurov <serg.foo@gmail.com>
2024-08-05 22:51:15 +0100

    Make F6 revert buffer like F5, for my ergodox keyboard’s sake

--- src/keys.el
+++ src/keys.el
@@ -24,7 +24,7 @@
   ("<f2>"     universal-argument)
 
   ("<f5>"     revert-buffer)
-  ("<f6>"     nil)
+  ("<f6>"     revert-buffer)
 
   ("<f9>"     compile)
   ("<f12>"    start-profiler-or-report)
