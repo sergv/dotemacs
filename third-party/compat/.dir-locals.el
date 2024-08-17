4b5d0969a53a9f797e07ab6ec01347c5501be0ae
Sergey Vinokurov <serg.foo@gmail.com>
2024-08-17 17:49:47 +0100

    Merge commit 'e9203e164903a6bb7de3e58aa0d653bbcff9d3d1' from 'https://github.com/emacsmirror/compat.git' into 'third-party/compat'

---
+++ third-party/compat/.dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (fill-column . 72)
+  (sentence-end-double-space . t)
+  (show-trailing-whitespace . t)
+  (indent-tabs-mode . nil)))
