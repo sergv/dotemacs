580293c6eec2e9d09a83fe52b0a91a84cd3c947e
Sergey Vinokurov <serg.foo@gmail.com>
2025-01-08 10:28:57 +0000

    Make ‘pcomplete-ghc-flags’ have fallback in case ‘ghc’ is not available. Add ‘compile--strict-mode’ to decide whether to fail with error or use the fallback

---
+++ src/recompile-init.el
@@ -0,0 +1,16 @@
+;; recompile-init.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created:  8 January 2025
+;; Description:
+
+(defvar compile--strict-mode t)
+
+(provide 'recompile-init)
+
+;; Local Variables:
+;; End:
+
+;; recompile-init.el ends here
