1ab26a978a0b91f59b03e97998741f8256f7954d
Sergey Vinokurov <serg.foo@gmail.com>
2025-01-16 00:14:53 +0000

    Merge commit '3aa2995549d5d8b26928e8d3fa2770fd4327414e' from 'git@github.com:tree-sitter/tree-sitter-c.git' into 'native/tree-sitter-c'

--- native/tree-sitter-c/Package.swift
+++ native/tree-sitter-c/Package.swift
@@ -3,34 +3,34 @@ import PackageDescription
 
 let package = Package(
     name: "TreeSitterC",
-    platforms: [.macOS(.v10_13), .iOS(.v11)],
     products: [
         .library(name: "TreeSitterC", targets: ["TreeSitterC"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", from: "0.8.0"),
+    ],
     targets: [
-        .target(name: "TreeSitterC",
-                path: ".",
-                exclude: [
-                    "binding.gyp",
-                    "bindings",
-                    "Cargo.toml",
-                    "examples",
-                    "grammar.js",
-                    "LICENSE",
-                    "Makefile",
-                    "package.json",
-                    "README.md",
-                    "src/grammar.json",
-                    "src/node-types.json",
-                ],
-                sources: [
-                    "src/parser.c",
-                ],
-                resources: [
-                    .copy("queries")
-                ],
-                publicHeadersPath: "bindings/swift",
-                cSettings: [.headerSearchPath("src")])
-    ]
+        .target(
+            name: "TreeSitterC",
+            dependencies: [],
+            path: ".",
+            sources: [
+                "src/parser.c",
+            ],
+            resources: [
+                .copy("queries")
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")]
+        ),
+        .testTarget(
+            name: "TreeSitterCTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterC",
+            ],
+            path: "bindings/swift/TreeSitterCTests"
+        )
+    ],
+    cLanguageStandard: .c11
 )
