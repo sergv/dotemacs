8d3f262fdd2a82aac1ad2bcb59751d03a89071b4
Sergey Vinokurov <serg.foo@gmail.com>
2025-01-30 00:32:08 +0000

    Switch to Iosevka with texture healing enabled
    Keep an eye on redisplay performance, now we’re using harfbuzz for
    everything which *may* be costly.

--- src/set-up-font.el
+++ src/set-up-font.el
@@ -20,34 +20,35 @@
 (require 'set-up-paths)
 
 (defconst current-font
-  (if-let ((iosevka-slab-lig-reg  (find-font (font-spec :name "Iosevka Slab Lig" :weight 'regular :slant 'normal)))
-           (iosevka-slab-lig-bold (find-font (font-spec :name "Iosevka Slab Lig" :weight 'bold :slant 'normal)))
-           (iosevka-slab-lig-reg-it  (find-font (font-spec :name "Iosevka Slab Lig" :weight 'regular :slant 'italic)))
-           (iosevka-slab-lig-bold-it (find-font (font-spec :name "Iosevka Slab Lig" :weight 'bold :slant 'italic))))
-      iosevka-slab-lig-reg
-    (car
-     (seq-filter
-      (lambda (font)
-        (condition-case nil
-            (and (x-list-fonts font) t)
-          (error nil)))
-      (append
-       '("-*-Iosevka Slab Lig-normal-normal-normal-*-16-*-*-*-*-*-iso10646-1"
-         "-*-Iosevka Slab Lig-*-*-*-*-16-*-*-*-*-*-*-*"
-         "-*-Iosevka Slab-normal-normal-normal-*-16-*-*-*-*-*-iso10646-1"
-         "-*-Terminus-normal-normal-normal-*-16-*-*-*-*-*-iso10646-1"
-         "-*-Terminus (TTF)-normal-normal-normal-*-16-*-*-*-*-*-iso10646-1"
-         "-raster-Terminus-normal-normal-normal-*-20-*-*-*-c-*-iso8859-1")
-       (if (and (<= (display-pixel-width) 1280)
-                (<= (display-pixel-height) 1024))
-           '("-outline-Anonymous Pro-normal-normal-normal-mono-15-*-*-*-c-*-iso8859-1")
-         '("-outline-Anonymous Pro-normal-normal-normal-mono-19-*-*-*-c-*-iso8859-1"))
-       '("-unknown-Droid Sans Mono-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1"
-         "-unknown-DejaVu Sans Mono-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1"
-         "-unknown-Inconsolata-normal-normal-normal-*-17-*-*-*-m-0-iso10646-1"
-         "-unknown-Liberation Mono-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1"
-         "-unknown-FreeMono-normal-normal-normal-*-17-*-*-*-m-0-iso10646-1"
-         "-monotype-Courier New-normal-normal-normal-*-17-*-*-*-m-0-iso10646-1"))))))
+  (let ((name "Iosevka Slab Lig"))
+    (if-let ((iosevka-slab-lig-reg (find-font (font-spec :name name :weight 'regular :slant 'normal)))
+             (iosevka-slab-lig-bold (find-font (font-spec :name name :weight 'bold :slant 'normal)))
+             (iosevka-slab-lig-reg-it  (find-font (font-spec :name name :weight 'regular :slant 'italic)))
+             (iosevka-slab-lig-bold-it (find-font (font-spec :name name :weight 'bold :slant 'italic))))
+        iosevka-slab-lig-reg
+      (car
+       (seq-filter
+        (lambda (font)
+          (condition-case nil
+              (and (x-list-fonts font) t)
+            (error nil)))
+        (append
+         '("-*-Iosevka Slab Lig-normal-normal-normal-*-16-*-*-*-*-*-iso10646-1"
+           "-*-Iosevka Slab Lig-*-*-*-*-16-*-*-*-*-*-*-*"
+           "-*-Iosevka Slab-normal-normal-normal-*-16-*-*-*-*-*-iso10646-1"
+           "-*-Terminus-normal-normal-normal-*-16-*-*-*-*-*-iso10646-1"
+           "-*-Terminus (TTF)-normal-normal-normal-*-16-*-*-*-*-*-iso10646-1"
+           "-raster-Terminus-normal-normal-normal-*-20-*-*-*-c-*-iso8859-1")
+         (if (and (<= (display-pixel-width) 1280)
+                  (<= (display-pixel-height) 1024))
+             '("-outline-Anonymous Pro-normal-normal-normal-mono-15-*-*-*-c-*-iso8859-1")
+           '("-outline-Anonymous Pro-normal-normal-normal-mono-19-*-*-*-c-*-iso8859-1"))
+         '("-unknown-Droid Sans Mono-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1"
+           "-unknown-DejaVu Sans Mono-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1"
+           "-unknown-Inconsolata-normal-normal-normal-*-17-*-*-*-m-0-iso10646-1"
+           "-unknown-Liberation Mono-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1"
+           "-unknown-FreeMono-normal-normal-normal-*-17-*-*-*-m-0-iso10646-1"
+           "-monotype-Courier New-normal-normal-normal-*-17-*-*-*-m-0-iso10646-1")))))))
 
 (defvar current-font-scaling nil)
 
@@ -99,6 +100,36 @@ use that, otherwise either use past specified value or a reasonable default."
     (setf current-font-scaling effective-scaling)
     (dolist (frame (frame-list))
       (set-face-attribute 'default frame :height effective-scaling))))
+
+(when (pretty-ligatures-supported?)
+  ;; Compose ASCII with harfbuzz to take advantage of the TXTR feature (requires custom
+  ;; emacs build as well).
+  ;; See also:
+  ;; ! https://github.com/mickeynp/ligature.el/issues/53#issuecomment-1828732077
+  ;; https://github.com/githubnext/monaspace/issues/12
+  ;; https://github.com/harfbuzz/harfbuzz/discussions/4490
+  (let ((composition-spec
+         `([,(rx (+ (any
+                     ?\s ?\! ?\" ?\' ?\( ?\) ?\, ?\- ?\. ?\: ?\; ?\< ?\> ?\? ?\@ ?\[ ?\] ?\_ ?\{ ?\| ?\}
+                     (?a . ?z)
+                     (?A . ?Z)))
+                 ;; Somehow newline does not work as space does and
+                 ;; does not lead to glyph enlargement.
+                 (? (any ?\r ?\n)))
+            0
+            font-shape-gstring
+            ])))
+    (dolist (c '(?\s
+                 ?\! ?\" ?\' ?\( ?\) ?\, ?\- ?\. ?\: ?\; ?\< ?\> ?\? ?\@ ?\[ ?\] ?\_ ?\{ ?\| ?\}
+                 (?a . ?z)
+                 (?A . ?Z)))
+      (if (numberp c)
+          (set-char-table-range composition-function-table c composition-spec)
+        (cl-loop
+         for i from (car c) to (cdr c)
+         do
+         (set-char-table-range composition-function-table i composition-spec)
+         )))))
 
 (provide 'set-up-font)
 
