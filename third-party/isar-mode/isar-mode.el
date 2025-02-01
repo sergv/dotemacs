efc965c48332ba13853da98050e248c6c2091c29
Sergey Vinokurov <serg.foo@gmail.com>
2025-02-01 15:42:11 +0000

    Don’t fontity Isabelle keywords if they’re not standalone words

--- third-party/isar-mode/isar-mode.el
+++ third-party/isar-mode/isar-mode.el
@@ -348,7 +348,7 @@ Optional argument OBJECT is the string or buffer containing the text."
 
 ;; dark blue
 (defvar isar-keyword1
-  (rx bow
+  (rx symbol-start
       (or "ML"
           "ML_command"
           "ML_file"
@@ -602,11 +602,11 @@ Optional argument OBJECT is the string or buffer containing the text."
           ;; "{"
           ;; "}"
           )
-      eow))
+      symbol-end))
 
 ;; green
 (defvar isar-keyword2
-  (rx bow
+  (rx symbol-start
       (or "and"
           "assumes"
           "begin"
@@ -621,11 +621,11 @@ Optional argument OBJECT is the string or buffer containing the text."
           "obtains"
           "shows"
           "where")
-      eow))
+      symbol-end))
 
 ;; light blue
 (defvar isar-keyword3
-  (rx bow
+  (rx symbol-start
       (or "assume"
           "case"
           "define"
@@ -634,10 +634,10 @@ Optional argument OBJECT is the string or buffer containing the text."
           "show"
           "thus"
           "presume")
-      eow))
+      symbol-end))
 
 (defvar isar-tactics
-  (rx bow
+  (rx symbol-start
       (or "auto"
           "assumption"
           "blast"
@@ -654,12 +654,12 @@ Optional argument OBJECT is the string or buffer containing the text."
           "rule"
           "simp"
           "subst")
-      eow))
+      symbol-end))
 
 (defvar isar-minor
-  (rx bow
+  (rx symbol-start
       (or "is" "of" "OF" "THEN")
-      eow))
+      symbol-end))
 
 (defface isar-keyword1-face
   `((t (:inherit font-lock-keyword-face)))
