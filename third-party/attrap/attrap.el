2e349fd31e809b3cabaeb20d202d7ab86decae6c
Sergey Vinokurov <serg.foo@gmail.com>
2025-03-26 23:43:25 +0000

    Add attrap renaming for Data.Proxy

--- third-party/attrap/attrap.el
+++ third-party/attrap/attrap.el
@@ -1077,6 +1077,7 @@ Error is given as MSG and reported between POS and END."
                          ("GHC.Internal.Data.Function" . "Data.Function")
                          ("GHC.Internal.Data.Functor" . "Data.Functor")
                          ("GHC.Internal.Data.Functor.Identity" . "Data.Functor.Identity")
+                         ("GHC.Internal.Data.Proxy" . "Data.Proxy")
                          ("GHC.Internal.Data.Traversable" . "Data.Traversable")
                          ("GHC.Internal.Data.Ord" . "Data.Ord")
                          ("GHC.Internal.Foreign.Storable" . "Foreign.Storable")
