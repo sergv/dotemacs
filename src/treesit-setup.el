fc4a1f0b61a194cefcee0eed3209c7ac903e84bd
Sergey Vinokurov <serg.foo@gmail.com>
2025-06-27 23:10:02 +0100

    Make treesitter work with indirect buffers

--- src/treesit-setup.el
+++ src/treesit-setup.el
@@ -55,7 +55,8 @@ Return the root node of the syntax tree."
 (defun treesit-node-text-no-properties-unsafe (node)
   (cl-assert (not (null node)))
   (cl-assert (treesit-node-p node))
-  (cl-assert (eq (current-buffer) (treesit-node-buffer node)))
+  (cl-assert (or (eq (current-buffer) (treesit-node-buffer node))
+                 (eq (buffer-base-buffer (current-buffer)) (treesit-node-buffer node))))
   (buffer-substring-no-properties (treesit-node-start node) (treesit-node-end node)))
 
 (add-to-list 'treesit-simple-indent-presets
