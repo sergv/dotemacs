0fef44e6c710817beab5a193ca106769346ad428
Sergey Vinokurov <serg.foo@gmail.com>
2025-09-13 14:24:12 +0100

    Simplify use of ‘lsp-rust-analyzer-server-command’ by the lsp-mode so that nix wrapper could be added

--- third-party/lsp-mode/clients/lsp-rust.el
+++ third-party/lsp-mode/clients/lsp-rust.el
@@ -1783,11 +1783,7 @@ https://github.com/rust-lang/rust-analyzer/blob/master/docs/dev/lsp-extensions.m
  (make-lsp-client
   :new-connection (lsp-stdio-connection
                    (lambda ()
-                     `(,(or (executable-find
-                             (cl-first lsp-rust-analyzer-server-command))
-                            (lsp-package-path 'rust-analyzer)
-                            "rust-analyzer")
-                       ,@(cl-rest lsp-rust-analyzer-server-command))))
+                     lsp-rust-analyzer-server-command))
   :activation-fn (lsp-activate-on "rust")
   :priority (if (eq lsp-rust-server 'rust-analyzer) 1 -1)
   :initialization-options 'lsp-rust-analyzer--make-init-options
