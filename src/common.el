8535865fc73d9a41ceb60b9d85d4ffdd1a92cc35
Sergey Vinokurov <serg.foo@gmail.com>
2025-10-04 00:18:06 +0100

    Generalise checks whether the point is inside a string or comment, supply treesitter-based implementation for ‘haskell-ts-base-mode’. Fix ‘pretty-ligatures--compose-p’ to use one of the generalised checks.

--- src/common.el
+++ src/common.el
@@ -228,29 +228,52 @@ combinations"
 (defsubst parse-partial-sexp--comment-or-string-start (state)
   (elt state 8))
 
+(defvar point-inside-string?-impl #'point-inside-string?--default)
+(defvar point-inside-comment?-impl #'point-inside-comment?--default)
+(defvar point-inside-string-or-comment?-impl #'point-inside-string-or-comment?--default)
+(defvar point-not-inside-string-or-comment?-impl #'point-not-inside-string-or-comment?--default)
+
 (defun point-inside-string? (&optional pos)
+  "Return non-nil if point is positioned inside a string."
+  (declare (pure nil) (side-effect-free t))
+  (funcall point-inside-string?-impl pos))
+
+(defun point-inside-comment? (&optional pos)
+  "Return non-nil if point is positioned inside a string."
+  (declare (pure nil) (side-effect-free t))
+  (funcall point-inside-comment?-impl pos))
+
+(defun point-inside-string-or-comment? (&optional pos)
+  "Return t if point is positioned inside a string."
+  (declare (pure nil) (side-effect-free t))
+  (funcall point-inside-string-or-comment?-impl pos))
+
+(defsubst point-not-inside-string-or-comment? (&optional pos)
+  (declare (pure nil) (side-effect-free t))
+  (funcall point-not-inside-string-or-comment?-impl pos))
+
+(defun point-inside-string?--default (&optional pos)
   "Return non-nil if point is positioned inside a string."
   (declare (pure nil) (side-effect-free t))
   (save-excursion
     (let ((state (syntax-ppss (or pos (point)))))
       (elt state 3))))
 
-(defun point-inside-comment? (&optional pos)
+(defun point-inside-comment?--default (&optional pos)
   "Return non-nil if point is positioned inside a string."
   (declare (pure nil) (side-effect-free t))
   (save-excursion
     (let ((state (syntax-ppss (or pos (point)))))
       (elt state 4))))
 
-(defun point-inside-string-or-comment? (&optional pos)
+(defun point-inside-string-or-comment?--default (&optional pos)
   "Return t if point is positioned inside a string."
   (declare (pure nil) (side-effect-free t))
   (save-excursion
     (let ((state (syntax-ppss (or pos (point)))))
-      (or (elt state 3)
-          (elt state 4)))))
+      (elt state 8))))
 
-(defsubst point-not-inside-string-or-comment? (&optional pos)
+(defsubst point-not-inside-string-or-comment?--default (&optional pos)
   (declare (pure nil) (side-effect-free t))
   (not (point-inside-string-or-comment? pos)))
 
