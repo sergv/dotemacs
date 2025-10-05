be8e9c5500161203544a03e86f87b49cf5a98b1e
Sergey Vinokurov <serg.foo@gmail.com>
2025-10-05 20:36:50 +0100

    Introduce structure type for cabal components reported by flycheck-haskell’s get-cabal-configuration.hs

---
+++ src/haskell/haskell-cabal-components.el
@@ -0,0 +1,60 @@
+;; haskell-cabal-components.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created:  6 October 2025
+;; Description:
+
+(eval-when-compile
+  (require 'cl)
+  (require 'subr-x)
+  (require 'macro-util)
+  (require 'set-up-platform)
+  (require 'treesit-utils)
+  (require 'trie))
+
+(cl-defstruct (cabal-component
+               (:conc-name cabal-component/))
+  type
+  name
+  main-file
+  module-list
+  source-dirs)
+
+(defun parse-cabal-component (entry)
+  "Turn
+
+(<component type> <component name> <main file> <module list> <source dirs>)
+
+into
+
+‘cabal-component’ struct."
+  (let ((type (car entry))
+        (name (cadr entry))
+        (main-file (caddr entry))
+        (module-list (cadddr entry))
+        (source-dirs (car (cddddr entry))))
+    (cl-assert (stringp type))
+    (cl-assert (stringp name))
+    (cl-assert (or (string= type "lib")
+                   (string= type "flib")
+                   (stringp main-file)))
+    (cl-assert (listp module-list))
+    (cl-assert (-all? #'listp module-list))
+    (cl-assert (-all? (lambda (xs) (-all? #'stringp xs)) module-list))
+    (cl-assert (listp source-dirs))
+    (cl-assert (-all? #'stringp source-dirs))
+    (make-cabal-component
+     :type type
+     :name name
+     :main-file main-file
+     :module-list module-list
+     :source-dirs source-dirs)))
+
+(provide 'haskell-cabal-components)
+
+;; Local Variables:
+;; End:
+
+;; haskell-cabal-components.el ends here
