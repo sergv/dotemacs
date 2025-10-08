3c087d4139be3fc34bb2254199bfd92606cf365b
Sergey Vinokurov <serg.foo@gmail.com>
2025-10-09 00:53:30 +0100

    Add automatic preprocessing before loading buffer file into ghci to both dante and dante-repl so that ghci will have an option to pick latest changes instead of requiring manual restart

--- src/haskell/haskell-constants.el
+++ src/haskell/haskell-constants.el
@@ -6,11 +6,17 @@
 ;; Created: 25 January 2022
 ;; Description:
 
+(defconst +haskell-preprocessing-extensions+
+  '("hsc" "chs" "alex" "x" "lx" "happy" "y" "ly" "ag" "lag")
+  "Extensions for Haskell files that need to be preprocessed before being fed to GHC.")
+
 (defconst +haskell-extensions+
-  '("hs" "hsig" "lhs" "hsc" "chs" "hs-boot" "lhs-boot" "alex" "x" "lx" "happy" "y" "ly" "ag" "lag"))
+  (append '("hs" "hsig" "lhs" "hs-boot" "lhs-boot")
+          +haskell-preprocessing-extensions+)
+  "All Haskell-related file extensions.")
 
 (defconst +haskell-syntax-modes+
-  '(haskell-mode haskell-ts-mode haskell-literate-mode haskell-c-mode haskell-c2hs-mode alex-mode happy-mode uuag-mode)
+  '(haskell-mode haskell-ts-mode haskell-literate-mode haskell-hsc-mode haskell-c2hs-mode alex-mode happy-mode uuag-mode)
   "List of modes that use haskell syntax.")
 
 (defconst +haskell-default-checker+ 'haskell-dante ;;'lsp
