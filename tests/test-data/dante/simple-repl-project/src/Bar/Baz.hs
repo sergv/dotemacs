86d5ff33efb9069907a0c847ae9ef3311bfde6aa
Sergey Vinokurov <serg.foo@gmail.com>
2025-10-12 02:11:57 +0100

    Add basic tests for dante checker and dante-repl

---
+++ tests/test-data/dante/simple-repl-project/src/Bar/Baz.hs
@@ -0,0 +1,10 @@
+-- |
+-- Module:     Bar.Baz
+-- Copyright:  (c) Sergey Vinokurov 2025
+-- License:    Apache-2.0 (see LICENSE)
+-- Maintainer: serg.foo@gmail.com
+
+module Bar.Baz (baz) where
+
+baz :: a -> a
+baz x = x
