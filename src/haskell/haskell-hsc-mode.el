d443ee74661394655409f240f404210441262aa7
Sergey Vinokurov <serg.foo@gmail.com>
2026-01-18 00:56:39 +0000

    Add treesitter-based haskell-hsc-mode and tests, fix vanilla Haskell mode functions to work with hsc too

--- src/haskell/haskell-hsc-mode.el
+++ src/haskell/haskell-hsc-mode.el
@@ -10,15 +10,32 @@
   (require 'macro-util))
 
 (require 'haskell-mode)
+(require 'haskell-ts-mode)
 (require 'haskell-font-lock)
 (require 'haskell-utils)
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.hsc\\'" . haskell-hsc-mode))
 
+(defconst haskell-hsc-mode-syntax-table haskell-ts-mode-syntax-table)
+
 ;;;###autoload
-(define-derived-mode haskell-hsc-mode haskell-ts-base-mode "HSC"
-  "Mode for editing *.hsc files of the hsc2hs Haskell tool.")
+(define-derived-mode haskell-hsc-mode haskell-ts-base-mode "HSC[ts]"
+  "Mode for editing *.hsc files of the hsc2hs Haskell tool."
+  (let ((res (treesit-language-available-p 'hsc t)))
+    (unless (car res)
+      (error "HSC treesitter not available: %s" (cdr res))))
+
+  (setq-local haskell-ts-buffer-lang 'hsc)
+
+  ;; Font locking
+  (setq-local treesit-font-lock-settings
+              (apply #'treesit-font-lock-rules (haskell-ts-lang-selection-resolve haskell-ts-font-lock-rules)))
+
+  ;; Associate parser with current buffer.
+  (treesit-parser-create 'hsc (current-buffer))
+
+  (treesit-major-mode-setup))
 
 (provide 'haskell-hsc-mode)
 
