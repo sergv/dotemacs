9302dc4911ef91abfe9b481745ff27558bbb7cbb
Sergey Vinokurov <serg.foo@gmail.com>
2026-03-24 01:59:31 +0000

    Add quick keybinding to clear *Messages* buffer

--- src/messages-buffer-setup.el
+++ src/messages-buffer-setup.el
@@ -6,9 +6,20 @@
 ;; Created: 15 January 2022
 ;; Description:
 
+(eval-when-compile
+  (require 'macro-util))
+
+(defun message-buffer-erase ()
+  (interactive)
+  (with-inhibited-read-only
+   (erase-buffer)))
+
 ;;;###autoload
 (defun messages-buffer-setup ()
-  (hl-line-mode +1))
+  (hl-line-mode +1)
+
+  (def-keys-for-map messages-buffer-mode-map
+    ("C-SPC" message-buffer-erase)))
 
 ;;;###autoload
 (add-hook 'messages-buffer-mode-hook #'messages-buffer-setup)
