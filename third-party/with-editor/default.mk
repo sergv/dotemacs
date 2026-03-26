69b95aaf4748c0872efd5dd131420b9cd2869d51
Sergey Vinokurov <serg.foo@gmail.com>
2026-03-26 23:30:20 +0000

    Merge commit '64211dcb815f2533ac3d2a7e56ff36ae804d8338' (tag: v3.4.9, with-editor-repo/main, with-editor-repo/HEAD) from 'https://github.com/magit/with-editor.git' into 'third-party/with-editor'

--- third-party/with-editor/default.mk
+++ third-party/with-editor/default.mk
@@ -1,5 +1,7 @@
 TOP := $(dir $(lastword $(MAKEFILE_LIST)))
 
+DOMAIN ?= magit.vc
+
 PKG = with-editor
 
 ELS   = $(PKG).el
@@ -8,27 +10,26 @@ ELCS  = $(ELS:.el=.elc)
 DEPS  = compat
 DEPS += vterm
 
-DOMAIN      ?= magit.vc
-CFRONT_DIST ?= E2LUHBKU1FBV02
+LOAD_PATH     ?= $(addprefix -L ../../,$(DEPS))
+LOAD_PATH     += -L .
+ORG_LOAD_PATH ?= -L ../../org/lisp
 
 VERSION ?= $(shell test -e $(TOP).git && git describe --tags --abbrev=0 | cut -c2-)
 REVDESC := $(shell test -e $(TOP).git && git describe --tags)
 
-EMACS      ?= emacs
-EMACS_ARGS ?=
-EMACS_ARGS += --eval '(setq with-editor-emacsclient-executable nil)'
-
-LOAD_PATH  ?= $(addprefix -L ../../,$(DEPS))
-LOAD_PATH  += -L .
-
-ifndef ORG_LOAD_PATH
-ORG_LOAD_PATH  = -L ../../org/lisp
-endif
+EMACS       ?= emacs
+EMACS_ARGS  ?= --eval "(setq with-editor-emacsclient-executable nil)"
+EMACS_Q_ARG ?= -Q
+EMACS_BATCH ?= $(EMACS) $(EMACS_Q_ARG) --batch $(EMACS_ARGS) $(LOAD_PATH)
+EMACS_ORG   ?= $(EMACS) $(EMACS_Q_ARG) --batch $(EMACS_ARGS) $(ORG_LOAD_PATH)
 
 INSTALL_INFO     ?= $(shell command -v ginstall-info || printf install-info)
 MAKEINFO         ?= makeinfo
-MANUAL_HTML_ARGS ?= --css-ref /assets/page.css
+MANUAL_HTML_ARGS ?= --css-ref https://magit.vc/assets/page.css
 
 GITSTATS      ?= gitstats
 GITSTATS_DIR  ?= $(TOP)docs/stats
 GITSTATS_ARGS ?= -c style=https://magit.vc/assets/stats.css -c max_authors=999
+
+RCLONE      ?= rclone
+RCLONE_ARGS ?= -v
