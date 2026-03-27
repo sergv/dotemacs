b4fda7b13f42b60f6179c7558b3186d59819b7ec
Sergey Vinokurov <serg.foo@gmail.com>
2026-03-27 00:47:18 +0000

    Make ‘eproj-switch-to-file-or-buffer’ show relative paths only for files in current project or the ones that “share path vertical” with it

---
+++ tests/eproj-sample-projects/navigation-file-construction/mainproject/Mainfile.hs
@@ -0,0 +1,10 @@
+-- |
+-- Module:     Mainfile
+-- Copyright:  (c) Sergey Vinokurov 2026
+-- License:    Apache-2.0 (see LICENSE)
+-- Maintainer: serg.foo@gmail.com
+
+module Mainfile where
+
+quux :: a -> a
+quux x = x
