3ff6cefac80e22e338aa84567b117922180bd30b
Sergey Vinokurov <serg.foo@gmail.com>
2026-03-27 00:47:18 +0000

    Make ‘eproj-switch-to-file-or-buffer’ show relative paths only for files in current project or the ones that “share path vertical” with it

---
+++ tests/eproj-sample-projects/navigation-file-construction/sideproject/Sidefile.hs
@@ -0,0 +1,10 @@
+-- |
+-- Module:     Sidefile
+-- Copyright:  (c) Sergey Vinokurov 2026
+-- License:    Apache-2.0 (see LICENSE)
+-- Maintainer: serg.foo@gmail.com
+
+module Sidefile where
+
+bar :: a -> a
+bar x = x
