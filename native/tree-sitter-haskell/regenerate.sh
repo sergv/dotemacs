6af661100e9a01af79bdcaa72dba84312e5b2064
Sergey Vinokurov <serg.foo@gmail.com>
2026-03-28 12:11:15 +0000

    Improve script for regenerating tree-sitter-haskell grammars to automatically get tree-sitter via nix shell

--- native/tree-sitter-haskell/regenerate.sh
+++ native/tree-sitter-haskell/regenerate.sh
@@ -10,6 +10,16 @@ set -u
 # propagate errors from all parts of pipes
 set -o pipefail
 
+set -e
+
+# [[ -z "${IN_NIX_SHELL-}" ]]
+
+if ! which tree-sitter >/dev/null 2>/dev/null && which nix >/dev/null 2>/dev/null; then
+    echo "Building via nix"
+    echo nix shell nixpkgs#nodejs nixpkgs#tree-sitter --command "$0"
+    exec nix shell nixpkgs#nodejs nixpkgs#tree-sitter --command "$0"
+fi
+
 # nix shell nixpkgs#nodejs nixpkgs#tree-sitter --command
 tree-sitter generate "${@}"
 tree-sitter generate --build -o hsc/src hsc/grammar.js "${@}"
