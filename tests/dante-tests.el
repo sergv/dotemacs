7394bd2344ea2da3d2e62f4b53845e09be05ac9f
Sergey Vinokurov <serg.foo@gmail.com>
2026-04-11 23:45:45 +0100

    Reduce repetition in dante tests

--- tests/dante-tests.el
+++ tests/dante-tests.el
@@ -167,62 +167,63 @@
       dante-tests-tests/simple-test-project-archive
       tmp-dir
 
-    (dante-tests/with-file
-        (concat tmp-dir "/simple-check-project/src/Foo.hs")
-      (should (derived-mode-p 'haskell-ts-base-mode))
-      (should flycheck-mode)
-      (should dante-mode)
-
-      (should (string= (dante-config/cabal-target (dante-get-config))
-                       "emacs-dante-simple-check-test-project:lib:emacs-dante-simple-check-test-project"))
-
-      (delete-directory (dante-config/build-dir (dante-get-config)) t)
-
-      (dante-tests/check-buffer-and-assert-when-done
-       (should (not (null flycheck-current-errors)))
-
-       (let ((err (--find (string-suffix-p "Baz.hs" (flycheck-error-filename it) t)
-                          flycheck-current-errors)))
-         (should (not (null err)))
-         (should (string= (flycheck-error-filename err)
-                          (concat tmp-dir "/simple-check-project/src/Bar/Baz.hs")))
-         (should (= (flycheck-error-line err) 10))
-         (should (string-search "GHC-25897" (flycheck-error-message err)))
-         (should (string-search "Couldn't match expected type ‘b’ with actual type ‘a’"
-                                (flycheck-error-message err)))))
-
-      (progn
-        (flycheck-enhancements-next-error-with-wraparound)
-
-        (should (string= (concat tmp-dir "/simple-check-project/src/Bar/Baz.hs")
-                         (buffer-file-name))))
-
-      (progn
-        (goto-line-dumb 10)
-        (move-to-column 4)
-
-        (dante-tests/type-at-point-and-assert-when-done
-            ty
-          (should (string= ty "x :: a"))))
-
-      (progn
-        (goto-char (point-min))
-
-        (save-match-data
-          (search-forward "baz :: a -> b"))
-
-        (delete-word -1)
-        (insert "a")
-        (save-buffer)
-
-        (dante-tests/check-buffer-and-assert-when-done
-         (should (null flycheck-current-errors))))
-
+    (let ((proj-dir (concat tmp-dir "/simple-check-project")))
       (dante-tests/with-file
-          (concat tmp-dir "/simple-check-project/src/Foo.hs")
+          (concat proj-dir "/src/Foo.hs")
+        (should (derived-mode-p 'haskell-ts-base-mode))
+        (should flycheck-mode)
+        (should dante-mode)
+
+        (should (string= (dante-config/cabal-target (dante-get-config))
+                         "emacs-dante-simple-check-test-project:lib:emacs-dante-simple-check-test-project"))
+
+        (delete-directory (dante-config/build-dir (dante-get-config)) t)
 
         (dante-tests/check-buffer-and-assert-when-done
-         (should (null flycheck-current-errors)))))))
+         (should (not (null flycheck-current-errors)))
+
+         (let ((err (--find (string-suffix-p "Baz.hs" (flycheck-error-filename it) t)
+                            flycheck-current-errors)))
+           (should (not (null err)))
+           (should (string= (flycheck-error-filename err)
+                            (concat proj-dir "/src/Bar/Baz.hs")))
+           (should (= (flycheck-error-line err) 10))
+           (should (string-search "GHC-25897" (flycheck-error-message err)))
+           (should (string-search "Couldn't match expected type ‘b’ with actual type ‘a’"
+                                  (flycheck-error-message err)))))
+
+        (progn
+          (flycheck-enhancements-next-error-with-wraparound)
+
+          (should (string= (concat proj-dir "/src/Bar/Baz.hs")
+                           (buffer-file-name))))
+
+        (progn
+          (goto-line-dumb 10)
+          (move-to-column 4)
+
+          (dante-tests/type-at-point-and-assert-when-done
+           ty
+           (should (string= ty "x :: a"))))
+
+        (progn
+          (goto-char (point-min))
+
+          (save-match-data
+            (search-forward "baz :: a -> b"))
+
+          (delete-word -1)
+          (insert "a")
+          (save-buffer)
+
+          (dante-tests/check-buffer-and-assert-when-done
+           (should (null flycheck-current-errors))))
+
+        (dante-tests/with-file
+            (concat proj-dir "/src/Foo.hs")
+
+          (dante-tests/check-buffer-and-assert-when-done
+           (should (null flycheck-current-errors))))))))
 
 (ert-deftest z-dante-tests/simple-check-project-3-name-shadowing-error ()
   (unless (executable-find "cabal")
@@ -338,37 +339,19 @@
       dante-tests-tests/simple-check-test-project-with-hsc-archive
       tmp-dir
 
-    (dante-tests/with-file
-        (concat tmp-dir "/simple-check-project-with-hsc/src/Foo.hs")
-      (should (derived-mode-p 'haskell-ts-base-mode))
-      (should flycheck-mode)
-      (should dante-mode)
+    (let ((proj-dir (concat tmp-dir "/simple-check-project-with-hsc")))
+      (dante-tests/with-file
+          (concat proj-dir "/src/Foo.hs")
+        (should (derived-mode-p 'haskell-ts-base-mode))
+        (should flycheck-mode)
+        (should dante-mode)
 
-      (should (string= (dante-config/cabal-target (dante-get-config))
-                       "emacs-dante-simple-check-test-project-with-hsc:lib:emacs-dante-simple-check-test-project-with-hsc"))
+        (should (string= (dante-config/cabal-target (dante-get-config))
+                         "emacs-dante-simple-check-test-project-with-hsc:lib:emacs-dante-simple-check-test-project-with-hsc"))
 
-      (should (eq 'build (dante-method/name (dante-config/method (dante-get-config)))))
+        (should (eq 'build (dante-method/name (dante-config/method (dante-get-config)))))
 
-      (delete-directory (dante-config/build-dir (dante-get-config)) t)
-
-      (dante-tests/check-buffer-and-assert-when-done
-       (should (not (null flycheck-current-errors)))
-
-       (let ((err (--find (string-suffix-p "Baz.hsc" (flycheck-error-filename it) t)
-                          flycheck-current-errors)))
-         (should (not (null err)))
-         (should (string= (flycheck-error-filename err)
-                          (concat tmp-dir "/simple-check-project-with-hsc/src/Bar/Baz.hsc")))
-         (should (= (flycheck-error-line err) 18))
-         (should (string-search "GHC-25897" (flycheck-error-message err)))
-         (should (string-search "Couldn't match expected type ‘a’ with actual type ‘CDoubleTyp’"
-                                (flycheck-error-message err)))))
-
-      (progn
-        (flycheck-enhancements-next-error-with-wraparound)
-
-        (should (string= (concat tmp-dir "/simple-check-project-with-hsc/src/Bar/Baz.hsc")
-                         (buffer-file-name)))
+        (delete-directory (dante-config/build-dir (dante-get-config)) t)
 
         (dante-tests/check-buffer-and-assert-when-done
          (should (not (null flycheck-current-errors)))
@@ -377,30 +360,49 @@
                             flycheck-current-errors)))
            (should (not (null err)))
            (should (string= (flycheck-error-filename err)
-                            (concat tmp-dir "/simple-check-project-with-hsc/src/Bar/Baz.hsc")))
+                            (concat proj-dir "/src/Bar/Baz.hsc")))
            (should (= (flycheck-error-line err) 18))
            (should (string-search "GHC-25897" (flycheck-error-message err)))
            (should (string-search "Couldn't match expected type ‘a’ with actual type ‘CDoubleTyp’"
-                                  (flycheck-error-message err)))))        )
+                                  (flycheck-error-message err)))))
 
-      (progn
-        (goto-char (point-min))
+        (progn
+          (flycheck-enhancements-next-error-with-wraparound)
 
-        (save-match-data
-          (search-forward "baz :: Double -> a"))
+          (should (string= (concat proj-dir "/src/Bar/Baz.hsc")
+                           (buffer-file-name)))
 
-        (delete-word -1)
-        (insert "CDoubleTyp")
-        (save-buffer)
+          (dante-tests/check-buffer-and-assert-when-done
+           (should (not (null flycheck-current-errors)))
 
-        (dante-tests/check-buffer-and-assert-when-done
-         (should (null flycheck-current-errors))))
+           (let ((err (--find (string-suffix-p "Baz.hsc" (flycheck-error-filename it) t)
+                              flycheck-current-errors)))
+             (should (not (null err)))
+             (should (string= (flycheck-error-filename err)
+                              (concat proj-dir "/src/Bar/Baz.hsc")))
+             (should (= (flycheck-error-line err) 18))
+             (should (string-search "GHC-25897" (flycheck-error-message err)))
+             (should (string-search "Couldn't match expected type ‘a’ with actual type ‘CDoubleTyp’"
+                                    (flycheck-error-message err)))))        )
 
-      (dante-tests/with-file
-          (concat tmp-dir "/simple-check-project-with-hsc/src/Foo.hs")
+        (progn
+          (goto-char (point-min))
 
-        (dante-tests/check-buffer-and-assert-when-done
-         (should (null flycheck-current-errors)))))))
+          (save-match-data
+            (search-forward "baz :: Double -> a"))
+
+          (delete-word -1)
+          (insert "CDoubleTyp")
+          (save-buffer)
+
+          (dante-tests/check-buffer-and-assert-when-done
+           (should (null flycheck-current-errors))))
+
+        (dante-tests/with-file
+            (concat proj-dir "/src/Foo.hs")
+
+          (dante-tests/check-buffer-and-assert-when-done
+           (should (null flycheck-current-errors))))))))
 
 (ert-deftest z-dante-tests/check-hsc-project-2 ()
   (unless (executable-find "cabal")
@@ -414,53 +416,54 @@
       dante-tests-tests/simple-check-test-project-with-hsc-archive
       tmp-dir
 
-    (dante-tests/with-file
-        (concat tmp-dir "/simple-check-project-with-hsc/src/Foo.hs")
-      (should (derived-mode-p 'haskell-ts-base-mode))
-      (should flycheck-mode)
-      (should dante-mode)
+    (let ((proj-dir (concat tmp-dir "/simple-check-project-with-hsc")))
+      (dante-tests/with-file
+          (concat proj-dir "/src/Foo.hs")
+        (should (derived-mode-p 'haskell-ts-base-mode))
+        (should flycheck-mode)
+        (should dante-mode)
 
-      (should (string= (dante-config/cabal-target (dante-get-config))
-                       "emacs-dante-simple-check-test-project-with-hsc:lib:emacs-dante-simple-check-test-project-with-hsc"))
+        (should (string= (dante-config/cabal-target (dante-get-config))
+                         "emacs-dante-simple-check-test-project-with-hsc:lib:emacs-dante-simple-check-test-project-with-hsc"))
 
-      (should (eq 'build (dante-method/name (dante-config/method (dante-get-config)))))
+        (should (eq 'build (dante-method/name (dante-config/method (dante-get-config)))))
 
-      (delete-directory (dante-config/build-dir (dante-get-config)) t)
+        (delete-directory (dante-config/build-dir (dante-get-config)) t)
 
-      (dante-tests/check-buffer-and-assert-when-done
-       (should (not (null flycheck-current-errors)))
+        (dante-tests/check-buffer-and-assert-when-done
+         (should (not (null flycheck-current-errors)))
 
-       (let ((err (--find (string-suffix-p "Baz.hsc" (flycheck-error-filename it) t)
-                          flycheck-current-errors)))
-         (should (not (null err)))
-         (should (string= (flycheck-error-filename err)
-                          (concat tmp-dir "/simple-check-project-with-hsc/src/Bar/Baz.hsc")))
-         (should (= (flycheck-error-line err) 18))
-         (should (string-search "GHC-25897" (flycheck-error-message err)))
-         (should (string-search "Couldn't match expected type ‘a’ with actual type ‘CDoubleTyp’"
-                                (flycheck-error-message err)))))
+         (let ((err (--find (string-suffix-p "Baz.hsc" (flycheck-error-filename it) t)
+                            flycheck-current-errors)))
+           (should (not (null err)))
+           (should (string= (flycheck-error-filename err)
+                            (concat proj-dir "/src/Bar/Baz.hsc")))
+           (should (= (flycheck-error-line err) 18))
+           (should (string-search "GHC-25897" (flycheck-error-message err)))
+           (should (string-search "Couldn't match expected type ‘a’ with actual type ‘CDoubleTyp’"
+                                  (flycheck-error-message err)))))
 
-      ;; Check that changes outside Emacs trigger preprocessing.
-      (call-process "sed" nil nil nil
-                    "-re"
-                    "s/baz :: Double -> a/baz :: Double -> CDoubleTyp/"
-                    "-i"
-                    (concat tmp-dir "/simple-check-project-with-hsc/src/Bar/Baz.hsc"))
-      (dante-tests/check-buffer-and-assert-when-done
-       (should (string= (buffer-file-name)
-                        (concat tmp-dir "/simple-check-project-with-hsc/src/Foo.hs")))
-       (should (null flycheck-current-errors)))
+        ;; Check that changes outside Emacs trigger preprocessing.
+        (call-process "sed" nil nil nil
+                      "-re"
+                      "s/baz :: Double -> a/baz :: Double -> CDoubleTyp/"
+                      "-i"
+                      (concat proj-dir "/src/Bar/Baz.hsc"))
+        (dante-tests/check-buffer-and-assert-when-done
+         (should (string= (buffer-file-name)
+                          (concat proj-dir "/src/Foo.hs")))
+         (should (null flycheck-current-errors)))
 
-      ;; And back again
-      (call-process "sed" nil nil nil
-                    "-re"
-                    "s/baz :: Double -> CDoubleTyp/baz :: Double -> a/"
-                    "-i"
-                    (concat tmp-dir "/simple-check-project-with-hsc/src/Bar/Baz.hsc"))
-      (dante-tests/check-buffer-and-assert-when-done
-       (should (string= (buffer-file-name)
-                        (concat tmp-dir "/simple-check-project-with-hsc/src/Foo.hs")))
-       (should (not (null flycheck-current-errors)))))))
+        ;; And back again
+        (call-process "sed" nil nil nil
+                      "-re"
+                      "s/baz :: Double -> CDoubleTyp/baz :: Double -> a/"
+                      "-i"
+                      (concat proj-dir "/src/Bar/Baz.hsc"))
+        (dante-tests/check-buffer-and-assert-when-done
+         (should (string= (buffer-file-name)
+                          (concat proj-dir "/src/Foo.hs")))
+         (should (not (null flycheck-current-errors))))))))
 
 (ert-deftest z-dante-tests/repl-hsc-project-1 ()
   (unless (executable-find "cabal")
@@ -569,73 +572,74 @@
   (test-utils--with-unzipped-project
       dante-tests-tests/simple-check-test-project-with-hsc-archive
       tmp-dir
-    (dante-tests/with-file
-        (concat tmp-dir "/simple-check-project-with-hsc/src/Bar/Baz.hsc")
-      (should (derived-mode-p 'haskell-ts-base-mode))
-      (should flycheck-mode)
-      (should dante-mode)
+    (let ((proj-dir (concat tmp-dir "/simple-check-project-with-hsc")))
+      (dante-tests/with-file
+          (concat proj-dir "/src/Bar/Baz.hsc")
+        (should (derived-mode-p 'haskell-ts-base-mode))
+        (should flycheck-mode)
+        (should dante-mode)
 
-      (should (string= (dante-config/cabal-target (dante-get-config))
-                       "emacs-dante-simple-check-test-project-with-hsc:lib:emacs-dante-simple-check-test-project-with-hsc"))
+        (should (string= (dante-config/cabal-target (dante-get-config))
+                         "emacs-dante-simple-check-test-project-with-hsc:lib:emacs-dante-simple-check-test-project-with-hsc"))
 
-      (should (eq 'build (dante-method/name (dante-config/method (dante-get-config)))))
+        (should (eq 'build (dante-method/name (dante-config/method (dante-get-config)))))
 
-      (delete-directory (dante-config/repl-dir (dante-get-config)) t)
+        (delete-directory (dante-config/repl-dir (dante-get-config)) t)
 
-      (let ((hsc-buf (current-buffer)))
-
-        (vim:haskell-dante-load-file-into-repl:wrapper)
-
-        (let ((repl-proc (get-buffer-process (current-buffer))))
-          (dante-repl/wait-for-modules-loaded repl-proc)
-          (accept-process-output repl-proc 1 nil t)
-
-          (let ((initial-output (dante-repl-get-last-output)))
-            (should (string-search "GHC-25897" initial-output))
-            (should (string-search "Couldn't match expected type ‘a’ with actual type ‘CDoubleTyp’" initial-output)))
-
-          (insert ":t 1 + 2")
-          (comint-send-input)
-          (dante-repl/wait-for-prompt repl-proc)
-          (should (string= (dante-repl-get-last-output) "1 + 2 :: Num a => a\n"))
-
-          (insert ":i Num")
-          (comint-send-input)
-          (dante-repl/wait-for-prompt repl-proc)
-          (let ((msg (dante-repl-get-last-output)))
-            (should (string-match-p "class Num a where" msg))
-            (should (string-match-p "instance Num Double" msg))
-            (should (string-match-p "instance Num Int" msg)))
-
-          (switch-to-buffer hsc-buf)
-
-          (progn
-            (goto-char (point-min))
-
-            (save-match-data
-              (search-forward "baz :: Double -> a"))
-
-            (delete-word -1)
-            (insert "CDoubleTyp")
-            (save-buffer))
+        (let ((hsc-buf (current-buffer)))
 
           (vim:haskell-dante-load-file-into-repl:wrapper)
-          (dante-repl/wait-for-prompt repl-proc)
 
-          (insert ":t 1 + 2")
-          (comint-send-input)
-          (dante-repl/wait-for-prompt repl-proc)
-          (should (string= (dante-repl-get-last-output) "1 + 2 :: Num a => a\n"))
+          (let ((repl-proc (get-buffer-process (current-buffer))))
+            (dante-repl/wait-for-modules-loaded repl-proc)
+            (accept-process-output repl-proc 1 nil t)
 
-          (insert ":t baz")
-          (comint-send-input)
-          (dante-repl/wait-for-prompt repl-proc)
-          (should (string= (dante-repl-get-last-output) "baz :: Double -> CDoubleTyp\n"))
+            (let ((initial-output (dante-repl-get-last-output)))
+              (should (string-search "GHC-25897" initial-output))
+              (should (string-search "Couldn't match expected type ‘a’ with actual type ‘CDoubleTyp’" initial-output)))
 
-          (insert "baz 0")
-          (comint-send-input)
-          (dante-repl/wait-for-prompt repl-proc)
-          (should (string= (dante-repl-get-last-output) "0.0\n")))))))
+            (insert ":t 1 + 2")
+            (comint-send-input)
+            (dante-repl/wait-for-prompt repl-proc)
+            (should (string= (dante-repl-get-last-output) "1 + 2 :: Num a => a\n"))
+
+            (insert ":i Num")
+            (comint-send-input)
+            (dante-repl/wait-for-prompt repl-proc)
+            (let ((msg (dante-repl-get-last-output)))
+              (should (string-match-p "class Num a where" msg))
+              (should (string-match-p "instance Num Double" msg))
+              (should (string-match-p "instance Num Int" msg)))
+
+            (switch-to-buffer hsc-buf)
+
+            (progn
+              (goto-char (point-min))
+
+              (save-match-data
+                (search-forward "baz :: Double -> a"))
+
+              (delete-word -1)
+              (insert "CDoubleTyp")
+              (save-buffer))
+
+            (vim:haskell-dante-load-file-into-repl:wrapper)
+            (dante-repl/wait-for-prompt repl-proc)
+
+            (insert ":t 1 + 2")
+            (comint-send-input)
+            (dante-repl/wait-for-prompt repl-proc)
+            (should (string= (dante-repl-get-last-output) "1 + 2 :: Num a => a\n"))
+
+            (insert ":t baz")
+            (comint-send-input)
+            (dante-repl/wait-for-prompt repl-proc)
+            (should (string= (dante-repl-get-last-output) "baz :: Double -> CDoubleTyp\n"))
+
+            (insert "baz 0")
+            (comint-send-input)
+            (dante-repl/wait-for-prompt repl-proc)
+            (should (string= (dante-repl-get-last-output) "0.0\n"))))))))
 
 (provide 'dante-tests)
 
