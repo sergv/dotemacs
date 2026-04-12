12d5a2d37a6037f686b5c393497a511f3a22ac51
Sergey Vinokurov <serg.foo@gmail.com>
2026-04-12 12:50:29 +0100

    Fix for running dante checker under ‘trix’ that may end up having different checker working directory than when using vanilla cabal which confuses flycheck’s resolution of relative paths in reported errors
    Flycheck tries to resolve error paths against checker’s default
    directory. With ‘trix’ this directory may end up the directory where
    ‘cabal.project’ is located but checker would report errors from
    subdirectory where cabal package resides,
    e.g. $ROOT/cabal.project vs $ROOT/my-package/...
    
    Not sure whether ‘nix develop’ changes checker’s directory the same
    way as ‘trix develop’ does but generally this change is how to do
    it properly.

---
+++ tests/test-data/dante/simple-check-project-error-with-relative-path-from-subproject/dep/Dep.hs
@@ -0,0 +1,10 @@
+-- |
+-- Module:     Dep
+-- Copyright:  (c) Sergey Vinokurov 2026
+-- License:    Apache-2.0 (see LICENSE)
+-- Maintainer: serg.foo@gmail.com
+
+module Dep (dep) where
+
+dep :: Int -> Int
+dep x = x * x
