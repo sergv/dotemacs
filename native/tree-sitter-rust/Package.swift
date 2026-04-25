326c1bd1279fd18aa1da422160745da654a5d9e0
Sergey Vinokurov <serg.foo@gmail.com>
2026-04-26 00:01:41 +0100

    Merge commit '77a3747266f4d621d0757825e6b11edcbf991ca5' from 'git@github.com:tree-sitter/tree-sitter-rust.git' into 'native/tree-sitter-rust'

---
+++ native/tree-sitter-rust/Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TreeSitterRust",
+    products: [
+        .library(name: "TreeSitterRust", targets: ["TreeSitterRust"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", from: "0.9.0"),
+    ],
+    targets: [
+        .target(
+            name: "TreeSitterRust",
+            dependencies: [],
+            path: ".",
+            sources: [
+                "src/parser.c",
+                "src/scanner.c",
+            ],
+            resources: [
+                .copy("queries")
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")]
+        ),
+        .testTarget(
+            name: "TreeSitterRustTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterRust",
+            ],
+            path: "bindings/swift/TreeSitterRustTests"
+        )
+    ],
+    cLanguageStandard: .c11
+)
