27743ce48da2342e4fe647f535a42774b519ad9a
Sergey Vinokurov <serg.foo@gmail.com>
2026-04-26 14:21:29 +0100

    Throw away useless parts of rust-mode

--- third-party/rust-mode/test-project/src/rustfmt-default.rs
+++
@@ -1,5 +0,0 @@
-// this file ensures that by default rustfmt is invoked without parameters
-// and formats with its default (--edition=2015 as of today)
-
-// With --edition=2024 this import will be reordered
-use std::{i16, i8};
