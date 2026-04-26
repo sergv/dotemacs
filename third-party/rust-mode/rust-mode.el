7f2998c443d0c775e508e586730cad956877a980
Sergey Vinokurov <serg.foo@gmail.com>
2026-04-26 14:21:29 +0100

    Throw away useless parts of rust-mode

--- third-party/rust-mode/rust-mode.el
+++ third-party/rust-mode/rust-mode.el
@@ -20,17 +20,8 @@
   (require 'rx)
   (require 'subr-x))
 
-(require 'current-column-fixed)
-
-(defvar rust-load-optional-libraries t
-  "Whether loading `rust-mode' also loads optional libraries.
-This variable might soon be remove again.")
-
-(when rust-load-optional-libraries
-  (require 'rust-cargo)
-  (require 'rust-compile)
-  (require 'rust-playpen)
-  (require 'rust-rustfmt))
+(require 'rust-cargo)
+(require 'rust-rustfmt)
 
 ;;; Customization
 
@@ -39,52 +30,7 @@ This variable might soon be remove again.")
   :link '(url-link "https://www.rust-lang.org/")
   :group 'languages)
 
-(defcustom rust-mode-treesitter-derive t
-  "Whether rust-mode should derive from the new treesitter mode `rust-ts-mode'
-instead of `prog-mode'. This option requires emacs29+."
-  :version "29.1"
-  :type 'boolean
-  :group 'rustic)
-
-;;; Faces
-
-(define-obsolete-face-alias 'rust-unsafe-face
-  'rust-unsafe "0.6.0")
-(define-obsolete-face-alias 'rust-question-mark-face
-  'rust-question-mark "0.6.0")
-(define-obsolete-face-alias 'rust-builtin-formatting-macro-face
-  'rust-builtin-formatting-macro "0.6.0")
-(define-obsolete-face-alias 'rust-string-interpolation-face
-  'rust-string-interpolation "0.6.0")
-
-;;; Mode
-
-(defvar rust-mode-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "C-c C-d") #'rust-dbg-wrap-or-unwrap)
-    (when rust-load-optional-libraries
-      (define-key map (kbd "C-c C-c C-u") #'rust-compile)
-      (define-key map (kbd "C-c C-c C-k") #'rust-check)
-      (define-key map (kbd "C-c C-c C-t") #'rust-test)
-      (define-key map (kbd "C-c C-c C-r") #'rust-run)
-      (define-key map (kbd "C-c C-c C-l") #'rust-run-clippy)
-      (define-key map (kbd "C-c C-f") #'rust-format-region)
-      (define-key map (kbd "C-c C-n") #'rust-goto-format-problem))
-    map)
-  "Keymap for Rust major mode.")
-
-;;;###autoload
-(autoload 'rust-mode "rust-mode" "Major mode for Rust code." t)
-
-;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
-
 (provide 'rust-mode)
-
-(if (and rust-mode-treesitter-derive
-         (version<= "29.1" emacs-version))
-    (require 'rust-mode-treesitter)
-  (require 'rust-prog-mode))
 
 (require 'rust-utils)
 
