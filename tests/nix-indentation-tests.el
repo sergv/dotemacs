439c792a5d67f81da02ab8c69a86e2c175f79b7f
Sergey Vinokurov <serg.foo@gmail.com>
2026-07-01 11:34:47 +0100

    Improve nix treesitter indentation of lets, add tests

---
+++ tests/nix-indentation-tests.el
@@ -0,0 +1,122 @@
+;; nix-indentation-tests.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created:  Wednesday,  1 July 2026
+;; Description:
+
+(eval-when-compile
+  (require 'cl-lib)
+  (require 'cl))
+
+(require 'nix-ts-mode)
+
+(require 'common)
+(require 'ert)
+(require 'search)
+(require 'tests-utils)
+(require 'treesit-setup)
+
+(cl-defmacro nix-indentation-tests--test-treesitter
+    (&key name
+          contents        ;; Buffer text before indent.
+          expected-value  ;; Buffer text after indent.
+          expected-result ;; Whether the test should fail or succeed.
+          )
+  (cl-assert (symbolp name))
+  `(ert-deftest ,name ()
+     :expected-result ,(or expected-result :passed) ;;:failed
+     (tests-utils--test-buffer-contents
+      :action (treesit-indent)
+      :contents ,contents
+      :expected-value ,expected-value
+      :initialisation (nix-ts-mode)
+      :buffer-id
+      ,(string->symbol "nix-indentation-tests"))))
+
+(nix-indentation-tests--test-treesitter
+ :name nix-indentation-tests--let-comment-after-in-1
+ :contents
+ (tests-utils--multiline
+  "let foo = bar;"
+  "in"
+  "    _|_# foo"
+  "{"
+  "  bar = 1;"
+  "}")
+ :expected-value
+ (tests-utils--multiline
+  "let foo = bar;"
+  "in"
+  "_|_# foo"
+  "{"
+  "  bar = 1;"
+  "}"))
+
+(nix-indentation-tests--test-treesitter
+ :name nix-indentation-tests--let-in-binding-1
+ :contents
+ (tests-utils--multiline
+  "let"
+  "      _|_foo = bar;"
+  "in"
+  "{"
+  "  bar = 1;"
+  "}")
+ :expected-value
+ (tests-utils--multiline
+  "let"
+  "  _|_foo = bar;"
+  "in"
+  "{"
+  "  bar = 1;"
+  "}"))
+
+(nix-indentation-tests--test-treesitter
+ :name nix-indentation-tests--let-in-comment-ater-binding-1
+ :contents
+ (tests-utils--multiline
+  "let"
+  "  foo = bar;"
+  "            _|_# foo"
+  "in"
+  "{"
+  "  bar = 1;"
+  "}")
+ :expected-value
+ (tests-utils--multiline
+  "let"
+  "  foo = bar;"
+  "  _|_# foo"
+  "in"
+  "{"
+  "  bar = 1;"
+  "}"))
+
+(nix-indentation-tests--test-treesitter
+ :name nix-indentation-tests--let-in-comment-ater-binding-2
+ :contents
+ (tests-utils--multiline
+  "let foo = bar;"
+  "            _|_# foo"
+  "in"
+  "{"
+  "  bar = 1;"
+  "}")
+ :expected-value
+ (tests-utils--multiline
+  "let foo = bar;"
+  "    _|_# foo"
+  "in"
+  "{"
+  "  bar = 1;"
+  "}"))
+
+(provide 'nix-indentation-tests)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
+
+;; nix-indentation-tests.el ends here
