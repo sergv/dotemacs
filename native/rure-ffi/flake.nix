bb1ce0af114d7006f5edf1aea62b6cf142d23c3a
Sergey Vinokurov <serg.foo@gmail.com>
2026-07-05 19:03:22 +0100

    Merge commit '682a1b4f9ad60518749dd25cbd73c1981a4c8c2d' from 'git@github.com:sergv/rure-ffi.git' into 'native/rure-ffi'

--- native/rure-ffi/flake.nix
+++ native/rure-ffi/flake.nix
@@ -5,26 +5,38 @@
       url = "github:nixos/nixpkgs";
     };
 
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
-
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem ["x86_64-linux" "i686-linux"] (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [
-            pkgs.rure
-            pkgs.rure.dev
-            pkgs.pkg-config
-          ];
-          LD_LIBRARY_PATH = "${pkgs.rure}/lib";
-          # shellHook = ''
-          #   echo "Updated rure-dev is at ${pkgs.rure.dev}"
-          # '';
-        };
-      });
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "i686-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forEachSystem = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages."${system}";
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [
+              pkgs.rure
+              pkgs.rure.dev
+              pkgs.pkg-config
+            ];
+            LD_LIBRARY_PATH = "${pkgs.rure}/lib";
+            # shellHook = ''
+            #   echo "Updated rure-dev is at ${pkgs.rure.dev}"
+            # '';
+          };
+        }
+      );
+    };
 }
