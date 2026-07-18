e00822b637909e7796ebdf052eddffeefb1259f7
Sergey Vinokurov <serg.foo@gmail.com>
2026-07-18 11:58:27 +0100

    This is ridiculous, why auctex thinks it’s acceptable to modify load-path?

--- third-party/auctex/lpath.el
+++ third-party/auctex/lpath.el
@@ -5,6 +5,5 @@
 ;;; It is not a part of AUCTeX itself.
 
 ;; Make sure we get the right files.
-(setq load-path (cons "." load-path)
-      TeX-lisp-directory "<none>"
+(setq TeX-lisp-directory "<none>"
       TeX-auto-global "<none>")
