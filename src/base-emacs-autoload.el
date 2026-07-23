3db47f0978dc60f860b96d84b07b43de6b950e81
Sergey Vinokurov <serg.foo@gmail.com>
2026-07-23 22:51:58 +0100

    Refactor ‘hydra--format’ regexp to use ‘rx’

---
+++ src/base-emacs-autoload.el
@@ -0,0 +1,17 @@
+;; base-emacs-autoload.el --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) Sergey Vinokurov
+;;
+;; Author: Sergey Vinokurov <serg.foo@gmail.com>
+;; Created: 23 July 2026
+;; Description:
+
+(autoload 'smerge-next "smerge-mode")
+(autoload 'smerge-prev "smerge-mode")
+
+(provide 'base-emacs-autoload)
+
+;; Local Variables:
+;; End:
+
+;; base-emacs-autoload.el ends here
