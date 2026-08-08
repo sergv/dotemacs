d3bc63036264098245ad88e2684ffddffb046737
Sergey Vinokurov <serg.foo@gmail.com>
2026-08-08 13:00:27 +0100

    Reindnet treesit-sexp modules

--- third-party/treesit-sexp/structural-editing.el
+++ third-party/treesit-sexp/structural-editing.el
@@ -7,100 +7,101 @@
 ;;; Code:
 
 (defun structural-editing-splice ()
-    "Remove parentheses around the current sexp, merging it with its parent."
-    (interactive)
-    (save-excursion
-        (condition-case err
-                (progn
-                    (backward-up-list)
-                    (let ((start (point)))
-                        (forward-sexp)
-                        (delete-char -1)
-                        (goto-char start)
-                        (delete-char 1)))
-            (scan-error (error "Cannot splice: not in a list")))))
+  "Remove parentheses around the current sexp, merging it with its parent."
+  (interactive)
+  (save-excursion
+    (condition-case err
+        (progn
+          (backward-up-list)
+          (let ((start (point)))
+            (forward-sexp)
+            (delete-char -1)
+            (goto-char start)
+            (delete-char 1)))
+      (scan-error (error "Cannot splice: not in a list")))))
 
 (defun structural-editing-slurp-forward ()
-    "Pull the next sexp into the current list."
-    (interactive)
-    (condition-case err
-            (let ((sibling nil))
-                (save-excursion
-                    (catch 'found
-                        (while t
-                            (backward-up-list)
-                            (forward-list)
-                            (let ((test-pos (point)))
-                                (condition-case nil
-                                        (progn
-                                            (forward-sexp)
-                                            (setq sibling (delete-and-extract-region test-pos (point)))
-                                            (throw 'found nil))
-                                    (scan-error nil))))
-                        (error "Cannot slurp forward: no next sexp in any containing list")))
-                (backward-up-list)
-                (forward-list)
-                (backward-char)
-                (when (eq (char-before) ?\()
-                    (when (and (> (length sibling) 0)
-                               (eq (string-to-char sibling) ?\ ))
-                        (setq sibling (substring sibling 1))))
-                (insert sibling))
-        (scan-error (error "Cannot slurp forward: no next sexp in any containing list"))))
+  "Pull the next sexp into the current list."
+  (interactive)
+  (condition-case err
+      (let ((sibling nil))
+        (save-excursion
+          (catch 'found
+            (while t
+              (backward-up-list)
+              (forward-list)
+              (let ((test-pos (point)))
+                (condition-case nil
+                    (progn
+                      (forward-sexp)
+                      (setq sibling (delete-and-extract-region test-pos (point)))
+                      (throw 'found nil))
+                  (scan-error nil))))
+            (error "Cannot slurp forward: no next sexp in any containing list")))
+        (backward-up-list)
+        (forward-list)
+        (backward-char)
+        (when (eq (char-before) ?\()
+          (when (and (> (length sibling) 0)
+                     (eq (string-to-char sibling) ?\ ))
+            (setq sibling (substring sibling 1))))
+        (insert sibling))
+    (scan-error (error "Cannot slurp forward: no next sexp in any containing list"))))
 
 (defun structural-editing-slurp-backward ()
-    "Pull the previous sexp into the current list."
-    (interactive)
-    (condition-case err
-            (let ((sibling nil))
-                (save-excursion
-                    (catch 'found
-                        (while t
-                            (backward-up-list)
-                            (let ((test-end (point)))
-                                (condition-case nil
-                                        (progn
-                                            (backward-sexp)
-                                            (setq sibling (delete-and-extract-region (point) test-end))
-                                            (throw 'found nil))
-                                    (scan-error nil))))
-                        (error "Cannot slurp backward: no previous sexp in any containing list")))
-                (backward-up-list)
-                (forward-char)
-                (when (eq (char-after) ?\))
-                    (when (and (> (length sibling) 0)
-                               (eq (string-to-char (substring sibling -1)) ?\ ))
-                        (setq sibling (substring sibling 0 -1))))
-                (insert sibling))
-        (scan-error (error "Cannot slurp backward: no previous sexp in any containing list"))))
+  "Pull the previous sexp into the current list."
+  (interactive)
+  (condition-case err
+      (let ((sibling nil))
+        (save-excursion
+          (catch 'found
+            (while t
+              (backward-up-list)
+              (let ((test-end (point)))
+                (condition-case nil
+                    (progn
+                      (backward-sexp)
+                      (setq sibling (delete-and-extract-region (point) test-end))
+                      (throw 'found nil))
+                  (scan-error nil))))
+            (error "Cannot slurp backward: no previous sexp in any containing list")))
+        (backward-up-list)
+        (forward-char)
+        (when (eq (char-after) ?\))
+          (when (and (> (length sibling) 0)
+                     (eq (string-to-char (substring sibling -1)) ?\ ))
+            (setq sibling (substring sibling 0 -1))))
+        (insert sibling))
+    (scan-error (error "Cannot slurp backward: no previous sexp in any containing list"))))
 
 (defun structural-editing-barf-forward ()
-    "Move the last element out of the current list."
-    (interactive)
-    (save-excursion
-        (condition-case err
-                (progn
-                    (up-list)
-                    (delete-char -1)
-                    (backward-sexp)
-                    (when (eq (char-before) ?\ )
-                        (delete-char -1))
-                    (insert ") "))
-            (scan-error (error "Cannot barf forward: no elements to barf")))))
+  "Move the last element out of the current list."
+  (interactive)
+  (save-excursion
+    (condition-case err
+        (progn
+          (up-list)
+          (delete-char -1)
+          (backward-sexp)
+          (when (eq (char-before) ?\ )
+            (delete-char -1))
+          (insert ") "))
+      (scan-error (error "Cannot barf forward: no elements to barf")))))
 
 (defun structural-editing-barf-backward ()
-    "Move the first element out of the current list."
-    (interactive)
-    (save-excursion
-        (condition-case err
-                (progn
-                    (backward-up-list)
-                    (delete-char 1)
-                    (forward-sexp)
-                    (when (eq (char-after) ?\ )
-                        (delete-char 1))
-                    (insert " ("))
-            (scan-error (error "Cannot barf backward: no elements to barf")))))
+  "Move the first element out of the current list."
+  (interactive)
+  (save-excursion
+    (condition-case err
+        (progn
+          (backward-up-list)
+          (delete-char 1)
+          (forward-sexp)
+          (when (eq (char-after) ?\ )
+            (delete-char 1))
+          (insert " ("))
+      (scan-error (error "Cannot barf backward: no elements to barf")))))
 
 (provide 'structural-editing)
+
 ;;; structural-editing.el ends here
