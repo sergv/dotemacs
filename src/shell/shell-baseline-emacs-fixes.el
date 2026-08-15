f5f6fb6154d29e26dc7ac4f99db26d7e473cc4e2
Sergey Vinokurov <serg.foo@gmail.com>
2026-08-15 21:09:46 +0100

    More tramp fixes so that el-patched versions will get picked up. Take remote shell to start from "$SHELL" environment variable as the most portable option across Linux and MacOS

--- src/shell/shell-baseline-emacs-fixes.el
+++ src/shell/shell-baseline-emacs-fixes.el
@@ -7,7 +7,8 @@
 ;; Description:
 
 (eval-when-compile
-  (require 'el-patch))
+  (require 'el-patch)
+  (require 'shell))
 
 (defvar base-emacs-fixes--shell-exe nil
   "String - executable to start remote shell with that overrides default Emacs mechanism.")
@@ -17,9 +18,10 @@
 ;;;###autoload
 (add-to-list 'el-patch-features 'shell)
 
-(when-emacs-version (<= 30 it)
-  (el-patch-defun shell (&optional buffer file-name)
-    "Run an inferior shell, with I/O through BUFFER (which defaults to `*shell*').
+(with-eval-after-load "shell"
+  (when-emacs-version (<= 30 it)
+    (el-patch-defun shell (&optional buffer file-name)
+      "Run an inferior shell, with I/O through BUFFER (which defaults to `*shell*').
 Interactively, a prefix arg means to prompt for BUFFER.
 If `default-directory' is a remote file name, it is also prompted
 to change if called with a prefix arg.
@@ -53,115 +55,109 @@ Otherwise, one argument `-i' is passed to the shell.
 Make the shell buffer the current buffer, and return it.
 
 \(Type \\[describe-mode] in the shell buffer for a list of commands.)"
-    (interactive
-     (let* ((buffer
-             (and current-prefix-arg
-                  (read-buffer "Shell buffer: "
-                               ;; If the current buffer is an inactive
-                               ;; shell buffer, use it as the default.
-                               (if (and (eq major-mode 'shell-mode)
-                                        (null (get-buffer-process
-                                               (current-buffer))))
-                                   (buffer-name)
-                                 (generate-new-buffer-name "*shell*")))))
-            (buf (if (or buffer (not (derived-mode-p 'shell-mode))
-                         (comint-check-proc (current-buffer)))
-                     (get-buffer-create (or buffer "*shell*"))
-                   ;; If the current buffer is a dead shell buffer, use it.
-                   (current-buffer))))
+      (interactive
+       (let* ((buffer
+               (and current-prefix-arg
+                    (read-buffer "Shell buffer: "
+                                 ;; If the current buffer is an inactive
+                                 ;; shell buffer, use it as the default.
+                                 (if (and (eq major-mode 'shell-mode)
+                                          (null (get-buffer-process
+                                                 (current-buffer))))
+                                     (buffer-name)
+                                   (generate-new-buffer-name "*shell*")))))
+              (buf (if (or buffer (not (derived-mode-p 'shell-mode))
+                           (comint-check-proc (current-buffer)))
+                       (get-buffer-create (or buffer "*shell*"))
+                     ;; If the current buffer is a dead shell buffer, use it.
+                     (current-buffer))))
 
-       (with-current-buffer buf
-         (when (and buffer (file-remote-p default-directory))
-           ;; It must be possible to declare a local default-directory.
-           (setq default-directory
+         (with-current-buffer buf
+           (when (and buffer (file-remote-p default-directory))
+             ;; It must be possible to declare a local default-directory.
+             (setq default-directory
+                   (expand-file-name
+                    (read-directory-name
+                     "Default directory: " default-directory default-directory
+                     t nil))))
+           (list
+            buffer
+            ;; On remote hosts, the local `shell-file-name' might be useless.
+            (with-connection-local-variables
+             (when (and (file-remote-p default-directory)
+                        (null explicit-shell-file-name)
+                        (null (getenv "ESHELL")))
+               ;; `expand-file-name' shall not add the MS Windows volume letter
+               ;; (Bug#49229).
+               (replace-regexp-in-string
+                "^[[:alpha:]]:" ""
+                (file-local-name
                  (expand-file-name
-                  (read-directory-name
-                   "Default directory: " default-directory default-directory
-                   t nil))))
-         (list
-          buffer
-          ;; On remote hosts, the local `shell-file-name' might be useless.
-          (with-connection-local-variables
-           (when (and (file-remote-p default-directory)
-                      (null explicit-shell-file-name)
-                      (null (getenv "ESHELL")))
-             ;; `expand-file-name' shall not add the MS Windows volume letter
-             ;; (Bug#49229).
-             (replace-regexp-in-string
-              "^[[:alpha:]]:" ""
-              (file-local-name
-               (expand-file-name
-                (read-file-name "Remote shell path: " default-directory
-                                shell-file-name t shell-file-name
-                                #'file-remote-p))))))))))
+                  (read-file-name "Remote shell path: " default-directory
+                                  shell-file-name t shell-file-name
+                                  #'file-remote-p))))))))))
 
-    (setq buffer (if (or buffer (not (derived-mode-p 'shell-mode))
-                         (comint-check-proc (current-buffer)))
-                     (get-buffer-create (or buffer "*shell*"))
-                   ;; If the current buffer is a dead shell buffer, use it.
-                   (current-buffer)))
-    ;; The buffer's window must be correctly set when we call comint
-    ;; (so that comint sets the COLUMNS env var properly).
-    (with-suppressed-warnings ((obsolete display-comint-buffer-action))
-      (pop-to-buffer buffer display-comint-buffer-action))
+      (setq buffer (if (or buffer (not (derived-mode-p 'shell-mode))
+                           (comint-check-proc (current-buffer)))
+                       (get-buffer-create (or buffer "*shell*"))
+                     ;; If the current buffer is a dead shell buffer, use it.
+                     (current-buffer)))
+      ;; The buffer's window must be correctly set when we call comint
+      ;; (so that comint sets the COLUMNS env var properly).
+      (with-suppressed-warnings ((obsolete display-comint-buffer-action))
+        (pop-to-buffer buffer display-comint-buffer-action))
 
-    (with-connection-local-variables
-     (when file-name
-       (setq-local explicit-shell-file-name file-name))
+      (with-connection-local-variables
+       (when file-name
+         (setq-local explicit-shell-file-name file-name))
 
-     ;; Rain or shine, BUFFER must be current by now.
-     (unless (comint-check-proc buffer)
-       (let* ((prog (or (el-patch-add base-emacs-fixes--shell-exe)
-                        explicit-shell-file-name
-                        (getenv "ESHELL") shell-file-name))
-              (name (file-name-nondirectory prog))
-              (startfile (concat "~/.emacs_" name))
-              (xargs-name (el-patch-wrap 3 0
-                            ;; Don’t attempt to resolve explicit args if
-                            ;; we supplied explicit args via this new mechanism.
-                            (if base-emacs-fixes--shell-args
-                                nil
-                              (intern-soft (concat "explicit-" name "-args")))))
-              (start-point (point)))
-         (unless (file-exists-p startfile)
-           (setq startfile (locate-user-emacs-file
-                            (concat "init_" name ".sh"))))
-         (setq-local shell--start-prog (file-name-nondirectory prog))
-         (el-patch-add
-           (message "PATH = %s" (shell-command-to-string "echo $PATH"))
-           (message "shell: buffer = %s, prog = %s, base-emacs-fixes--shell-args = %s"
-                    (pp-to-string buffer)
-                    (pp-to-string prog)
-                    (pp-to-string base-emacs-fixes--shell-args)))
-         (apply #'make-comint-in-buffer "shell" buffer prog
-                nil
-                (el-patch-wrap 3 0
-                  (if base-emacs-fixes--shell-args
-                      base-emacs-fixes--shell-args
-                    (if (and xargs-name (boundp xargs-name))
-                        (symbol-value xargs-name)
-                      '("-i")))))
-         (shell-mode)
-         (when (file-exists-p startfile)
-           ;; Wait until the prompt has appeared.
-           (while (= start-point (point))
-             (sleep-for 0.1))
-           (shell-eval-command
-            (with-temp-buffer
-              (insert-file-contents startfile)
-              (buffer-string)))))))
-    (when shell-kill-buffer-on-exit
-      (let* ((buffer (current-buffer))
-             (process (get-buffer-process buffer))
-             (sentinel (process-sentinel process)))
-        (set-process-sentinel
-         process
-         (lambda (proc event)
-           (when sentinel
-             (funcall sentinel proc event))
-           (unless (buffer-live-p proc)
-             (kill-buffer buffer))))))
-    buffer))
+       ;; Rain or shine, BUFFER must be current by now.
+       (unless (comint-check-proc buffer)
+         (let* ((prog (or (el-patch-add base-emacs-fixes--shell-exe)
+                          explicit-shell-file-name
+                          (getenv "ESHELL") shell-file-name))
+                (name (file-name-nondirectory prog))
+                (startfile (concat "~/.emacs_" name))
+                (xargs-name (el-patch-wrap 3 0
+                              ;; Don’t attempt to resolve explicit args if
+                              ;; we supplied explicit args via this new mechanism.
+                              (if base-emacs-fixes--shell-args
+                                  nil
+                                (intern-soft (concat "explicit-" name "-args")))))
+                (start-point (point)))
+           (unless (file-exists-p startfile)
+             (setq startfile (locate-user-emacs-file
+                              (concat "init_" name ".sh"))))
+           (setq-local shell--start-prog (file-name-nondirectory prog))
+           (apply #'make-comint-in-buffer "shell" buffer prog
+                  nil
+                  (el-patch-wrap 3 0
+                    (if base-emacs-fixes--shell-args
+                        base-emacs-fixes--shell-args
+                      (if (and xargs-name (boundp xargs-name))
+                          (symbol-value xargs-name)
+                        '("-i")))))
+           (shell-mode)
+           (when (file-exists-p startfile)
+             ;; Wait until the prompt has appeared.
+             (while (= start-point (point))
+               (sleep-for 0.1))
+             (shell-eval-command
+              (with-temp-buffer
+                (insert-file-contents startfile)
+                (buffer-string)))))))
+      (when shell-kill-buffer-on-exit
+        (let* ((buffer (current-buffer))
+               (process (get-buffer-process buffer))
+               (sentinel (process-sentinel process)))
+          (set-process-sentinel
+           process
+           (lambda (proc event)
+             (when sentinel
+               (funcall sentinel proc event))
+             (unless (buffer-live-p proc)
+               (kill-buffer buffer))))))
+      buffer)))
 
 (provide 'shell-baseline-emacs-fixes)
 
